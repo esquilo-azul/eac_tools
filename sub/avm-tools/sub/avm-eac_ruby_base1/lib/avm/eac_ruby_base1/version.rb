a0fc23d5adf9fb5d0c0e808665360a30fec4bd58
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-03 15:06:33 -0300

    Gem "avm-eac_ruby_base1" (Local): remove.

--- sub/avm-tools/sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRubyBase1
-    VERSION = '0.7.0'
-  end
-end
--- sub/avm-tools/sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/sources/base_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/eac_ruby_base1/sources/base'
-
-::RSpec.describe ::Avm::EacRubyBase1::Sources::Base do
-  include_examples 'in_avm_registry', 'sources'
-end
