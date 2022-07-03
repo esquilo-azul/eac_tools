77447a85530498958610171cbb5b779b033eaf51
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-03 18:58:00 -0300

    Gem "avm-eac_rails_base1" (Local): remove.

--- sub/avm-tools/sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRailsBase1
-    VERSION = '0.3.0'
-  end
-end
--- sub/avm-tools/sub/avm-eac_rails_base1/template/avm/eac_rails_base1/systemd_unit/tasks_scheduler_command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source "$HOME/.rvm/scripts/rvm"
-( cd '%%fs_path%%'; RAILS_ENV=production bundle exec tasks_scheduler "$@" )
