3710ce09d1654d1a16cbc69010f5c89e99692e18
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-05 22:10:43 -0300

    Gem "avm-eac_redmine_base0" (Local): add '0.6.1'.

---
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/rest_api.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_rest/api'
+
+module Avm
+  module EacRedmineBase0
+    class RestApi < ::EacRest::Api
+    end
+  end
+end
