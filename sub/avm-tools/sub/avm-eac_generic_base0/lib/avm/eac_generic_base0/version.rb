44088d0c4d0bea6c9c20c967af8aa171e110878c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-06 21:30:26 -0300

    Gem "avm-eac_generic_base0" (Local): remove.

--- sub/avm-tools/sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacGenericBase0
-    VERSION = '0.2.0'
-  end
-end
--- sub/avm-tools/sub/avm-eac_generic_base0/spec/rubocop_spec.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-::EacRubyUtils::Rspec.default_setup.describe_rubocop
