9e31d8f2e6070cc44fe259c46dc9227190a137af
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-16 09:21:31 -0300

    Avm::EacGenericBase0::Sources::Tester: merge with "avm":Avm::Sources::Tester.

--- sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/sources/base.rb
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/sources/base.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'avm/eac_generic_base0/sources/tester'
 require 'avm/sources/base'
 require 'eac_ruby_utils/core_ext'
 
@@ -10,11 +9,6 @@ module Avm
       class Base < ::Avm::Sources::Base
         require_sub __FILE__, include_modules: true
         enable_abstract_methods
-
-        # @return [Avm::EacGenericBase0::Sources::Tester]
-        def tester_class
-          Avm::EacGenericBase0::Sources::Tester
-        end
 
         def valid?
           ::Avm::Registry.scms.detect_optional(path).present?
