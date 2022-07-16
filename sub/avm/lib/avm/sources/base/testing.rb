a436c6f9ed2f88908de47da5c5cc0e9a15ba8f8e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-16 09:41:51 -0300

    Avm::Sources::Base::Testing.configured_test_commands.

--- sub/avm/lib/avm/sources/base/testing.rb
+++ sub/avm/lib/avm/sources/base/testing.rb
@@ -9,9 +9,31 @@ module Avm
         DEFAULT_TEST_COMMANDS = {}.freeze
         TEST_KEY = 'test'
         TEST_COMMAND_KEY = "#{TEST_KEY}.command"
+        TEST_COMMANDS_KEY = "#{TEST_KEY}.commands"
 
         def configured_test_command
           read_configuration_as_env_command(TEST_COMMAND_KEY)
+        end
+
+        # @return [Hash<String, EacRubyUtils::Envs::Command>, nil]
+        def configured_test_commands
+          configured_value_as_test_commands(configuration.entry(TEST_COMMANDS_KEY).value)
+        end
+
+        # @return [Hash<String, EacRubyUtils::Envs::Command>, nil]
+        def configured_value_as_test_commands(value)
+          return nil if value.blank?
+
+          [::Hash, ::Enumerable].each do |type|
+            next unless value.is_a?(type)
+
+            return send(
+              "configured_#{type.name.demodulize.variableize}_value_as_test_commands",
+              value
+            )
+          end
+
+          raise "Value for test commands should be a Hash or a Enumerable (Actual: #{value})"
         end
 
         # @return [Hash<String, EacRubyUtils::Envs::Command>]
@@ -27,6 +49,18 @@ module Avm
         # @return [Class<Avm::Sources::Tester>
         def tester_class
           Avm::Sources::Tester
+        end
+
+        protected
+
+        def configured_enumerable_value_as_test_commands(value)
+          configured_hash_value_as_test_commands(
+            value.each_with_index.map { |v| ["test_#{v[1]}", v[0]] }.to_h
+          )
+        end
+
+        def configured_hash_value_as_test_commands(value)
+          value.map { |k, v| [k.to_s.strip, configuration_value_to_env_command(v)] }.to_h
         end
       end
     end
