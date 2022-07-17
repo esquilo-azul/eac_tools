01ce9ea9bc37f4e337c5ddfc3d55008dd56cba62
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-16 23:49:49 -0300

    Gem "avm-eac_rails_base0" (Local): add '0.4.1'.

---
+++ sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/apache_host.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'avm/eac_rails_base1/apache_host'
+
+module Avm
+  module EacRailsBase0
+    class ApacheHost < ::Avm::EacRailsBase1::ApacheHost
+    end
+  end
+end
