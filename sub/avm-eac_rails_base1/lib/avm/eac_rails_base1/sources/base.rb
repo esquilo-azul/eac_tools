7937217a4e0707ec9949c2f8554eb3ba53b1d0da
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-17 13:03:07 -0300

    Avm::EacRailsBase1::Sources::Base.default_test_commands.

--- sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/sources/base.rb
+++ sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/sources/base.rb
@@ -8,10 +8,26 @@ module Avm
     module Sources
       class Base < ::Avm::EacRubyBase1::Sources::Base
         CONFIG_RU_SUBPATH = 'config.ru'
+        EAC_RAILS_BASE1_TEST_NAME = 'eac_rails_base1'
+        RAKE_TEST_COMMAND_DEFAULT_TASK_NAME = 'test'
         SUBS_PATHS_DEFAULT = ['sub/*/*'].freeze
 
         def config_ru_path
           path.join(CONFIG_RU_SUBPATH)
+        end
+
+        def default_test_commands
+          {
+            EAC_RAILS_BASE1_TEST_NAME => eac_rails_base1_test_command
+          }
+        end
+
+        def eac_rails_base1_test_command
+          rake_test_command
+        end
+
+        def rake_test_command(task_name = RAKE_TEST_COMMAND_DEFAULT_TASK_NAME)
+          rake(task_name).chdir_root.envvar('RAILS_ENV', 'test')
         end
 
         def subs_paths_default
