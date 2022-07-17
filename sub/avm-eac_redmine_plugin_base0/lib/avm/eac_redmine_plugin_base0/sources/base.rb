1978f6a55331efa290608754a3dd6390903789c8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-16 23:41:48 -0300

    Avm::EacRedminePluginBase0::Sources::Base.default_test_commands.

--- sub/avm-eac_redmine_plugin_base0/lib/avm/eac_redmine_plugin_base0/sources/base.rb
+++ sub/avm-eac_redmine_plugin_base0/lib/avm/eac_redmine_plugin_base0/sources/base.rb
@@ -8,6 +8,8 @@ module Avm
     module Sources
       class Base < ::Avm::EacRubyBase1::Sources::Base
         DEFAULT_GEMFILE_PATH = 'SelfGemfile'
+        RUBOCOP_TEST_NAME = 'rubocop'
+        PARENT_RAKE_TASK_TEST_NAME = 'parent_rake_task'
         INIT_SUBPATH = 'init.rb'
 
         # @return [String]
@@ -15,9 +17,34 @@ module Avm
           DEFAULT_GEMFILE_PATH
         end
 
+        # @return [Hash<String, EacRubyUtils::Envs::Command>]
+        def default_test_commands
+          {
+            PARENT_RAKE_TASK_TEST_NAME => parent_rake_test_command,
+            RUBOCOP_TEST_NAME => rubocop_test_command
+          }
+        end
+
         # @return [String]
         def init_path
           path.join(INIT_SUBPATH)
+        end
+
+        # @return [String]
+        def parent_rake_test_task_name
+          [gem_name, 'test'].map(&:variableize).join(':')
+        end
+
+        # @return [EacRubyUtils::Envs::Command]
+        def parent_rake_test_command
+          ruby_parent.rake(parent_rake_test_task_name).chdir_root.envvar('RAILS_ENV', 'test')
+        end
+
+        # @return [EacRubyUtils::Envs::Command]
+        def rubocop_test_command
+          bundle('exec', 'rubocop', '--ignore-parent-exclusion')
+            .envvar('RAILS_ENV', 'test')
+            .chdir_root
         end
 
         # @return [Boolean]
