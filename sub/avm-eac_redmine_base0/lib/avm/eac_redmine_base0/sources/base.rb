341e52eddaf512eadbf2b95948c8f68c1a611abf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-17 22:18:58 -0300

    Avm::EacRedmineBase0::Sources::Base.default_test_commands.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/sources/base.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/sources/base.rb
@@ -8,8 +8,16 @@ module Avm
   module EacRedmineBase0
     module Sources
       class Base < ::Avm::EacRailsBase1::Sources::Base
+        DEFAULT_TEST_COMMANDS = {}.freeze
         REDMINE_LIB_SUBPATH = 'lib/redmine.rb'
         SUBS_INCLUDE_PATHS_DEFAULT = ['plugins/*'].freeze
+
+        # Return a empty hash (No tests).
+        #
+        # @return [Hash<String, EacRubyUtils::Envs::Command].
+        def default_test_commands
+          DEFAULT_TEST_COMMANDS
+        end
 
         def redmine_lib_path
           path.join(REDMINE_LIB_SUBPATH)
