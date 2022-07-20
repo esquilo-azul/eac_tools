50f58d9ee9275be441c5c67dec13ad5f9d5e8fe2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-20 11:58:46 -0300

    Gem "eac_config" (Local): remove.

--- sub/avm-tools/sub/eac_config/lib/eac_config.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacConfig
-  require_sub __FILE__
-end
--- sub/avm-tools/sub/eac_config/lib/eac_config/rspec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_exts'
-
-module EacConfig
-  module Rspec
-    require_sub __FILE__
-  end
-end
