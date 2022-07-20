e08e87c4f30f2d7a0f45128e4ff441befa249d04
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-20 19:51:38 -0300

    Avm::EacRubyBase1::Sources::Base::Rake.rake_tasks.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/sources/base/rake.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/sources/base/rake.rb
@@ -9,6 +9,9 @@ module Avm
       class Base < ::Avm::EacGenericBase0::Sources::Base
         module Rake
           RAKEFILE_BASENAME = 'Rakefile'
+          RAKE_TASKS_LINE_PARSER = /\Arake\s+(\S+)\s+/.to_parser do |m|
+            m[1]
+          end
 
           # @return [Avm::EacRubyBase1::Sources::Base::BundleCommand]
           def rake(*args)
@@ -20,6 +23,12 @@ module Avm
           # @return [Pathname]
           def rakefile_path
             path.join(RAKEFILE_BASENAME)
+          end
+
+          # @return [Array<String>]
+          def rake_tasks
+            rake('--all', '--tasks').execute!.each_line
+              .map { |line| RAKE_TASKS_LINE_PARSER.parse!(line) }
           end
         end
       end
