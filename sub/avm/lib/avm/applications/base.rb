c2908d366d3655f0d5426b5f686bdab764c9b90b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-21 12:48:39 -0300

    Avm::Applications::Base.initialize: refact with .common_constructor.

--- sub/avm/lib/avm/applications/base.rb
+++ sub/avm/lib/avm/applications/base.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 require 'avm/instances/entries'
+require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Applications
     class Base
       include ::Avm::Instances::Entries
 
-      attr_reader :id
-
-      def initialize(id)
-        @id = id.to_s
+      common_constructor :id do
+        self.id = id.to_s
       end
 
       def to_s
