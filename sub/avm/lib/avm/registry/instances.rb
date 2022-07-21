6d0d83b481e6415a66b45bb0a3f8751b2e7d6eee
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-21 15:08:44 -0300

    Avm::Registry::Instances.

---
+++ sub/avm/lib/avm/registry/instances.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'avm/instances/base'
+require 'avm/registry/from_gems'
+
+module Avm
+  module Registry
+    class Instances < ::Avm::Registry::FromGems
+      # @return [Avm::Instances::Base, nil]
+      def class_detect(klass, detect_args)
+        r = ::Avm::Instances::Base.by_id(*detect_args)
+        r.application.stereotype.instance_class == klass ? r : nil
+      end
+    end
+  end
+end
