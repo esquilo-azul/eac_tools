7347c3d113f1147d99ca77de279470c4fb8ae092
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-25 20:34:02 -0300

    Avm::EacRedmineBase0::Instances::Base.

---
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/base.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/instances/base'
+require 'avm/eac_redmine_base0/instances/runners'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRedmineBase0
+    module Instances
+      class Base < ::Avm::Instances::Base
+      end
+    end
+  end
+end
