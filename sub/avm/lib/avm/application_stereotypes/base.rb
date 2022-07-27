853f95ef8ebc482ffd63c4fcc1158bfc5442c9ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-27 13:20:15 -0300

    Avm::ApplicationStereotypes::Base::RESOURCE_*.

--- sub/avm/lib/avm/application_stereotypes/base.rb
+++ sub/avm/lib/avm/application_stereotypes/base.rb
@@ -5,6 +5,8 @@ require 'eac_ruby_utils/core_ext'
 module Avm
   module ApplicationStereotypes
     class Base
+      enable_listable
+      lists.add_symbol :resource, :instance, :source
       common_constructor :namespace_module, :instance_class, :source_class
 
       # @return [String]
