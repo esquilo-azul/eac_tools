9bae7f02048a8728446a7dfa964d488f5c6797c4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-27 11:23:18 -0300

    Avm::SourceGenerators::Base.

---
+++ sub/avm/lib/avm/source_generators/base.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'avm/with_application_stereotype'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module SourceGenerators
+    class Base
+      include ::Avm::WithApplicationStereotype
+      common_constructor :target_path do
+        self.target_path = target_path.to_pathname
+      end
+
+      def perform
+        assert_clear_directory
+        apply_template
+      end
+
+      def assert_clear_directory
+        target_path.mkpath
+        raise "\"#{target_path}\" is not empty" if target_path.children.any?
+      end
+
+      def apply_template
+        template.apply(self, target_path)
+      end
+    end
+  end
+end
