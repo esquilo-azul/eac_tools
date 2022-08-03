c030f5d8e000fc871d256bdac43106fac758df91
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-03 14:50:13 -0300

    RSpec.describe(Avm::Instances::Base): refact.

--- sub/avm/spec/lib/avm/instances/base_spec.rb
+++ sub/avm/spec/lib/avm/instances/base_spec.rb
@@ -13,56 +13,56 @@ RSpec.describe ::Avm::Instances::Base do
     .stub_eac_config_node(self, ::File.join(__dir__, 'base_spec_configs_storage.yml'))
 
   describe '#read_entry' do
-    context 'when a auto value is requested' do
-      {
-        'app_0' => {
-          fs_path: '/fs_root/app_0',
-          data_fs_path: '/data_fs_root/app_0',
-          ::Avm::Instances::EntryKeys::DATABASE_NAME => 'app_0',
-          ::Avm::Instances::EntryKeys::DATABASE_USERNAME => 'user1',
-          ::Avm::Instances::EntryKeys::DATABASE_PASSWORD => 'pass1',
-          ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => 'database.net',
-          ::Avm::Instances::EntryKeys::DATABASE_PORT => 5432,
-          ::Avm::Instances::EntryKeys::INSTALL_HOSTNAME => 'myhost.com',
-          ::Avm::Instances::EntryKeys::INSTALL_USERNAME => 'myuser',
-          ::Avm::Instances::EntryKeys::INSTALL_URL => 'ssh://otheruser@otherhost.com'
-        },
-        'app_2' => {
-          ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => '127.0.0.1',
-          ::Avm::Instances::EntryKeys::SOURCE_INSTANCE_ID => 'app_dev'
-        },
-        'app_3' => {
-          ::Avm::Instances::EntryKeys::DATABASE_SYSTEM => 'postgresql',
-          ::Avm::Instances::EntryKeys::DATABASE_NAME => 'app_1_db',
-          ::Avm::Instances::EntryKeys::DATABASE_USERNAME => 'user1',
-          ::Avm::Instances::EntryKeys::DATABASE_PASSWORD => 'pass1',
-          ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => 'database.net',
-          ::Avm::Instances::EntryKeys::DATABASE_PORT => 5432,
-          ::Avm::Instances::EntryKeys::MAILER_ID => 'mailer_0',
-          ::Avm::Instances::EntryKeys::MAILER_FROM => 'noreply@example.net',
-          ::Avm::Instances::EntryKeys::MAILER_REPLY_TO => nil,
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_ADDRESS => 'smtp.example.net',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_PORT => '587',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_DOMAIN => 'example.net',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_USERNAME => 'a_user',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_PASSWORD => 'a_secret',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_AUTHENTICATION => 'plain',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_STARTTLS_AUTO => 'true'
-        },
-        'mailer_0' => {
-          ::Avm::Instances::EntryKeys::MAILER_FROM => 'noreply@example.net',
-          ::Avm::Instances::EntryKeys::MAILER_REPLY_TO => '',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_ADDRESS => 'smtp.example.net',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_PORT => '587',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_DOMAIN => 'example.net',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_USERNAME => 'a_user',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_PASSWORD => 'a_secret',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_AUTHENTICATION => 'plain',
-          ::Avm::Instances::EntryKeys::MAILER_SMTP_STARTTLS_AUTO => 'true'
-        }
-      }.each do |instance_id, values|
-        values.each do |input, expected|
-          it "read entry #{instance_id}.#{input} should return \"#{expected}\"" do
+    {
+      'app_0' => {
+        fs_path: '/fs_root/app_0',
+        data_fs_path: '/data_fs_root/app_0',
+        ::Avm::Instances::EntryKeys::DATABASE_NAME => 'app_0',
+        ::Avm::Instances::EntryKeys::DATABASE_USERNAME => 'user1',
+        ::Avm::Instances::EntryKeys::DATABASE_PASSWORD => 'pass1',
+        ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => 'database.net',
+        ::Avm::Instances::EntryKeys::DATABASE_PORT => 5432,
+        ::Avm::Instances::EntryKeys::INSTALL_HOSTNAME => 'myhost.com',
+        ::Avm::Instances::EntryKeys::INSTALL_USERNAME => 'myuser',
+        ::Avm::Instances::EntryKeys::INSTALL_URL => 'ssh://otheruser@otherhost.com'
+      },
+      'app_2' => {
+        ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => '127.0.0.1',
+        ::Avm::Instances::EntryKeys::SOURCE_INSTANCE_ID => 'app_dev'
+      },
+      'app_3' => {
+        ::Avm::Instances::EntryKeys::DATABASE_SYSTEM => 'postgresql',
+        ::Avm::Instances::EntryKeys::DATABASE_NAME => 'app_1_db',
+        ::Avm::Instances::EntryKeys::DATABASE_USERNAME => 'user1',
+        ::Avm::Instances::EntryKeys::DATABASE_PASSWORD => 'pass1',
+        ::Avm::Instances::EntryKeys::DATABASE_HOSTNAME => 'database.net',
+        ::Avm::Instances::EntryKeys::DATABASE_PORT => 5432,
+        ::Avm::Instances::EntryKeys::MAILER_ID => 'mailer_0',
+        ::Avm::Instances::EntryKeys::MAILER_FROM => 'noreply@example.net',
+        ::Avm::Instances::EntryKeys::MAILER_REPLY_TO => nil,
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_ADDRESS => 'smtp.example.net',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_PORT => '587',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_DOMAIN => 'example.net',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_USERNAME => 'a_user',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_PASSWORD => 'a_secret',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_AUTHENTICATION => 'plain',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_STARTTLS_AUTO => 'true'
+      },
+      'mailer_0' => {
+        ::Avm::Instances::EntryKeys::MAILER_FROM => 'noreply@example.net',
+        ::Avm::Instances::EntryKeys::MAILER_REPLY_TO => '',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_ADDRESS => 'smtp.example.net',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_PORT => '587',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_DOMAIN => 'example.net',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_USERNAME => 'a_user',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_PASSWORD => 'a_secret',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_AUTHENTICATION => 'plain',
+        ::Avm::Instances::EntryKeys::MAILER_SMTP_STARTTLS_AUTO => 'true'
+      }
+    }.each do |instance_id, values|
+      values.each do |input, expected|
+        context "when a auto value is requested for \"#{instance_id}.#{input}\"" do
+          it ".read_entry should return \"#{expected}\"" do
             expect(described_class.by_id(instance_id).read_entry(input)).to eq(expected)
           end
         end
