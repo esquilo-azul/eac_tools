78e0eae77e0fc36674cb2fe8b70b39758e3405b3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-04 15:44:15 -0300

    Gem "avm-git" (Local): remove.

--- sub/avm-tools/sub/avm-git/lib/avm/git/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Git
-    VERSION = '0.4.0'
-  end
-end
--- sub/avm-tools/sub/avm-git/spec/lib/avm/git/scms/git_subrepo_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/registry'
-require 'avm/git/scms/git_subrepo'
-
-::RSpec.describe ::Avm::Git::Scms::GitSubrepo do
-  include_examples 'in_avm_registry', 'scms'
-end
