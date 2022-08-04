af0cc735280620d918a403353972b82cfd0c21e2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-04 15:30:44 -0300

    Gem "eac_templates" (Local): remove.

--- sub/avm-tools/sub/eac_templates/lib/eac_templates.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacTemplates
-  require_sub __FILE__
-end
--- sub/avm-tools/sub/eac_templates/lib/eac_templates/from_all_gems.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_templates/from_gem'
-::EacTemplates::FromGem.include_all
