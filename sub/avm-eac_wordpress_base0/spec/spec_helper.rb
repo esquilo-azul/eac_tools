4700f9861f10bdd8adeed2b3dafc4294df0e696f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-04 13:31:23 -0300

    Avm::EacWordpressBase0::Instances::ApacheHost: move from "avm-tools":Avm::EacWordpressBase0::ApacheHost.

--- sub/avm-eac_wordpress_base0/spec/spec_helper.rb
+++ sub/avm-eac_wordpress_base0/spec/spec_helper.rb
@@ -2,3 +2,4 @@
 
 require 'eac_ruby_utils/rspec/default_setup'
 ::EacRubyUtils::Rspec.default_setup_create(::File.expand_path('..', __dir__))
+::EacRubyUtils::Rspec.default_setup.stub_eac_config_node
