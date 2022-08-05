2f096e25ee3f7ae03e6145ce236bb1b507c6ea00
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-05 20:18:27 -0300

    Avm::EacGenericBase0::Instances::Base.

---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/instances/base.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'avm/instances/base'
+
+module Avm
+  module EacGenericBase0
+    module Instances
+      class Base < ::Avm::Instances::Base
+      end
+    end
+  end
+end
