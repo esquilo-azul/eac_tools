6abc3c5dfdaca39dae1b789ba1d3012233c31b6d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-05 22:55:38 -0300

    Avm::Entries::AutoValues::UriEntry.

---
+++ sub/avm/lib/avm/entries/auto_values/uri_entry.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'avm/entries/uri_builder'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Entries
+    module AutoValues
+      class UriEntry
+        common_constructor :entries_provider, :suffix
+
+        def found?
+          value.present?
+        end
+
+        def value
+          ::Avm::Entries::UriBuilder.from_all_fields do |field_name|
+            entries_provider.entry([suffix, field_name]).value
+          end.to_uri.to_s
+        end
+      end
+    end
+  end
+end
