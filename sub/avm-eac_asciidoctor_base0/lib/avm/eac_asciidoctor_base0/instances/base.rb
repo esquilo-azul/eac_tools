e994ccb42bffdd28ee147ce3abb40ab148280469
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-09 11:38:36 -0300

    Avm::EacAsciidoctorBase0::Instances::Base: adapt to "avm-eac_webapp_base0" '0.4.0'.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'avm/eac_webapp_base0/instance'
+require 'avm/eac_webapp_base0/instances/base'
 
 module Avm
   module EacAsciidoctorBase0
     module Instances
-      class Base < ::Avm::EacWebappBase0::Instance
+      class Base < ::Avm::EacWebappBase0::Instances::Base
         FILES_UNITS = {}.freeze
       end
     end
