88caaea6d797b0a9ffb32901db5ae9058370886e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-11 19:23:18 -0300

    Avm::EacAsciidoctorBase0::Instances::Base: move from Avm::EacAsciidoctorBase0::Instance.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
@@ -4,8 +4,10 @@ require 'avm/eac_webapp_base0/instance'
 
 module Avm
   module EacAsciidoctorBase0
-    class Instance < ::Avm::EacWebappBase0::Instance
-      FILES_UNITS = {}.freeze
+    module Instances
+      class Base < ::Avm::EacWebappBase0::Instance
+        FILES_UNITS = {}.freeze
+      end
     end
   end
 end
