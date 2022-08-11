796f6301447eafaf89f5a6656a87f249c2c726de
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-10 22:46:51 -0300

    Avm::Tools::Runner::Instance.instance: fix.

--- sub/avm-tools/lib/avm/tools/runner/instance.rb
+++ sub/avm-tools/lib/avm/tools/runner/instance.rb
@@ -22,7 +22,9 @@ module Avm
         private
 
         def instance_uncached
-          ::Avm::Registry.instances.detect(parsed.instance_id)
+          parsed.instance_id.if_present do |v|
+            ::Avm::Registry.instances.detect(v)
+          end
         end
       end
     end
