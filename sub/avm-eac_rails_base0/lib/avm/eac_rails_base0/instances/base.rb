da7da34cbe7b5b947b5106f841779377537072d6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-11 19:55:39 -0300

    Avm::EacRailsBase0::Instances::Base: adapt to "avm-eac_rails_base1" '0.4.0'.

--- sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/instances/base.rb
+++ sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/instances/base.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'avm/eac_rails_base1/instance'
+require 'avm/eac_rails_base1/instances/base'
 
 module Avm
   module EacRailsBase0
     module Instances
-      class Base < ::Avm::EacRailsBase1::Instance
+      class Base < ::Avm::EacRailsBase1::Instances::Base
         FILES_UNITS = { uploads: 'public/uploads' }.freeze
       end
     end
