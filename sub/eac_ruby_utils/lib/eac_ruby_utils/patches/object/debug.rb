ce504f6f48c229a33adb5ad9afeccf5a42c7e9bb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-13 23:46:57 -0300

    Object.print_debug_title.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/patches/object/debug.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/patches/object/debug.rb
@@ -1,16 +1,31 @@
 # frozen_string_literal: true
 
 class Object
-  def pretty_debug
+  def pretty_debug(options = {})
+    print_debug_title(options)
+
     STDERR.write(pretty_inspect)
 
     self
   end
 
-  def print_debug
+  def print_debug(options = {})
+    print_debug_title(options)
     STDERR.write(to_debug + "\n")
 
     self
+  end
+
+  def print_debug_title(title)
+    if title.is_a?(::Hash)
+      return unless title[:title]
+
+      title = title[:title]
+    end
+    char = '='
+    STDERR.write(char * (4 + title.length) + "\n")
+    STDERR.write("#{char} #{title} #{char}\n")
+    STDERR.write(char * (4 + title.length) + "\n")
   end
 
   def to_debug
