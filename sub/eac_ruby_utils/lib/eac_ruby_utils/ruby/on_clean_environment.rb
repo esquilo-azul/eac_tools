d2b9ba42a9e4cfbbb91a63823ffd048cfd65323c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-17 22:22:16 -0300

    EacRubyUtils::Ruby::OnCleanEnvironment: extract from EacRubyUtils::Ruby.on_clean_environment.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/ruby/on_clean_environment.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/ruby/on_clean_environment.rb
@@ -6,32 +6,44 @@ module EacRubyUtils
   module Ruby
     class << self
       # Executes a block in an environment when the variables BUNDLE* and RUBY* are removed.
-      def on_clean_environment
-        bundler_with_unbundled_env do
-          on_clean_envvars('BUNDLE', 'RUBY') { yield }
+      def on_clean_environment(&block)
+        OnCleanEnvironment.new(&block).perform
+      end
+
+      class OnCleanEnvironment
+        attr_reader :block
+
+        def initialize(&block)
+          @block = block
         end
-      end
 
-      private
-
-      def bundler_with_unbundled_env(&block)
-        if ::Bundler.respond_to?(:with_unbundled_env)
-          ::Bundler.with_unbundled_env(&block)
-        else
-          ::Bundler.with_clean_env(&block)
+        def perform
+          bundler_with_unbundled_env do
+            on_clean_envvars('BUNDLE', 'RUBY')
+          end
         end
-      end
 
-      def on_clean_envvars(*start_with_vars)
-        old_values = envvars_starting_with(start_with_vars)
-        old_values.each_key { |k| ENV.delete(k) }
-        yield
-      ensure
-        old_values&.each { |k, v| ENV[k] = v }
-      end
+        private
 
-      def envvars_starting_with(start_with_vars)
-        ENV.select { |k, _v| start_with_vars.any? { |var| k.start_with?(var) } }
+        def bundler_with_unbundled_env(&block)
+          if ::Bundler.respond_to?(:with_unbundled_env)
+            ::Bundler.with_unbundled_env(&block)
+          else
+            ::Bundler.with_clean_env(&block)
+          end
+        end
+
+        def on_clean_envvars(*start_with_vars)
+          old_values = envvars_starting_with(start_with_vars)
+          old_values.each_key { |k| ENV.delete(k) }
+          block.call
+        ensure
+          old_values&.each { |k, v| ENV[k] = v }
+        end
+
+        def envvars_starting_with(start_with_vars)
+          ENV.select { |k, _v| start_with_vars.any? { |var| k.start_with?(var) } }
+        end
       end
     end
   end
