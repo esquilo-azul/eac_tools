ae650d66dcf3a9e8a81dcafb8226643bf9581b04
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-06 15:07:11 -0300

    Avm::EacAsciidoctorBase0::Sources::Base.valid?: fix.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/sources/base.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/sources/base.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 require 'avm/eac_webapp_base0/sources/base'
+require 'avm/eac_asciidoctor_base0/sources/runners'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module EacAsciidoctorBase0
     module Sources
       class Base < ::Avm::EacWebappBase0::Sources::Base
+        MAIN_FILE_SUBPATH = ::Pathname.new('index.adoc')
+
         def valid?
-          false
+          path.join(MAIN_FILE_SUBPATH).file?
         end
       end
     end
