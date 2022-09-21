f1ee68364677f619a8b074d674bc23cf7587e03c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-20 22:01:35 -0300

    Class.enable_static_method_class.

---
+++ sub/eac_ruby_utils/spec/lib/eac_ruby_utils/patches/class/static_method_class_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/method_class'
+require 'eac_ruby_utils/patches/class/static_method_class'
+
+RSpec.describe ::Class do
+  let(:stub_method_class) do
+    described_class.new do
+      def self.name
+        'StubMethodClass'
+      end
+
+      enable_static_method_class
+    end
+  end
+
+  describe '#enable_static_method_class' do
+    it { expect(stub_method_class.included_modules).to include(::EacRubyUtils::StaticMethodClass) }
+  end
+end
