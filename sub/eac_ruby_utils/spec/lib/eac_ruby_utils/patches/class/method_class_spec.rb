212972dcc33539fe6cf815b5cc7a084eb086783a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-20 22:01:35 -0300

    Class.enable_method_class: move from Module.enable_method_class.

--- sub/eac_ruby_utils/spec/lib/eac_ruby_utils/patches/class/method_class_spec.rb
+++ sub/eac_ruby_utils/spec/lib/eac_ruby_utils/patches/class/method_class_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/method_class'
-require 'eac_ruby_utils/patches/module/method_class'
+require 'eac_ruby_utils/patches/class/method_class'
 
 RSpec.describe ::Class do
   let(:stub_method_class) do
