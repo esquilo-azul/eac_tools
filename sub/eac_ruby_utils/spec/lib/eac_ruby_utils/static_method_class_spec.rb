ebf6e4474098d90eef740d43937c6d78a98bb27b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-20 21:36:47 -0300

    EacRubyUtils::StaticMethodClass.

---
+++ sub/eac_ruby_utils/spec/lib/eac_ruby_utils/static_method_class_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/static_method_class'
+
+::RSpec.describe ::EacRubyUtils::StaticMethodClass do
+  let(:sender_class) do
+    ::Class.new do
+      class << self
+        def sender_value
+          'AAA'
+        end
+      end
+    end
+  end
+
+  let(:method_class) do
+    the_described_class = described_class
+    ::Class.new do
+      def self.name
+        'TheSender::PerformX'
+      end
+
+      include the_described_class
+      attr_accessor :sender, :method_param
+
+      def initialize(sender, method_param)
+        self.sender = sender
+        self.method_param = method_param
+      end
+
+      def result
+        "#{sender.sender_value},#{method_param}"
+      end
+    end
+  end
+
+  before do
+    ::Object.const_set('TheSender', sender_class)
+    sender_class.const_set('PerformX', method_class)
+  end
+
+  it { expect(sender_class).to respond_to(:perform_x) }
+  it { expect(sender_class.perform_x('BBB')).to eq('AAA,BBB') }
+end
