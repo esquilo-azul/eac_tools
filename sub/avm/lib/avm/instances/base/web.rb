235197f1218ef22d1b543955b8105413b3c661ac
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-22 00:08:55 -0300

    Avm::Instances::Base::Web: refact with .uri_components_entries_values.

--- sub/avm/lib/avm/instances/base/web.rb
+++ sub/avm/lib/avm/instances/base/web.rb
@@ -7,36 +7,8 @@ module Avm
   module Instances
     class Base
       module Web
-        def auto_web_authority
-          web_url_as_uri(&:authority)
-        end
-
-        def auto_web_hostname
-          web_url_as_uri(&:host)
-        end
-
-        def auto_web_path
-          web_url_as_uri(&:path)
-        end
-
-        def auto_web_port
-          web_url_as_uri(&:port)
-        end
-
-        def auto_web_scheme
-          web_url_as_uri(&:scheme)
-        end
-
-        def auto_web_userinfo
-          web_url_as_uri(&:userinfo)
-        end
-
-        private
-
-        def web_url_as_uri
-          read_entry_optional(::Avm::Instances::EntryKeys::WEB_URL).if_present do |v|
-            yield(::Addressable::URI.parse(v))
-          end
+        common_concern do
+          uri_components_entries_values 'web'
         end
       end
     end
