3ef7b2903ae49774873ce834099a0897382999a2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-07 23:39:23 -0300

    Avm::Git::Vendor::Github: move from "avm-git":Avm::Launcher::Vendor::Github.

---
+++ sub/avm-git/spec/lib/avm/git/vendor/github_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'avm/git/vendor/github'
+
+RSpec.describe ::Avm::Git::Vendor::Github do
+  describe '#to_ssh_url' do
+    SSH_URL = 'git@github.com:esquilo-azul/eac_launcher.git' # rubocop:disable RSpec/LeakyConstantDeclaration
+    NO_SSH_URL = 'https://otherhost.com/esquilo-azul/eac_launcher' # rubocop:disable RSpec/LeakyConstantDeclaration
+
+    {
+      nil => nil,
+      '   ' => nil,
+      'https://github.com/esquilo-azul/eac_launcher' => SSH_URL,
+      'https://github.com/esquilo-azul/eac_launcher.git' => SSH_URL,
+      SSH_URL => SSH_URL,
+      NO_SSH_URL => NO_SSH_URL
+    }.each do |input, expected|
+      it "converts \"#{input}\" to \"#{expected}\"" do
+        expect(described_class.to_ssh_url(input)).to eq(expected)
+      end
+    end
+  end
+end
