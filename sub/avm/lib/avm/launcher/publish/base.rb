8e2b6333cd648f67d785ab69ec6d5826b2e0c6d1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-07 23:27:03 -0300

    Avm::Launcher::Publish::Base.initialize: refact with .common_constructor.

--- sub/avm/lib/avm/launcher/publish/base.rb
+++ sub/avm/lib/avm/launcher/publish/base.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 require 'avm/launcher/errors/base'
+require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Launcher
     module Publish
       class Base
-        attr_reader :instance
-
-        def initialize(instance)
-          @instance = instance
-        end
+        common_constructor :instance
 
         def run
           s = check
