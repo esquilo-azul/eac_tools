b8e470c753d258fff77cc63559da7867047c8f87
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-09 20:39:12 -0300

    Avm::Launcher::Instances: move to "avm".

--- sub/avm-tools/lib/avm/launcher/instances.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/launcher/errors/base'
-require 'avm/launcher/instances/base'
-require 'avm/launcher/instances/runner_helper'
-require 'avm/launcher/instances/settings'
