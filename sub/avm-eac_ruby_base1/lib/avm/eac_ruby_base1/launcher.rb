507554f834cd11fe662e43bf725b9165306574b5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-10 01:08:26 -0300

    Avm::EacRubyBase1::Launcher::*: move from "avm-tools":Avm::Launcher::Ruby::*.

---
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+require 'avm/eac_ruby_base1/launcher/gem'
---
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher/gem.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'avm/eac_ruby_base1/launcher/gem/build'
+require 'avm/eac_ruby_base1/launcher/gem/specification'
