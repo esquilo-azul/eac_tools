cb72d4e2c1af14a97dcb2bcdaab2670373295280
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-10 01:59:58 -0300

    Avm::Projects::Stereotypes.list: remove unused.

--- sub/avm-tools/lib/avm/projects/stereotypes.rb
+++ sub/avm-tools/lib/avm/projects/stereotypes.rb
@@ -1,20 +1,11 @@
 # frozen_string_literal: true
 
-require 'avm/projects/stereotype'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Projects
     module Stereotypes
       require_sub __FILE__, base: nil
-
-      class << self
-        def list
-          @list ||= constants.map { |c| const_get(c) }
-                      .select { |c| c.included_modules.include?(Avm::Projects::Stereotype) }
-                      .freeze
-        end
-      end
     end
   end
 end
