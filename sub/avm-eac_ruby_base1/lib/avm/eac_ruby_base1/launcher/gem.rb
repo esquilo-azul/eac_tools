1069becfb14ed13c2d4efd930c2b4159276826c0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-10 01:29:56 -0300

    Avm::EacRubyBase1::Launcher::Gem: refact with .refact_sub.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher/gem.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher/gem.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
-require 'avm/eac_ruby_base1/launcher/gem/build'
-require 'avm/eac_ruby_base1/launcher/gem/specification'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRubyBase1
+    module Launcher
+      module Gem
+        require_sub __FILE__
+      end
+    end
+  end
+end
