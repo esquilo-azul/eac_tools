cb7ee1e56f16242c616628fa840d3b083c2d63c1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-11 18:31:00 -0300

    Avm::Git::LauncherStereotypes::GitSubrepo: move from "avm-tools":Avm::Projects::Stereotypes::GitSubrepo.

---
+++ sub/avm-git/lib/avm/git/launcher_stereotypes/git_subrepo/publish.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/git/launcher/publish_base'
+
+module Avm
+  module Git
+    module LauncherStereotypes
+      class GitSubrepo
+        class Publish < ::Avm::Git::Launcher::PublishBase
+        end
+      end
+    end
+  end
+end
--- sub/avm-git/lib/avm/git/launcher_stereotypes/provider.rb
+++ sub/avm-git/lib/avm/git/launcher_stereotypes/provider.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require 'avm/git/launcher_stereotypes/git'
+require 'avm/git/launcher_stereotypes/git_subrepo'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Git
     module LauncherStereotypes
       class Provider
-        STEREOTYPES = [::Avm::Git::LauncherStereotypes::Git].freeze
+        STEREOTYPES = [::Avm::Git::LauncherStereotypes::Git,
+                       ::Avm::Git::LauncherStereotypes::GitSubrepo].freeze
 
         def all
           STEREOTYPES
