11f5a90951ca87274a7d525b24a84b800d7cbac9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-11 22:16:03 -0300

    RSpec.describe(Avm::EacRubyBase1::Sources::Base): test generated source.

--- sub/avm-eac_ruby_base1/spec/spec_helper.rb
+++ sub/avm-eac_ruby_base1/spec/spec_helper.rb
@@ -2,3 +2,7 @@
 
 require 'eac_ruby_utils/rspec/default_setup'
 ::EacRubyUtils::Rspec.default_setup_create(::File.expand_path('..', __dir__))
+
+require 'eac_ruby_utils/speaker'
+require 'eac_ruby_utils/rspec/stub_speaker'
+::EacRubyUtils::Speaker.context.push(::EacRubyUtils::Rspec::StubSpeaker.new)
