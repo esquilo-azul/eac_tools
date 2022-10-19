5231f21fc261ddf8c375c11988e6581bed4224d3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-19 18:37:55 -0300

    Avm::EacWebappBase0::FileFormats::Json: move from "avm-files":Avm::Files::Formatter::Formats::Json.

--- sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0/file_formats/provider.rb
+++ sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0/file_formats/provider.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
+require 'avm/eac_webapp_base0/file_formats/json'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module EacWebappBase0
     module FileFormats
       class Provider
-        ALL_NAMES = %w[].freeze
+        ALL_NAMES = %w[json].freeze
 
         def all
           @all ||= ALL_NAMES.map do |name|
