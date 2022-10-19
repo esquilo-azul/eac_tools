fe7d8996c6597eb0af769df1e206db1b8c3a65e8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-19 18:16:46 -0300

    Avm::EacGenericBase0::FileFormats::Base: move from "avm-files":Avm::Files::Formatter::Formats::GenericPlain.

---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/file_formats/base.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'avm/file_formats/base'
+
+module Avm
+  module EacGenericBase0
+    class FileFormats
+      class Base < ::Avm::FileFormats::Base
+        VALID_BASENAMES = %w[*.asm *.bat *.css.coffee *.java *.js *.rb *.s *.scss *.sql *.tex
+                             *.url *.yml *.yaml].freeze
+
+        VALID_TYPES = %w[plain x-shellscript].freeze
+
+        def internal_apply(files)
+          files.each { |file| file_apply(file) }
+        end
+
+        def file_apply(file)
+          file.write(string_apply(file.read))
+        end
+
+        def string_apply(string)
+          b = ''
+          string.each_line do |line|
+            b += "#{line.rstrip}\n"
+          end
+          "#{b.strip}\n".gsub(/\t/, '  ')
+        end
+      end
+    end
+  end
+end
