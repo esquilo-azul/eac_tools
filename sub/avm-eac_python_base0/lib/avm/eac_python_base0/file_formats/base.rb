d0fc05bb42f36d4807ff3d3b52098c2a521c8409
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-19 19:51:20 -0300

    Avm::EacPythonBase0::FileFormats::Base.

---
+++ sub/avm-eac_python_base0/lib/avm/eac_python_base0/file_formats/base.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'avm/executables'
+require 'avm/eac_generic_base0/file_formats/base'
+
+module Avm
+  module EacPythonBase0
+    module FileFormats
+      class Base < ::Avm::EacGenericBase0::FileFormats::Base
+        VALID_BASENAMES = %w[*.py].freeze
+        VALID_TYPES = ['x-python'].freeze
+
+        def internal_apply(files)
+          ::Avm::Executables.yapf.command.append(['--in-place', *files]).system!
+          super(files)
+        end
+      end
+    end
+  end
+end
