3bc9520b681df173a67bb3c144b25ae7ac61f0ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-20 10:47:15 -0300

    Avm::EacWebappBase0::Executables.

---
+++ sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0/executables.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/envs'
+
+module Avm
+  module EacWebappBase0
+    module Executables
+      class << self
+        enable_simple_cache
+
+        def env
+          ::EacRubyUtils::Envs.local
+        end
+
+        private
+
+        %w[].each do |program|
+          define_method(program.underscore + '_uncached') do
+            env.executable(program, '--version')
+          end
+        end
+      end
+    end
+  end
+end
