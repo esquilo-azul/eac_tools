7abfe5fb1090d4aa5b294a26ca2760321c647bea
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-24 23:52:02 -0300

    RSpec.describe(Avm::EacPhpBase0::FileFormats::Base): extract from "avm-tools":RSpec.describe(Avm::Tools::Runner::Files::Format).

---
+++ sub/avm-eac_php_base0/spec/lib/avm/eac_php_base0/file_formats/base_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'avm/eac_php_base0/file_formats/base'
+
+::RSpec.describe ::Avm::EacPhpBase0::FileFormats::Base do
+  include_examples 'avm_file_formats_with_fixtures', __FILE__
+end
