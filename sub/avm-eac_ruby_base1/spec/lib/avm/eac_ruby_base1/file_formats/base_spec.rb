664356c684032a62f9654982ebd9814ffc01d742
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-25 00:20:29 -0300

    RSpec.describe(Avm::EacRubyBase1::FileFormats::Base): extract from "avm-tools":RSpec.describe(Avm::Tools::Runner::Files::Format).

---
+++ sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/file_formats/base_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'avm/eac_ruby_base1/file_formats/base'
+
+::RSpec.describe ::Avm::EacRubyBase1::FileFormats::Base do
+  include_examples 'avm_file_formats_with_fixtures', __FILE__
+end
