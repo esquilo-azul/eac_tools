e86f5ba0403dc78405e3147cec66ba1e19a00243
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-24 23:20:14 -0300

    RSpec.describe(Avm::EacWebappBase0::FileFormats::Html): extract from "avm-tools":RSpec.describe(Avm::Tools::Runner::Files::Format).

---
+++ sub/avm-eac_webapp_base0/spec/lib/avm/eac_webapp_base0/file_formats/html_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'avm/eac_webapp_base0/file_formats/html'
+
+::RSpec.describe ::Avm::EacWebappBase0::FileFormats::Html do
+  include_examples 'avm_file_formats_with_fixtures', __FILE__
+end
