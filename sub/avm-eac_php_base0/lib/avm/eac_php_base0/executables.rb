68ae89c0b93c13231a1f8665285166246a5a5cc5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-24 23:54:42 -0300

    Avm::EacPhpBase0::Executables.

---
+++ sub/avm-eac_php_base0/lib/avm/eac_php_base0/executables.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/envs'
+
+module Avm
+  module EacPhpBase0
+    module Executables
+      class << self
+        enable_simple_cache
+
+        private
+
+        %w[].each do |program|
+          define_method(program.underscore + '_uncached') do
+            ::EacRubyUtils::Envs.local.executable(program, '--version')
+          end
+        end
+      end
+    end
+  end
+end
