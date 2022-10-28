a4d009f64f55a29a6c4b69ee795894f05792d187
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-28 18:26:07 -0300

    Avm::Rspec::Setup::SourceGenerator.avm_source: add option :target_basename.

--- sub/avm/lib/avm/rspec/setup/source_generator.rb
+++ sub/avm/lib/avm/rspec/setup/source_generator.rb
@@ -6,10 +6,13 @@ module Avm
   module Rspec
     module Setup
       module SourceGenerator
+        DEFAULT_TARGET_BASENAME = 'generated_app'
+
         # @return [Avm::Sources::Base]
         def avm_source(stereotype_name, options = {})
           options = options.dup
-          target_path ||= options.delete(:target_path) || temp_dir.join('generated_app')
+          target_basename ||= options.delete(:target_basename) || DEFAULT_TARGET_BASENAME
+          target_path ||= options.delete(:target_path) || temp_dir.join(target_basename)
           ::Avm::Registry.source_generators.detect(stereotype_name, target_path, options).perform
           ::Avm::Registry.sources.detect(target_path)
         end
