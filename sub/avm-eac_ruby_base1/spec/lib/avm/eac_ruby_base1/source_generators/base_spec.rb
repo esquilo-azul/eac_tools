4ac2b25d4853d05dcec48a83027eda561a4919d8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-28 18:42:20 -0300

    RSpec.describe(Avm::EacRubyBase1::SourceGenerators::Base): refact with shared example "avm_source_generated".

--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/source_generators/base_spec.rb
+++ sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/source_generators/base_spec.rb
@@ -4,54 +4,15 @@ require 'avm/eac_ruby_base1/source_generators/base'
 require 'avm/source_generators/runner'
 
 RSpec.describe ::Avm::EacRubyBase1::SourceGenerators::Base do
-  let(:targets_root) { ::Pathname.new('base_spec_files').expand_path(__dir__) }
+  include_examples 'avm_source_generated', __FILE__, 'EacRubyBase1',
+                   {
+                     'eac-ruby-utils-version' => '0.35.0',
+                     'eac-ruby-gem-support-version' => '0.2.0'
+                   }
 
-  %w[mygem dashed-mygem].each do |gem_name|
-    context "when runner is executed for #{gem_name}" do
-      let(:stereotype) { 'EacRubyBase1' }
-      let(:target_dir) { targets_root.join(gem_name) }
-      let(:options) do
-        {
-          'eac-ruby-utils-version' => '0.35.0',
-          'eac-ruby-gem-support-version' => '0.2.0'
-        }
-      end
-      let(:source) do
-        avm_source(stereotype, options.merge(target_basename: gem_name))
-      end
+  alias_method :fs_comparator_super, :fs_comparator
 
-      it do
-        expect(directory_to_h(source.path)).to eq(directory_to_h(target_dir))
-      end
-    end
-  end
-
-  def directory_to_h(dir)
-    dir.children.map do |child|
-      [fs_object_basename(child), fs_object_to_h(child)]
-    end.to_h
-  end
-
-  # @return [String]
-  def fs_object_basename(obj)
-    r = obj.basename.to_path
-    r = '.gitignore' if r == '_gitignore'
-    r
-  end
-
-  def fs_object_to_h(obj)
-    if obj.file?
-      file_to_h(obj)
-    elsif obj.directory?
-      directory_to_h(obj)
-    else
-      raise "Unknown filesystem object \"#{obj}\""
-    end
-  end
-
-  def file_to_h(file)
-    return "TRUNCATED\n" if file.basename.to_path == 'Gemfile.lock'
-
-    file.read
+  def fs_comparator
+    fs_comparator_super.truncate_file('Gemfile.lock')
   end
 end
