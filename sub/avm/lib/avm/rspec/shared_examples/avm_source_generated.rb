e9609e40cbb15ed59fd43696153fb8611eef239c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-28 18:41:25 -0300

    RSpec.shared_examples('avm_source_generated').

---
+++ sub/avm/lib/avm/rspec/shared_examples/avm_source_generated.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'eac_fs/comparator'
+require 'eac_ruby_utils/core_ext'
+
+::RSpec.shared_examples 'avm_source_generated' do |spec_file, stereotype, options = {}|
+  fixtures_dir = ::Pathname.new('base_spec_files').expand_path(::File.dirname(spec_file))
+
+  fixtures_dir.children.select(&:directory?).each do |target_dir|
+    context "when target is \"#{target_dir.basename}\"" do
+      let(:source) { avm_source(stereotype, options.merge(target_basename: target_dir.basename)) }
+
+      it do
+        expect(fs_comparator.build(source.path)).to eq(fs_comparator.build(target_dir))
+      end
+    end
+  end
+
+  # @return [EacFs::Comparato]
+  def fs_comparator
+    ::EacFs::Comparator.new.rename_file('.gitignore', '_gitignore')
+  end
+end
