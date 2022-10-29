bac612c71ff55cadc6247bd9ba4b23fec9160e63
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-29 15:55:37 -0300

    RSpec.describe(Avm::EacAsciidoctorBase0::Sources::Build).

---
+++ sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/sources/build_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'avm/eac_asciidoctor_base0/sources/base'
+require 'avm/eac_asciidoctor_base0/sources/build'
+require 'eac_fs/comparator'
+
+::RSpec.describe ::Avm::EacAsciidoctorBase0::Sources::Build do
+  let(:fixtures_dir) { __dir__.to_pathname.join('build_spec_files') }
+  let(:fs_comparator) { ::EacFs::Comparator.new.truncate_file('*.html') }
+  let(:instance) { described_class.new(source) }
+  let(:source) { avm_source('EacAsciidoctorBase0') }
+  let(:target_dir) { fixtures_dir }
+  let(:main_document) do
+    source.path.join(::Avm::EacAsciidoctorBase0::Sources::Base::MAIN_FILE_SUBPATH)
+  end
+
+  before do
+    %w[doc1 doc2 doc2_1].each do |basename|
+      ::FileUtils.cp(main_document, source.path.join("#{basename}.adoc"))
+    end
+    instance.run
+  end
+
+  it do
+    expect(fs_comparator.build(instance.target_directory)).to eq(fs_comparator.build(target_dir))
+  end
+end
