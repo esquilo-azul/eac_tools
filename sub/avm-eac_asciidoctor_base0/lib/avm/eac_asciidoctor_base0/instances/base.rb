2d98d5276e5c93df4e7e70fe4c99728b64518fee
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-31 18:24:47 -0300

    Avm::EacAsciidoctorBase0::Instances::Base.author_*.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/base.rb
@@ -10,6 +10,24 @@ module Avm
     module Instances
       class Base < ::Avm::EacWebappBase0::Instances::Base
         FILES_UNITS = {}.freeze
+        AUTHOR_EMAIL_ENTRY_PATH = 'install.email'
+        AUTHOR_NAME_ENTRY_PATH = 'install.name'
+        AUTHOR_NAME_INITIALS_ENTRY_PATH = 'install.name_initials'
+
+        # @return [String]
+        def author_email
+          entry(AUTHOR_EMAIL_ENTRY_PATH).value!
+        end
+
+        # @return [String]
+        def author_name
+          entry(AUTHOR_NAME_ENTRY_PATH).value!
+        end
+
+        # @return [String]
+        def author_name_initials
+          entry(AUTHOR_NAME_INITIALS_ENTRY_PATH).value!
+        end
       end
     end
   end
