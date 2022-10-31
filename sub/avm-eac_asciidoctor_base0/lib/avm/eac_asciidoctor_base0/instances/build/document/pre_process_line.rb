483f59a2465dcf5a248259b8909a42e3e551d0f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-31 16:52:40 -0300

    Avm::EacAsciidoctorBase0::Instances::Build::Document::PreProcessLine.macro_*.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/build/document/pre_process_line.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/build/document/pre_process_line.rb
@@ -9,10 +9,38 @@ module Avm
         class Document
           class PreProcessLine
             enable_method_class
+            enable_simple_cache
+
+            MACRO_PARSER = %r{\A\s*//\#(\S+.*)}.to_parser do |m|
+              m[1]
+            end
+
             common_constructor :document, :line
 
             def result
-              line
+              if macro?
+                macro_value
+              else
+                line
+              end
+            end
+
+            def macro?
+              macro_name.present?
+            end
+
+            def macro_parser
+              MACRO_PARSER
+            end
+
+            def macro_value
+              document.send("#{macro_name}_macro_value")
+            end
+
+            private
+
+            def macro_name_uncached
+              macro_parser.parse(line)
             end
           end
         end
