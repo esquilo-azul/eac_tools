6c51a82df15848312b7956e2c29edeb0986b0d90
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-10 18:43:04 -0300

    EacCli::Config::Entry::Undefined.undefined_value_no_loop: rescue EacCli::Speaker::InputBlocked.

--- sub/eac_cli/lib/eac_cli/config/entry/undefined.rb
+++ sub/eac_cli/lib/eac_cli/config/entry/undefined.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'eac_cli/speaker/input_requested'
+require 'eac_config/entry/not_found_error'
 require 'eac_config/node_entry'
 require 'eac_ruby_utils/core_ext'
 
@@ -20,6 +22,8 @@ module EacCli
 
         def undefined_value_no_loop
           input("Value for entry \"#{path}\"", options.request_input_options)
+        rescue ::EacCli::Speaker::InputRequested
+          raise ::EacConfig::Entry::NotFoundError, self
         end
       end
     end
