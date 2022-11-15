103bec78a30dfe8a121113ff2857e9b403b7b9ea
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-15 20:48:42 -0300

    Avm::EacRedminePluginBase0::Sources::Base::Init.init_file.

--- sub/avm-eac_redmine_plugin_base0/lib/avm/eac_redmine_plugin_base0/sources/base/init.rb
+++ sub/avm-eac_redmine_plugin_base0/lib/avm/eac_redmine_plugin_base0/sources/base/init.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/eac_redmine_plugin_base0/sources/init_file'
 require 'avm/eac_ruby_base1/sources/base'
 require 'eac_ruby_utils/core_ext'
 
@@ -9,6 +10,11 @@ module Avm
       class Base < ::Avm::EacRubyBase1::Sources::Base
         module Init
           INIT_SUBPATH = 'init.rb'
+
+          # @return [Avm::EacRedminePluginBase0::Sources::InitFile]
+          def init_file
+            ::Avm::EacRedminePluginBase0::Sources::InitFile.new(init_path)
+          end
 
           # @return [String]
           def init_path
