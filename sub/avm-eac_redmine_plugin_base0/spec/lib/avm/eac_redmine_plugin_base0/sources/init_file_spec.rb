fcc54bf0a0a5529e92a8d1d88e9ce77fa2adceb3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-15 20:36:30 -0300

    Avm::EacRedminePluginBase0::Sources::InitFile.

---
+++ sub/avm-eac_redmine_plugin_base0/spec/lib/avm/eac_redmine_plugin_base0/sources/init_file_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/eac_redmine_plugin_base0/sources/init_file'
+
+::RSpec.describe ::Avm::EacRedminePluginBase0::Sources::InitFile do
+  let(:stubs_dir) { ::Pathname.new(__dir__).join('init_file_spec_files') }
+  let(:stub_file) { stubs_dir.join('a_init_file.rb') }
+  let(:instance) { described_class.new(stub_file) }
+  let(:target_version) { ::Gem::Version.new('0.16.0') }
+
+  describe '#version' do
+    it { expect(instance.version).to eq(target_version) }
+  end
+end
