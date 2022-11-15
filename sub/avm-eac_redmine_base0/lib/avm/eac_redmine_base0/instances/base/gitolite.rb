b60582a026b39681bdeb1ae00f83e6b37ff17f54
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-14 23:30:36 -0300

    Avm::EacRedmineBase0::Instances::Base::Gitolite.gitolite_data_unit.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/base/gitolite.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/base/gitolite.rb
@@ -18,6 +18,12 @@ module Avm
             uri_components_entries_values GITOLITE_ENTRY_KEYS_PREFIX
           end
 
+          # @return [Avm::Instances::Data::FilesUnit]
+          def gitolite_data_unit
+            ::Avm::Instances::Data::FilesUnit.new(self, gitolite_repositories_path,
+                                                  sudo_user: entry('gitolite.username').value!)
+          end
+
           def gitolite_hostname_default_value
             DEFAULT_HOSTNAME
           end
@@ -26,6 +32,11 @@ module Avm
             read_entry_optional([GITOLITE_ENTRY_KEYS_PREFIX, 'username']).if_present do |v|
               ::File.join(DEFAULT_PATH_PARENT, v)
             end
+          end
+
+          # @return [Pathname]
+          def gitolite_repositories_path
+            entry('gitolite.path').value!.to_pathname.join('repositories')
           end
 
           def gitolite_scheme_default_value
