a64ef1213aa4b4785ece956a4befdfe2a3ef6a29
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-15 19:48:43 -0300

    Avm::Data::Package: include Avm::Data::Callbacks.

--- sub/avm/lib/avm/data/package.rb
+++ sub/avm/lib/avm/data/package.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require 'avm/data/callbacks'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Data
     class Package
       require_sub __FILE__
+      include ::Avm::Data::Callbacks
 
       def initialize(options)
         options = options.to_options_consumer
@@ -29,11 +31,15 @@ module Avm
       end
 
       def dump_units_to_directory(directory)
-        units.each { |identifier, unit| unit.dump_to_directory(directory, identifier) }
+        run_callbacks :dump do
+          units.each { |identifier, unit| unit.dump_to_directory(directory, identifier) }
+        end
       end
 
       def load_units_from_directory(directory)
-        units.each { |identifier, unit| unit.load_from_directory(directory, identifier) }
+        run_callbacks :load do
+          units.each { |identifier, unit| unit.load_from_directory(directory, identifier) }
+        end
       end
 
       def units
