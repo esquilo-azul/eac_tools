1b75894d77123eefd192bc9be87ba77f3178553e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-14 22:56:15 -0300

    Avm::Instances::Data: refact.

--- sub/avm/lib/avm/instances/data.rb
+++ sub/avm/lib/avm/instances/data.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/require_sub'
-::EacRubyUtils.require_sub(__FILE__)
+require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Instances
     module Data
+      require_sub __FILE__
     end
   end
 end
