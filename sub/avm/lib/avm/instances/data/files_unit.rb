387563ffc8d4f15286f6bf27df19c0a76a87faad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-14 23:59:24 -0300

    Avm::Instances::Data::FilesUnit.instance_command: extract from .*_command.

--- sub/avm/lib/avm/instances/data/files_unit.rb
+++ sub/avm/lib/avm/instances/data/files_unit.rb
@@ -18,17 +18,22 @@ module Avm
         end
 
         def dump_command
-          instance.host_env.command('tar', '-czf', '-', '-C', files_path, '.')
+          instance_command('tar', '-czf', '-', '-C', files_path, '.')
         end
 
         def load_command
-          instance.host_env.command('tar', '-xzf', '-', '-C', files_path)
+          instance_command('tar', '-xzf', '-', '-C', files_path)
         end
 
         def clear_files
           infom "Removing all files under #{files_path}..."
-          instance.host_env.command('mkdir', '-p', files_path).execute!
-          instance.host_env.command('find', files_path, '-mindepth', 1, '-delete').execute!
+          instance_command('mkdir', '-p', files_path).execute!
+          instance_command('find', files_path, '-mindepth', 1, '-delete').execute!
+        end
+
+        # @return [EacRubyUtils::Envs::Command]
+        def instance_command(*args)
+          instance.host_env.command(*args)
         end
       end
     end
