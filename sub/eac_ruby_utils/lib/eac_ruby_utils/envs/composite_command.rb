f72b91408b348c77f2bf67dd0a022f58ce7d72d3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-19 22:33:50 -0300

    EacRubyUtils::Envs::CompositeCommand.

---
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/envs/composite_command.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/envs/base_command'
+
+module EacRubyUtils
+  module Envs
+    class CompositeCommand
+      include ::EacRubyUtils::Envs::BaseCommand
+
+      enable_listable
+      lists.add_string :operator, '&&' => :and, ';' => :before, '||' => :or, '|' => :pipe
+
+      common_constructor :operator, :left_command, :right_command do
+        self.operator = self.class.lists.operator.value_validate!(operator.to_s)
+      end
+
+      # @return [EacRubyUtils::Envs::BaseEnv]
+      delegate :env, to: :left_command
+
+      # @return [String]
+      def command_line_without_env(_options = {})
+        ::Shellwords.join(['bash', '-c', bash_command])
+      end
+
+      # @return [String]
+      def bash_command
+        ['set', '-euo', 'pipefail', OPERATOR_BEFORE, left_command.command, operator,
+         right_command.command].join(' ')
+      end
+    end
+  end
+end
