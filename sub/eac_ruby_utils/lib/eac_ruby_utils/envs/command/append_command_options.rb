56bd2c0c024b4df6a4e3cb21c3f3a7b686c2830a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-21 22:41:45 -0300

    EacRubyUtils::Envs::Command::AppendCommandOptions.output_file: extract from .result.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/envs/command/append_command_options.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/envs/command/append_command_options.rb
@@ -10,11 +10,11 @@ module EacRubyUtils
         enable_method_class
         common_constructor :command, :command_line, :options
 
-        def result # rubocop:disable Metrics/AbcSize
+        def result
           r = command_line
           r = input.command + ' | ' + r if input
           r = "cat #{Shellwords.escape(input_file)} | #{r}" if input_file
-          r += ' > ' + Shellwords.escape(options[:output_file]) if options[:output_file]
+          r += ' > ' + Shellwords.escape(output_file) if output_file
           r
         end
 
@@ -26,6 +26,11 @@ module EacRubyUtils
         # @return [Pathname,  nil]
         def input_file
           options[:input_file].if_present(&:to_pathname)
+        end
+
+        # @return [Pathname, nil]
+        def output_file
+          options[:output_file].if_present(&:to_pathname)
         end
       end
     end
