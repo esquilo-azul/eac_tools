8373e75422fb4e5c29ad91224ef86b3e53a3f273
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-11-22 15:37:08 -0300

    Avm::EacPhpBase0::Instances::Base.

---
+++ sub/avm-eac_php_base0/lib/avm/eac_php_base0/instances/base.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/eac_webapp_base0/instances/base'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacPhpBase0
+    module Instances
+      class Base < ::Avm::EacWebappBase0::Instances::Base
+        FILES_UNITS = {}.freeze
+      end
+    end
+  end
+end
