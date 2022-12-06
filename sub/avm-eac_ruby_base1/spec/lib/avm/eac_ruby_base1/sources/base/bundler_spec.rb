374c21ab00f504f355a265584e8dbb6b4727f0bd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-06 18:00:35 -0300

    RSpec.describe(Avm::EacRubyBase1::Sources::Base::Bundler): fix.

--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/sources/base/bundler_spec.rb
+++ sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/sources/base/bundler_spec.rb
@@ -6,6 +6,10 @@ require 'avm/eac_ruby_base1/sources/base'
   let(:mygem_path) { ::Pathname.new(__dir__).join('bundler_spec_files', 'mygem') }
   let(:mygem) { described_class.new(mygem_path) }
 
+  before do
+    mygem.bundle.execute!
+  end
+
   describe '#bundle' do
     specify do
       expect(mygem.bundle('exec', 'myrunner').execute!).to include('My Runner')
