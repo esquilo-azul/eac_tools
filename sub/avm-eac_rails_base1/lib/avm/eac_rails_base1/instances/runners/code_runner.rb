afc1cfa25e0588045a9b44bf8c994209bf757dd6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-08 14:28:00 -0300

    Avm::EacRailsBase1::Instances::Runners::CodeRunner: move from Avm::EacRailsBase1::Runner::CodeRunner.

---
+++ sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/instances/runners/code_runner.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'avm/eac_rails_base1/runner_with/bundle'
+require 'eac_cli/core_ext'
+
+module Avm
+  module EacRailsBase1
+    module Instances
+      module Runners
+        class CodeRunner
+          runner_with :help, ::Avm::EacRailsBase1::RunnerWith::Bundle do
+            desc 'Runs a Ruby code with "rails runner".'
+            pos_arg :code
+          end
+
+          def run
+            infov 'Environment', runner_context.call(:instance).host_env
+            bundle_run
+          end
+
+          def bundle_args
+            %w[exec rails runner] + [parsed.code]
+          end
+        end
+      end
+    end
+  end
+end
