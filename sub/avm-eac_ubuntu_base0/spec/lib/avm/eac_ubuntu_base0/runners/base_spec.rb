ccacadb4f4468d3ab568d59b6b76dbf8d81626cb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-14 13:09:40 -0300

    Gem "avm-eac_ubuntu_base0" (Local): add '0.3.0'.

---
+++ sub/avm-eac_ubuntu_base0/spec/lib/avm/eac_ubuntu_base0/runners/base_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'avm/eac_ubuntu_base0/runners/base'
+
+::RSpec.describe ::Avm::EacUbuntuBase0::Runners::Base do
+  include_examples 'in_avm_registry', 'runners'
+end
