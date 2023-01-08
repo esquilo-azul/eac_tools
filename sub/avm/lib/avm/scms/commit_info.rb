11fc04f4f87f6d703bb216893dfdda111f9ff8a1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-08 20:30:07 -0300

    Avm::Scms::CommitInfo.to_s.

--- sub/avm/lib/avm/scms/commit_info.rb
+++ sub/avm/lib/avm/scms/commit_info.rb
@@ -9,6 +9,12 @@ module Avm
 
       immutable_accessor :fixup, :message
       immutable_accessor :path, type: :array
+
+      def to_s
+        self.class.name.demodulize + '[' +
+          %w[fixup message].map { |m| [m, send(m)] }.reject { |m| m[1].blank? }
+            .map { |m| m.join(': ') }.join(',') + ']'
+      end
     end
   end
 end
