b419a24940133cda07e7ec16705221e4db274790
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-07 23:02:54 -0300

    Avm::Scms::AutoCommit::Rules: move from Avm::Git::AutoCommit::Rules.

---
+++ sub/avm/lib/avm/scms/auto_commit/rules/new.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'avm/scms/auto_commit/rules/base'
+require 'avm/scms/auto_commit/file_resource_name'
+
+module Avm
+  module Scms
+    module AutoCommit
+      module Rules
+        class New < ::Avm::Scms::AutoCommit::Rules::Base
+          class WithFile < ::Avm::Scms::AutoCommit::Rules::Base::WithFile
+            def auto_commit_path
+              ::Avm::Scms::AutoCommit::FileResourceName.new(file.git, file.path)
+            end
+
+            def commit_info
+              new_commit_info.message(auto_commit_path.commit_message)
+            end
+          end
+        end
+      end
+    end
+  end
+end
