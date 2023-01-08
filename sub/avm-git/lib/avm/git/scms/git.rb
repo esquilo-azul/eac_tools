6963266adf0a1495e2bde5c5b0db78fae29ad45f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-08 12:57:46 -0300

    Avm::Git::Scms::Git::Commits: extract from Avm::Git::Scms::Git::Commits.

--- sub/avm-git/lib/avm/git/scms/git.rb
+++ sub/avm-git/lib/avm/git/scms/git.rb
@@ -17,44 +17,8 @@ module Avm
           git_repo <=> other.git_repo
         end
 
-        # @return [Avm::Git::Scms::Git::Commit,nil]
-        def commit(source)
-          if source.is_a?(::Avm::Git::Scms::Git::Commit)
-            return source if source.git_repo == self
-
-            raise 'Not same Git repository'
-          end
-          git_repo.commitize(source).if_present do |v|
-            ::Avm::Git::Scms::Git::Commit.new(self, v)
-          end
-        end
-
-        # @return [Avm::Git::Scms::Git::Commit,nil]
-        def commit_dirty(message = nil)
-          return nil unless git_repo.dirty?
-
-          git_repo.command('add', '.').execute!
-          git_repo.command('commit', '-m',
-                           message.call_if_proc.if_present(COMMIT_DIRTY_DEFAULT_MESSAGE)).execute!
-          commit(git_repo.head)
-        end
-
-        # @return [Avm::Git::Scms::Git::Commit,nil]
-        def commit_if_change(message = nil)
-          tracker = ::Avm::Git::Scms::Git::ChangeTracker.new(self, message)
-          tracker.start
-          yield
-          tracker.stop
-        end
-
         def git_repo
           @git_repo ||= ::EacGit::Local.new(path)
-        end
-
-        # @return [Avm::Git::Scms::Git::Commit]
-        def reset_and_commit(commit_to_reset, message)
-          git_repo.command('reset', '--soft', commit(commit_to_reset).git_commit.id).execute!
-          commit_dirty(message)
         end
 
         # @return [Enumerable<Avm::Git::Scms::GitSubrepo>]
---
+++ sub/avm-git/lib/avm/git/scms/git/commits.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'avm/scms/commit'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Git
+    module Scms
+      class Git < ::Avm::Scms::Base
+        module Commits
+          # @return [Avm::Git::Scms::Git::Commit,nil]
+          def commit(source)
+            if source.is_a?(::Avm::Git::Scms::Git::Commit)
+              return source if source.git_repo == self
+
+              raise 'Not same Git repository'
+            end
+            git_repo.commitize(source).if_present do |v|
+              ::Avm::Git::Scms::Git::Commit.new(self, v)
+            end
+          end
+
+          # @return [Avm::Git::Scms::Git::Commit,nil]
+          def commit_dirty(message = nil)
+            return nil unless git_repo.dirty?
+
+            git_repo.command('add', '.').execute!
+            git_repo.command(
+              'commit', '-m',
+              message.call_if_proc.if_present(COMMIT_DIRTY_DEFAULT_MESSAGE)
+            ).execute!
+            commit(git_repo.head)
+          end
+
+          # @return [Avm::Git::Scms::Git::Commit,nil]
+          def commit_if_change(message = nil)
+            tracker = ::Avm::Git::Scms::Git::ChangeTracker.new(self, message)
+            tracker.start
+            yield
+            tracker.stop
+          end
+
+          # @return [Avm::Git::Scms::Git::Commit]
+          def reset_and_commit(commit_to_reset, message)
+            git_repo.command('reset', '--soft', commit(commit_to_reset).git_commit.id).execute!
+            commit_dirty(message)
+          end
+        end
+      end
+    end
+  end
+end
