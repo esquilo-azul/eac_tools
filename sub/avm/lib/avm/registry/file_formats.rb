4248457ec41537e255142542e17ae3f623a43248
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-09 12:07:17 -0300

    Avm::Registry::FileFormats.class_detect.

--- sub/avm/lib/avm/registry/file_formats.rb
+++ sub/avm/lib/avm/registry/file_formats.rb
@@ -5,6 +5,10 @@ require 'avm/registry/from_gems'
 module Avm
   module Registry
     class FileFormats < ::Avm::Registry::FromGems
+      # @return [Avm::FileFormats::Base]
+      def class_detect(klass, detect_args)
+        klass.new if klass.new.match?(detect_args.first)
+      end
     end
   end
 end
