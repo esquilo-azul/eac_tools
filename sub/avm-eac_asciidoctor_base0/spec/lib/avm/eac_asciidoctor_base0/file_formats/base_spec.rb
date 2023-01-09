e9cf68bccb6000b7a5a0e593a1aa1f6dcb788d10
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-09 14:19:24 -0300

    Avm::EacAsciidoctorBase0::FileFormats::Base::FileResourceName.

---
+++ sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/file_formats/base_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'avm/eac_asciidoctor_base0/file_formats/base'
+
+::RSpec.describe ::Avm::EacAsciidoctorBase0::FileFormats::Base do
+  include_examples 'avm_file_format_file_resource_name', {
+    'content/title' => 'Content[/]',
+    'content/body.adoc' => 'Content[/]',
+    'content/abc/title' => 'Content[/abc]',
+    'content/abc/body.adoc' => 'Content[/abc]',
+    'content/abc/sub/body.adoc' => 'Content[/abc/sub]'
+  }
+end
