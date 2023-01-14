0e8280b7fa30762d7dfe2f1dcd372f605dafe9f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-13 23:11:08 -0300

    Avm::Scms::Null.head_commit.

--- sub/avm/lib/avm/scms/null.rb
+++ sub/avm/lib/avm/scms/null.rb
@@ -8,6 +8,11 @@ module Avm
     class Null < ::Avm::Scms::Base
       require_sub __FILE__
 
+      # @return [Avm::Scms::Null::Commit]
+      def head_commit
+        @head_commit ||= Avm::Scms::Null::Commit.new(self)
+      end
+
       def update
         # Do nothing
       end
