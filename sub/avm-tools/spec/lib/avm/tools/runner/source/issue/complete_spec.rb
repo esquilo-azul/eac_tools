722b9a7cdb1e6460560850f0ede4d2297773cf34
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-19 11:10:55 -0300

    RSpec.describe('Avm::Tools::Runner::Source::Issue::Complete'): fix.

--- sub/avm-tools/spec/lib/avm/tools/runner/source/issue/complete_spec.rb
+++ sub/avm-tools/spec/lib/avm/tools/runner/source/issue/complete_spec.rb
@@ -17,6 +17,7 @@ require 'fileutils'
       eac_local_repos.assert_remote_url(remote_name, remote_repos.root_path.to_path)
       local_repos.command('checkout', '-b', issue_ref).execute!
       local_repos.file('myfile1.txt').touch
+      local_repos.file(::Avm::Sources::Base::Configuration::CONFIGURATION_FILENAMES.first).touch
       local_repos.command('add', '.').execute!
       local_repos.command('commit', '-m', 'myfile1.txt').execute!
       local_repos.command('push', 'origin', issue_ref).execute!
