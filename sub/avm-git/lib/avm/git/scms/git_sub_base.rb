9425364598884d3ab11194ee2ff2037cc90ed492
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-03-15 00:34:49 -0300

    Avm::Git::Scms::GitSubBase.interval: return a Avm::Git::Scms::GitSubBase::Interval.

--- sub/avm-git/lib/avm/git/scms/git_sub_base.rb
+++ sub/avm-git/lib/avm/git/scms/git_sub_base.rb
@@ -9,8 +9,15 @@ module Avm
       class GitSubBase < ::Avm::Scms::Base
         enable_abstract_methods
 
-        delegate :changed_files, :commit_if_change, :current_milestone_base_commit, :interval,
+        delegate :changed_files, :commit_if_change, :current_milestone_base_commit,
                  :head_commit, :reset_and_commit, :run_commit, to: :parent_scm
+
+        # @param from [Avm::Git::Scms::Git::Commit]
+        # @param to [Avm::Git::Scms::Git::Commit]
+        # @return [Avm::Git::Scms::GitSubBase::Interval]
+        def interval(from, to)
+          ::Avm::Git::Scms::GitSubBase::Interval.new(self, from, to)
+        end
 
         require_sub __FILE__
       end
