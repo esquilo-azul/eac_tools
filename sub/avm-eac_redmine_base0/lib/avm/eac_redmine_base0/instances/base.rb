8a0f94463f6e29fd498e5674785dd4774ff54f67
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-03-31 00:17:43 -0300

    Avm::EacRedmineBase0::Instances::Base: fix modules inclusion.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/base.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/base.rb
@@ -11,7 +11,7 @@ module Avm
   module EacRedmineBase0
     module Instances
       class Base < ::Avm::EacRailsBase1::Instances::Base
-        require_sub __FILE__, include_modules: true
+        require_sub __FILE__, include_modules: true, require_dependency: true
         enable_simple_cache
 
         def docker_image_class
