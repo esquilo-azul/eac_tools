51bd02b8daf2c17a3f9a69abbdb2f23be129cb11
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-01 00:29:21 -0300

    Avm::FileFormats::Base.match: convert to "avm-eac_generic_base0":Avm::EacGenericBase0::FileFormats::Base::Match.

--- sub/avm/lib/avm/file_formats/base.rb
+++ sub/avm/lib/avm/file_formats/base.rb
@@ -25,37 +25,10 @@ module Avm
         self.class.name.demodulize
       end
 
-      def match?(file)
-        match_by_filename?(file) || match_by_type?(file)
-      end
-
-      def valid_basenames
-        constant_or_array('VALID_BASENAMES')
-      end
-
-      def valid_types
-        constant_or_array('VALID_TYPES')
-      end
-
-      private
-
-      def constant_or_array(name)
-        return [] unless self.class.const_defined?(name)
-
-        self.class.const_get(name)
-      end
-
-      def match_by_filename?(file)
-        valid_basenames.any? do |valid_basename|
-          file.basename.fnmatch?(valid_basename)
-        end
-      end
-
-      def match_by_type?(file)
-        info = ::EacFs::FileInfo.new(file)
-        return unless info.content_type.type == 'text'
-
-        valid_types.include?(info.content_type.subtype)
+      # @param file [Pathname]
+      # @return [Boolean]
+      def match?(_file)
+        raise_abstract_method __method__
       end
     end
   end
