2fe4470c7ac01c397d1e829a9af3ebd34ec80ad6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-01 15:56:58 -0300

    Avm::EacRailsBase0::Instances::ApachePath: move from Avm::EacRailsBase0::ApachePath.

---
+++ sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/instances/apache_path.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'eac_templates/core_ext'
+require 'avm/eac_webapp_base0/instances/apache_path'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRailsBase0
+    module Instances
+      class ApachePath < ::Avm::EacWebappBase0::Instances::ApachePath
+        def document_root
+          ::File.join(super, 'public')
+        end
+
+        def extra_content
+          template.child('extra_content.conf').apply(instance)
+        end
+      end
+    end
+  end
+end
--- sub/avm-eac_rails_base0/spec/lib/avm/eac_rails_base0/instances/apache_path_spec.rb
+++ sub/avm-eac_rails_base0/spec/lib/avm/eac_rails_base0/instances/apache_path_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'avm/eac_rails_base0/apache_path'
+require 'avm/eac_rails_base0/instances/apache_path'
 require 'avm/eac_rails_base0/instances/base'
 
-RSpec.describe ::Avm::EacRailsBase0::ApachePath do
+RSpec.describe ::Avm::EacRailsBase0::Instances::ApachePath do
   describe '#no_ssl_site_content' do
     let(:instance) { ::Avm::EacRailsBase0::Instances::Base.by_id('stub-app_0') }
     let(:apache_path) { described_class.new(instance) }
