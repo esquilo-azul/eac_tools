631f28482bd5aae33b3d6811e28f9ac9c9ea9662
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-04 23:17:07 -0300

    Rspec.describe(Avm::EacAsciidoctorBase0::Instances::Build): refact.

--- sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/instances/build_spec.rb
+++ sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/instances/build_spec.rb
@@ -10,14 +10,9 @@ require 'eac_fs/comparator'
     ::Avm::EacAsciidoctorBase0::Instances::Base
       .new(application, 'stub')
   end
-  let(:fixtures_dir) { __dir__.to_pathname.join('build_spec_files') }
   let(:fs_comparator) { ::EacFs::Comparator.new.truncate_file('*.html') }
   let(:instance) { described_class.new(application_instance) }
   let(:source) { avm_source('EacAsciidoctorBase0', target_basename: application.id) }
-  let(:target_dir) { fixtures_dir }
-  let(:main_document) do
-    source.path.join(::Avm::EacAsciidoctorBase0::Sources::Base::MAIN_FILE_SUBPATH)
-  end
 
   before do
     application.entry('stereotype').write('EacAsciidoctorBase0')
@@ -27,22 +22,17 @@ require 'eac_fs/comparator'
     application_instance.entry('install.name_initials').write('T.A.')
     application_instance.entry('install.email').write('theauthor@example.net')
     application_instance.entry('web.url').write('https://www.example.net')
-    %w[doc1 doc2 doc2/doc2_1].each do |subpath|
-      target_dir = source.content_directory.join(subpath)
-      target_dir.mkpath
-      ::FileUtils.cp(
-        main_document,
-        target_dir.join(::Avm::EacAsciidoctorBase0::Sources::Base::CONTENT_DOCUMENT_BASENAME)
-      )
-      ::FileUtils.cp(
-        source.root_document.title_path,
-        target_dir.join(::Avm::EacAsciidoctorBase0::Sources::Base::Document::TITLE_BASENAME)
-      )
-    end
-    instance.perform
   end
 
-  it do
-    expect(fs_comparator.build(instance.target_directory)).to eq(fs_comparator.build(target_dir))
+  include_examples 'source_target_fixtures', __FILE__ do
+    def source_data(source_file)
+      ::FileUtils.copy_entry(source_file, source.path)
+      instance.perform
+      fs_comparator.build(instance.target_directory)
+    end
+
+    def target_data(target_file)
+      fs_comparator.build(target_file.to_pathname)
+    end
   end
 end
