fdaeb939c9209361c99cc81c0afe5b000ab38a5f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-06 13:48:05 -0300

    RSpec.describe('Avm::EacAsciidoctorBase0::Instances::Build::Document'): use a document with parent and children.

--- sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/instances/build/document_spec.rb
+++ sub/avm-eac_asciidoctor_base0/spec/lib/avm/eac_asciidoctor_base0/instances/build/document_spec.rb
@@ -2,14 +2,71 @@
 
 require 'avm/eac_asciidoctor_base0/instances/build'
 
-::RSpec.describe ::Avm::EacAsciidoctorBase0::Instances::Build::Document do
+::RSpec.describe ::Avm::EacAsciidoctorBase0::Instances::Build::Document do # rubocop:disable Metrics/BlockLength
   let(:app_director) { eac_asciidoctor_base0_stubs }
   let(:build) { ::Avm::EacAsciidoctorBase0::Instances::Build.new(app_director.instance) }
   let(:fixtures_dir) { __dir__.to_pathname.join('document_spec_files') }
-  let(:instance) { build.root_document }
+  let(:instance) { build_doc_child(build.root_document, :c1, :c1_1, :c1_1_1) }
   let(:target_file) { fixtures_dir.join('pre_processed_root_body.adoc') }
+  let(:source) { app_director.source }
+  let(:doc_template) do
+    r = temp_dir
+    ::FileUtils.copy_entry(source.root_document.root_path, r)
+    r
+  end
+
+  before do
+    copy_template_hash(
+      source.root_document,
+      c1: {
+        c1_1: {
+          c1_1_1: {
+            c1_1_1_1: {},
+            c1_1_1_2: {}
+          },
+          c1_1_2: {
+            c1_1_2_1: {},
+            c1_1_2_2: {}
+          }
+        }
+      },
+      c2: {
+        c2_1: {}
+      }
+    )
+  end
 
   it do
     expect(instance.pre_processed_body_source_content).to eq(target_file.read)
+  end
+
+  # @return [Avm::EacAsciidoctorBase0::Sources::Base::Document]
+  def copy_template_doc(parent, basename)
+    parent.assert_argument(::Avm::EacAsciidoctorBase0::Sources::Base::Document, 'parent')
+
+    root_target = parent.root_path.join(basename.to_s)
+    ::FileUtils.copy_entry(doc_template, root_target)
+    root_target.join('title').write(root_target.basename.to_path)
+    ::Avm::EacAsciidoctorBase0::Sources::Base::Document.new(parent.source, parent,
+                                                            root_target.basename)
+  end
+
+  # @return [Array<Avm::EacAsciidoctorBase0::Sources::Base::Document>]
+  def copy_template_hash(parent, hash)
+    parent.assert_argument(::Avm::EacAsciidoctorBase0::Sources::Base::Document, 'parent')
+    hash.assert_argument(::Hash, 'hash')
+
+    hash.map do |k, v|
+      r = copy_template_doc(parent, k)
+      copy_template_hash(r, v)
+      r
+    end
+  end
+
+  # @return [Avm::EacAsciidoctorBase0::Instances::Build::Document]
+  def build_doc_child(build_doc, *path)
+    basename = path.shift.to_s
+    r = build_doc.child!(basename)
+    path.any? ? build_doc_child(r, *path) : r
   end
 end
