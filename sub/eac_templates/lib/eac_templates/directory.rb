3e62533102ba4025afd5d1a230b8e0c1053baeb8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-30 12:13:15 -0300

    EacTemplates::Variables::FsObject: move from Eactemplates::Directory::TemplateNode.

--- sub/eac_templates/lib/eac_templates/directory.rb
+++ sub/eac_templates/lib/eac_templates/directory.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 require 'eac_templates/file'
+require 'eac_templates/variables/fs_object'
 
 module EacTemplates
   class Directory
-    TEMPLATE_EXTNAME = '.template'
-
     attr_reader :path
 
     def initialize(path)
@@ -13,7 +12,7 @@ module EacTemplates
     end
 
     def apply(variables_source, directory)
-      TemplateNode.new(self, '.', directory, variables_source).apply
+      ::EacTemplates::Variables::FsObject.new(self, '.', directory, variables_source).apply
     end
 
     def child(subpath)
@@ -41,68 +40,6 @@ module EacTemplates
 
     def source_absolute(source_relative)
       ::File.expand_path(source_relative, path)
-    end
-
-    class TemplateNode
-      attr_reader :source_directory, :source_relative, :target_root_directory, :variables_source
-
-      def initialize(source_directory, source_relative, target_root_directory, variables_source)
-        @source_directory = source_directory
-        @source_relative = source_relative
-        @target_root_directory = target_root_directory
-        @variables_source = variables_source
-      end
-
-      def apply
-        if file?
-          apply_file
-        elsif directory?
-          apply_directory
-        else
-          raise "Unknown filesystem type: #{source_absolute}"
-        end
-      end
-
-      private
-
-      def apply_directory
-        ::FileUtils.mkdir_p(target_absolute)
-        Dir.entries(source_absolute).each do |entry|
-          child(entry).apply unless %w[. ..].include?(entry)
-        end
-      end
-
-      def apply_file
-        if ::File.extname(source_absolute) == TEMPLATE_EXTNAME
-          ::EacTemplates::File.new(source_absolute).apply_to_file(
-            variables_source, target_absolute
-          )
-        else
-          ::FileUtils.cp(source_absolute, target_absolute)
-        end
-      end
-
-      def child(entry)
-        TemplateNode.new(source_directory, ::File.join(source_relative, entry),
-                         target_root_directory, variables_source)
-      end
-
-      def file?
-        ::File.file?(source_absolute)
-      end
-
-      def directory?
-        ::File.directory?(source_absolute)
-      end
-
-      def source_absolute
-        ::File.expand_path(source_relative, source_directory.path)
-      end
-
-      def target_absolute
-        ::File.expand_path(source_relative, target_root_directory)
-          .gsub(/#{::Regexp.quote(TEMPLATE_EXTNAME)}\z/, '')
-      end
     end
   end
 end
---
+++ sub/eac_templates/lib/eac_templates/variables/fs_object.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'eac_templates/file'
+
+module EacTemplates
+  module Variables
+    class FsObject
+      TEMPLATE_EXTNAME = '.template'
+
+      attr_reader :source_directory, :source_relative, :target_root_directory, :variables_source
+
+      def initialize(source_directory, source_relative, target_root_directory, variables_source)
+        @source_directory = source_directory
+        @source_relative = source_relative
+        @target_root_directory = target_root_directory
+        @variables_source = variables_source
+      end
+
+      def apply
+        if file?
+          apply_file
+        elsif directory?
+          apply_directory
+        else
+          raise "Unknown filesystem type: #{source_absolute}"
+        end
+      end
+
+      private
+
+      def apply_directory
+        ::FileUtils.mkdir_p(target_absolute)
+        Dir.entries(source_absolute).each do |entry|
+          child(entry).apply unless %w[. ..].include?(entry)
+        end
+      end
+
+      def apply_file
+        if ::File.extname(source_absolute) == TEMPLATE_EXTNAME
+          ::EacTemplates::File.new(source_absolute).apply_to_file(
+            variables_source, target_absolute
+          )
+        else
+          ::FileUtils.cp(source_absolute, target_absolute)
+        end
+      end
+
+      def child(entry)
+        self.class.new(source_directory, ::File.join(source_relative, entry),
+                       target_root_directory, variables_source)
+      end
+
+      def file?
+        ::File.file?(source_absolute)
+      end
+
+      def directory?
+        ::File.directory?(source_absolute)
+      end
+
+      def source_absolute
+        ::File.expand_path(source_relative, source_directory.path)
+      end
+
+      def target_absolute
+        ::File.expand_path(source_relative, target_root_directory)
+          .gsub(/#{::Regexp.quote(TEMPLATE_EXTNAME)}\z/, '')
+      end
+    end
+  end
+end
