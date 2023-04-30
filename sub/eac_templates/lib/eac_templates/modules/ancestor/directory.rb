b750898336356e80dd4a2b9821e0042952d0eb40
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-30 02:14:33 -0300

    EacTemplates::Modules::Ancestor::Directory.

---
+++ sub/eac_templates/lib/eac_templates/modules/ancestor/directory.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_templates/interface_methods'
+require 'eac_templates/modules/ancestor/fs_object'
+
+module EacTemplates
+  module Modules
+    class Ancestor
+      class Directory < ::EacTemplates::Modules::Ancestor::FsObject
+        delegate(*::EacTemplates::InterfaceMethods::ONLY_DIRECTORY, to: :source_object)
+      end
+    end
+  end
+end
