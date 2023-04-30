25335144470106068e754e2faca4506414193d8b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-30 12:26:21 -0300

    EacTemplates::Sources::FsObject.

---
+++ sub/eac_templates/lib/eac_templates/sources/fs_object.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacTemplates
+  module Sources
+    class FsObject
+      enable_abstract_methods
+      enable_simple_cache
+      common_constructor :source_set, :subpath do
+        self.subpath = subpath.to_pathname
+      end
+
+      # @return [Boolean]
+      def found?
+        real_paths.any?
+      end
+
+      protected
+
+      # @return [Class]
+      def applier_class
+        raise_abstract_method __method__
+      end
+
+      # @return [EacTemplates::Variables::SourceFile]
+      def applier_uncached
+        raise "No #{self.class.name.downcase} found for \"#{subpath}\"" unless found?
+
+        applier_class.new(real_paths.first)
+      end
+
+      # @return [Array<Pathname>]
+      def real_paths_uncached
+        source_set.included_paths.lazy.map { |source_single| source_single_search(source_single) }
+          .select(&:present?)
+      end
+
+      # @param path [Pathname]
+      # @return [Boolean]
+      def select_path?(path)
+        path.present?
+      end
+
+      # @param source_single [EacTemplates::Sources::Single]
+      # @return [Pathname, nil]
+      def source_single_search(source_single)
+        r = source_single.search(subpath)
+        select_path?(r) ? r : nil
+      end
+    end
+  end
+end
