9e40bde2130e6e8efbf7f99823c38b7f9e5d4654
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-04-30 22:31:30 -0300

    Avm::EacAsciidoctorBase0::Instances::Macros.

---
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0/instances/macros.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacAsciidoctorBase0
+    module Instances
+      module Macros
+        require_sub __FILE__
+      end
+    end
+  end
+end
