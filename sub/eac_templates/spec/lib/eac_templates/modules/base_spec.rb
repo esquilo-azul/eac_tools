1e1668f7ba8c0d06650af888acba33133f7c7fe3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-01 23:40:21 -0300

    RSpec.describe(EacTemplates::Modules::Base): convert from RSpec.describe(Module.template).

--- sub/eac_templates/spec/lib/eac_templates/modules/base_spec.rb
+++ sub/eac_templates/spec/lib/eac_templates/modules/base_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'eac_templates/patches/module/template'
+require 'eac_templates/modules/base'
 require 'eac_templates/sources/set'
 
-RSpec.describe ::Module do
+RSpec.describe ::EacTemplates::Modules::Base do
   def self.on_node_specs(node_name, &block)
     context "when object is \"#{node_name}\"" do # rubocop:disable RSpec/EmptyExampleGroup
       let(:node) { send(node_name) }
@@ -46,7 +46,7 @@ RSpec.describe ::Module do
   end
 
   let(:a_module) do
-    described_class.new do
+    ::Module.new do
       def self.name
         'AModule'
       end
@@ -69,15 +69,15 @@ RSpec.describe ::Module do
     end
     r
   end
-  let(:files_dir) { __dir__.to_pathname.join('template_spec_files') }
+  let(:files_dir) { __dir__.to_pathname.join('base_spec_files') }
   let(:variables_source) { { vx: '_X_', vy: '_Y_' } }
 
-  let(:a) { instance.template.child('a') }
+  let(:a) { instance.child('a') }
   let(:a_a) { a.child('a_a') }
   let(:a_b) { a.child('a_b') }
   let(:a_c) { a.child('a_c') }
-  let(:b) { instance.template.child('b') }
-  let(:c) { instance.template.child('c') }
+  let(:b) { instance.child('b') }
+  let(:c) { instance.child('c') }
 
   before do
     %w[path1 path2].each do |sub|
@@ -86,7 +86,7 @@ RSpec.describe ::Module do
   end
 
   context 'when module is AModule' do # rubocop:disable RSpec/EmptyExampleGroup
-    let(:instance) { a_module }
+    let(:instance) { described_class.new(a_module) }
 
     dir_specs(:a, %w[a_a])
     file_specs_ok(:a_a, "A_MODULE_A_A\n", "A_MODULE_A_A\n", [])
@@ -97,7 +97,7 @@ RSpec.describe ::Module do
   end
 
   context 'when module is SuperClass' do # rubocop:disable RSpec/EmptyExampleGroup
-    let(:instance) { super_class }
+    let(:instance) { described_class.new(super_class) }
 
     dir_specs(:a, %w[a_b])
     file_specs_error(:a_a)
@@ -108,7 +108,7 @@ RSpec.describe ::Module do
   end
 
   context 'when module is SubClass' do # rubocop:disable RSpec/EmptyExampleGroup
-    let(:instance) { sub_class }
+    let(:instance) { described_class.new(sub_class) }
 
     dir_specs(:a, %w[a_b])
     file_specs_error(:a_a)
