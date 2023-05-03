c2d0f7715ba1cc3c1b2d0c0fdeceba5024bb950b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-03 16:39:16 -0300

    EacTemplates::Variables::File.abstract_file: replace .path.

--- sub/eac_templates/lib/eac_templates/variables/file.rb
+++ sub/eac_templates/lib/eac_templates/variables/file.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'eac_templates/abstract/file'
 require 'eac_templates/variables/content'
 require 'eac_templates/variables/providers'
 
@@ -8,9 +9,10 @@ module EacTemplates
   module Variables
     class File
       enable_simple_cache
-      common_constructor :path do
-        self.path = path.to_pathname
+      common_constructor :abstract_file do
+        self.abstract_file = ::EacTemplates::Abstract::File.assert(abstract_file)
       end
+      delegate :path, to: :abstract_file
       delegate :apply, :apply_to_file, :content, :variables, to: :content_applier
 
       private
