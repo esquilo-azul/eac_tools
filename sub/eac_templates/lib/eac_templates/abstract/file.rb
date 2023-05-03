50af5e052f012219e6cf1d32ed23187d41343649
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-03 15:14:15 -0300

    EacTemplates::Abstract::File.

---
+++ sub/eac_templates/lib/eac_templates/abstract/file.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_templates/interface_methods'
+require 'eac_templates/abstract/fs_object'
+require 'eac_templates/variables/file'
+
+module EacTemplates
+  module Abstract
+    class File < ::EacTemplates::Abstract::FsObject
+      enable_abstract_methods
+      delegate(*::EacTemplates::InterfaceMethods::FILE - %i[content path], to: :applier)
+
+      # @return [String]
+      def content
+        path.read
+      end
+
+      # @return [Pathname]
+      def path
+        raise_abstract_method __method__
+      end
+    end
+  end
+end
