a4fea9c475eb983207cb58ce6822d9d353fea374
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-03 23:02:16 -0300

    EacTemplates::Modules::Base: include EacTemplates::Abstract::WithDirectoryFileMethods.

--- sub/eac_templates/lib/eac_templates/modules/base.rb
+++ sub/eac_templates/lib/eac_templates/modules/base.rb
@@ -2,19 +2,21 @@
 
 require 'eac_ruby_utils/core_ext'
 require 'eac_templates/interface_methods'
+require 'eac_templates/abstract/with_directory_file_methods'
 require 'eac_templates/modules/ancestor'
 require 'eac_templates/sources/set'
 
 module EacTemplates
   module Modules
     class Base
+      include ::EacTemplates::Abstract::WithDirectoryFileMethods
       enable_listable
       lists.add_symbol :option, :source_set, :subpath
       common_constructor :the_module, :options, default: [{}] do
         self.options = self.class.lists.option.hash_keys_validate!(options)
       end
       delegate(*::EacTemplates::InterfaceMethods::ALL, :path_for_search, :source_object,
-               to: :self_ancestor)
+               to: :sub_fs_object)
 
       # @return [EacTemplates::Modules::Ancestor]
       def self_ancestor
