86e44dc3102d287d99fcc5ec05e968609e57d6fd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-05 16:37:25 -0300

    Avm::EacRedmineBase0::Instances::RestApi::WikiPage.

---
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/rest_api/wiki_page.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'avm/eac_redmine_base0/instances/rest_api/entity_base'
+require 'eac_rest/api'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRedmineBase0
+    module Instances
+      class RestApi < ::EacRest::Api
+        class WikiPage < ::Avm::EacRedmineBase0::Instances::RestApi::EntityBase
+          enable_simple_cache
+
+          # @return [Hash]
+          def data_from_id
+            fetch_data(prefix)
+          end
+
+          # @return [String]
+          def data_root
+            'wiki_page'
+          end
+
+          # @return [String]
+          def prefix
+            "#{parent_entity.prefix}/wiki/#{id}"
+          end
+
+          # @return [String]
+          def read
+            data.fetch(data_root).fetch('text')
+          end
+
+          # @param content [String]
+          # @return [+self+]
+          def write(text)
+            data_from_response(
+              build_request(prefix).verb(:put).header('Content-type', 'application/json')
+              .body_data(write_data(text).to_json).response
+            )
+          end
+
+          # @param text [String]
+          # @return [Hash]
+          def write_data(text)
+            {
+              data_root => { 'text' => text }
+            }
+          end
+        end
+      end
+    end
+  end
+end
