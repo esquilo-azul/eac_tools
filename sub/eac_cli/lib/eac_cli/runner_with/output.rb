f2751481ae30ce87fe5d2786f3855c27ca8063df
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-05 21:25:11 -0300

    EacCli::RunnerWith::Output: move from "eac_ruby_base0":EacRubyBase0::RunnerWith::Output.

---
+++ sub/eac_cli/lib/eac_cli/runner_with/output.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'eac_cli/runner'
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/abstract_methods'
+
+module EacCli
+  module RunnerWith
+    module Output
+      STDOUT_OPTION = '-'
+      DEFAULT_FILE_OPTION = '+'
+      DEFAULT_DEFAULT_OUTPUT_OPTION = STDOUT_OPTION
+      DEFAULT_DEFAULT_FILE_TO_OUTPUT = 'output'
+
+      common_concern do
+        enable_abstract_methods
+        enable_settings_provider
+        include ::EacCli::Runner
+
+        abstract_methods :output_content
+
+        runner_definition do
+          arg_opt '-o', '--output', 'Output to file.'
+        end
+      end
+
+      def run_output
+        file = file_to_output
+        if file
+          file.to_pathname.write(output_content)
+        else
+          $stdout.write(output_content)
+        end
+      end
+
+      def output_option
+        parsed.output || default_output_option_value
+      end
+
+      def file_to_output
+        case output_option
+        when STDOUT_OPTION then nil
+        when DEFAULT_FILE_OPTION then default_file_to_output_value
+        else output_option
+        end
+      end
+
+      def default_output_option_value
+        setting_value(:default_output_option,
+                      default: DEFAULT_DEFAULT_OUTPUT_OPTION)
+      end
+
+      def default_file_to_output_value
+        setting_value(:default_file_to_output, default: DEFAULT_DEFAULT_FILE_TO_OUTPUT)
+      end
+    end
+  end
+end
---
+++ sub/eac_cli/spec/lib/eac_cli/runner_with/output_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'eac_cli/runner_with/output'
+require 'eac_ruby_utils/fs/temp'
+
+RSpec.describe ::EacCli::RunnerWith::Output do
+  let(:runner) do
+    the_module = described_class
+    Class.new do
+      include the_module
+
+      attr_accessor :temp_dir
+
+      runner_definition do
+        desc 'A stub root runner.'
+        pos_arg :input_text
+      end
+
+      def run
+        run_output
+      end
+
+      def output_content
+        parsed.input_text
+      end
+
+      def default_file_to_output
+        temp_dir.join('default_file')
+      end
+    end
+  end
+
+  let(:stub_text) { 'STUB_TEXT' }
+  let(:instance) do
+    r = runner.create(argv: runner_argv)
+    r.temp_dir = temp_dir
+    r
+  end
+  let(:temp_dir) { ::EacRubyUtils::Fs::Temp.directory }
+
+  after { temp_dir.remove }
+
+  context 'without --output option' do
+    let(:runner_argv) { [stub_text] }
+
+    it do
+      expect { instance.run }.to output(stub_text).to_stdout_from_any_process
+    end
+  end
+
+  context 'without --output option as to stdout' do
+    let(:runner_argv) { ['--output', ::EacCli::RunnerWith::Output::STDOUT_OPTION, stub_text] }
+
+    it do
+      expect { instance.run }.to output(stub_text).to_stdout_from_any_process
+    end
+  end
+
+  context 'without --output option as to default file' do
+    let(:output_file) { temp_dir.join('default_file') }
+    let(:runner_argv) do
+      ['--output', ::EacCli::RunnerWith::Output::DEFAULT_FILE_OPTION,
+       stub_text]
+    end
+
+    before { instance.run }
+
+    it { expect(output_file).to exist }
+    it { expect(output_file.read).to eq(stub_text) }
+  end
+
+  context 'with --output option' do
+    let(:output_file) { temp_dir.join('a output file') }
+    let(:runner_argv) { ['--output', output_file.to_path, stub_text] }
+
+    before { instance.run }
+
+    it { expect(output_file).to exist }
+    it { expect(output_file.read).to eq(stub_text) }
+  end
+end
