0cfcc242f7cca79f300b31cc40a48f1b54a7a970
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-11 16:43:49 -0300

    **/*.rb: adapt to "eac_ruby_utils" '0.114.0'.

--- sub/eac_cli/lib/eac_cli/runner_with/output.rb
+++ sub/eac_cli/lib/eac_cli/runner_with/output.rb
@@ -2,7 +2,7 @@
 
 require 'eac_cli/runner'
 require 'eac_ruby_utils/core_ext'
-require 'eac_ruby_utils/abstract_methods'
+require 'eac_ruby_utils/acts_as_abstract'
 
 module EacCli
   module RunnerWith
