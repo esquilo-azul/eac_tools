7e42fce0d612016bc73f2efd8e01b72241a34607
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-11 22:27:17 -0300

    Avm::Instances::Data::Unit.data_default_dump_path.

--- sub/avm/lib/avm/instances/data/unit.rb
+++ sub/avm/lib/avm/instances/data/unit.rb
@@ -9,6 +9,13 @@ module Avm
       class Unit < ::Avm::Data::Unit
         common_constructor :instance
 
+        # @return [Pathname]
+        def data_default_dump_path
+          instance.data_default_dump_path.to_pathname.basename_sub('.*') do |b|
+            "#{b}_#{identifier}#{extension}"
+          end
+        end
+
         # @return [String]
         def identifier
           instance.data_package.units.key(self) || raise("No identifier found for #{self}")
