8266a319477f8111cd3837a9abfc029126e9ab13
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-12 13:23:47 -0300

    Avm::Tools::Runner::Instance::Data::Unit::Load.

---
+++ sub/avm-tools/lib/avm/tools/runner/instance/data/unit/load.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'avm/instances/runner'
+require 'eac_cli/core_ext'
+
+module Avm
+  module Tools
+    class Runner
+      class Instance < ::Avm::Instances::Runner
+        class Data
+          class Unit
+            class Load
+              runner_with :help do
+                pos_arg :dump_path
+              end
+
+              def run
+                runner_context.call(:data_unit).load(dump_path)
+              end
+
+              # @return [Pathname]
+              def dump_path
+                parsed.dump_path.to_pathname
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
