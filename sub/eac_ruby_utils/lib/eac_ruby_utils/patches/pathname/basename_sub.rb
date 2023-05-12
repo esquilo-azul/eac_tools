f7a419f8ff43cfcd70431b0267fbc63869d32a79
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-11 22:25:39 -0300

    Pathname.basename_sub: allow no block given.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/patches/pathname/basename_sub.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/patches/pathname/basename_sub.rb
@@ -3,7 +3,11 @@
 require 'pathname'
 
 class Pathname
+  # @param suffix [String]
+  # @return [Pathname]
   def basename_sub(suffix = '')
-    parent.join(yield(basename(suffix)))
+    new_basename = basename(suffix)
+    new_basename = yield(new_basename) if block_given?
+    parent.join(new_basename)
   end
 end
