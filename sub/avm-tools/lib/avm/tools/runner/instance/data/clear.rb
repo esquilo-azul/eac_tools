d34af692a8d27868906128a71bbeac4873b7b007
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-18 19:21:11 -0300

    Avm::Tools::Runner::Instance::Data::Clear.

--- sub/avm-tools/lib/avm/tools/runner/instance/data/clear.rb
+++ sub/avm-tools/lib/avm/tools/runner/instance/data/clear.rb
@@ -9,7 +9,7 @@ module Avm
       class Instance < ::Avm::Instances::Runner
         class Data
           class Clear
-            runner_with :help, :instance_data_clear
+            runner_with :help, :include_exclude, :instance_data_clear
           end
         end
       end
