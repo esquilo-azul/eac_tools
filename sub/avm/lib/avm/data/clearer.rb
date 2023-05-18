2b098804bcf4d530f064ac2989bc298938075431
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-18 00:02:49 -0300

    Avm::Data::Clearer: extend Avm::Data::Performer.

--- sub/avm/lib/avm/data/clearer.rb
+++ sub/avm/lib/avm/data/clearer.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
+require 'avm/data/performer'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Data
-    class Clearer
-      enable_speaker
+    class Clearer < ::Avm::Data::Performer
+      def cannot_perform_reason
+        nil
+      end
 
-      common_constructor :storage
+      protected
 
-      # @return [self]
-      def perform
-        storage.clear
+      def internal_perform
+        data_owner.clear(*include_excludes_arguments)
       end
     end
   end
