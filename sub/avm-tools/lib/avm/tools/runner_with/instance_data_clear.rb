ef31d51833229a28efe82ce8424ede4798f0d38b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-18 19:34:18 -0300

    Avm::Tools::RunnerWith::InstanceDataClear.

--- sub/avm-tools/lib/avm/tools/runner_with/instance_data_clear.rb
+++ sub/avm-tools/lib/avm/tools/runner_with/instance_data_clear.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/data/clearer'
 require 'eac_cli/core_ext'
 require 'eac_cli/runner'
 
@@ -8,11 +9,20 @@ module Avm
     module RunnerWith
       module InstanceDataClear
         common_concern do
+          enable_simple_cache
           include ::EacCli::Runner
         end
 
         def run
-          data_owner.clear
+          performer.perform
+        end
+
+        private
+
+        def performer_uncached
+          %i[include exclude].inject(::Avm::Data::Clearer.new(data_owner)) do |a1, e1|
+            if_respond(e1, []).inject(a1) { |a2, e2| a2.send(e1, e2) }
+          end
         end
       end
     end
