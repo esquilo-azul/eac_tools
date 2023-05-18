1aa13f151eb9e3a10835bb65eb7746f491c48d65
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-17 23:55:12 -0300

    Module.acts_as_immutable: deprecate Module.enable_immutable.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/patches/module/acts_as_immutable.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/patches/module/acts_as_immutable.rb
@@ -4,8 +4,7 @@ require 'eac_ruby_utils/patch'
 require 'eac_ruby_utils/acts_as_immutable'
 
 class Module
-  # @deprecated Use {#acts_as_immutable} instead.
-  def enable_immutable
+  def acts_as_immutable
     ::EacRubyUtils.patch(self, ::EacRubyUtils::ActsAsImmutable)
   end
 end
