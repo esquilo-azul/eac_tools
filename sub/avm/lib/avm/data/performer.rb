7c413d9c188f64e12d3d275efe41c5abb70fd1bd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-18 00:02:50 -0300

    Avm::Data::Performer.

---
+++ sub/avm/lib/avm/data/performer.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'avm/data/rotate'
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/fs/temp'
+
+module Avm
+  module Data
+    class Performer
+      acts_as_abstract(:cannot_perform_reason, :internal_perform)
+      acts_as_immutable
+      common_constructor :data_owner
+      immutable_accessor :exclude, :include, type: :set
+
+      # @return [Enumerable]
+      def immutable_constructor_args
+        [data_owner]
+      end
+
+      # @return [self]
+      def perform
+        raise "Cannot run: #{cannot_run_reason}" unless performable?
+
+        internal_perform
+
+        self
+      end
+
+      # @return [Boolean]
+      def performable?
+        cannot_perform_reason.blank?
+      end
+
+      protected
+
+      def include_excludes_arguments
+        options = include_excludes_options
+        options.any? ? [options] : []
+      end
+
+      def include_excludes_options
+        r = {}
+        r[:includes] = includes if includes.any?
+        r[:excludes] = excludes if excludes.any?
+        r
+      end
+    end
+  end
+end
