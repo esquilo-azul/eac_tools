48fdd3a8b2d6b6bcd4bc74cf876b1d142fc8eb06
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-19 11:55:13 -0300

    EacRubyBase0::Runner::Prepend.run: rescue and write a SystemStackError.

--- sub/eac_ruby_base0/lib/eac_ruby_base0/runner/prepend.rb
+++ sub/eac_ruby_base0/lib/eac_ruby_base0/runner/prepend.rb
@@ -10,16 +10,29 @@ require 'eac_ruby_utils/speaker'
 module EacRubyBase0
   module Runner
     module Prepend
+      SYSTEM_STACK_ERROR_FILE = 'system_stack_error'
+
       def run
-        if parsed.version?
-          show_version
-        else
-          run_with_subcommand
+        on_rescue_stack_overflow do
+          if parsed.version?
+            show_version
+          else
+            run_with_subcommand
+          end
         end
       end
 
       def run_run
         on_context { super }
+      end
+
+      private
+
+      def on_rescue_stack_overflow
+        yield
+      rescue ::SystemStackError => e
+        SYSTEM_STACK_ERROR_FILE.to_pathname.write(e.backtrace.map { |v| "#{v}\n" }.join)
+        raise e
       end
     end
   end
