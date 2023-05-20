2889cd48b4d7006d7490d79e96229bf614ef341e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-20 12:37:47 -0300

    EacRubyUtils::ActsAsImmutable::EnumerableAccessor: extract from EacRubyUtils::ActsAsImmutable::ArrayAccessor.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/acts_as_immutable/array_accessor.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/acts_as_immutable/array_accessor.rb
@@ -1,48 +1,15 @@
 # frozen_string_literal: true
 
-require 'active_support/inflector'
-require 'eac_ruby_utils/acts_as_immutable/base_accessor'
-require 'eac_ruby_utils/patches/class/common_constructor'
+require 'eac_ruby_utils/acts_as_immutable/enumerable_accessor'
 
 module EacRubyUtils
   module ActsAsImmutable
-    class ArrayAccessor < ::EacRubyUtils::ActsAsImmutable::BaseAccessor
-      def apply(klass)
-        apply_singular(klass)
-        apply_plural(klass)
-      end
+    class ArrayAccessor < ::EacRubyUtils::ActsAsImmutable::EnumerableAccessor
+      INITIAL_VALUE = [].freeze
 
-      def apply_plural(klass)
-        accessor = self
-        klass.send(:define_method, ::ActiveSupport::Inflector.pluralize(name)) do |*args|
-          case args.count
-          when 0 then next accessor.immutable_value_get_filtered(self)
-          when 1 then next accessor.immutable_value_set_filtered(self, args.first)
-          else
-            raise ::ArgumentError, "wrong number of arguments (given #{args.count}, expected 0..1)"
-          end
-        end
-      end
-
-      def apply_singular(klass)
-        accessor = self
-        klass.send(:define_method, name) do |value|
-          accessor.immutable_value_push(self, value)
-        end
-      end
-
-      def immutable_value_get(object)
-        super || []
-      end
-
-      def immutable_value_push(object, value)
-        duplicate_object(object) do |old_value|
-          (old_value || []) + [value]
-        end
-      end
-
-      def immutable_value_set(object, value)
-        duplicate_object(object) { |_old_value| value }
+      # @return [Array] A empty array.
+      def initial_value
+        INITIAL_VALUE
       end
     end
   end
---
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/acts_as_immutable/enumerable_accessor.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'active_support/inflector'
+require 'eac_ruby_utils/acts_as_immutable/base_accessor'
+require 'eac_ruby_utils/patches/class/common_constructor'
+
+module EacRubyUtils
+  module ActsAsImmutable
+    # Should implement in concrect classes:
+    # * initial_value()
+    class EnumerableAccessor < ::EacRubyUtils::ActsAsImmutable::BaseAccessor
+      def apply(klass)
+        apply_singular(klass)
+        apply_plural(klass)
+      end
+
+      def apply_plural(klass)
+        accessor = self
+        klass.send(:define_method, ::ActiveSupport::Inflector.pluralize(name)) do |*args|
+          case args.count
+          when 0 then next accessor.immutable_value_get_filtered(self)
+          when 1 then next accessor.immutable_value_set_filtered(self, args.first)
+          else
+            raise ::ArgumentError, "wrong number of arguments (given #{args.count}, expected 0..1)"
+          end
+        end
+      end
+
+      def apply_singular(klass)
+        accessor = self
+        klass.send(:define_method, name) do |value|
+          accessor.immutable_value_push(self, value)
+        end
+      end
+
+      def immutable_value_get(object)
+        super || initial_value.dup
+      end
+
+      def immutable_value_push(object, value)
+        duplicate_object(object) do |old_value|
+          (old_value || initial_value.dup) + [value]
+        end
+      end
+
+      def immutable_value_set(object, value)
+        duplicate_object(object) { |_old_value| value }
+      end
+    end
+  end
+end
