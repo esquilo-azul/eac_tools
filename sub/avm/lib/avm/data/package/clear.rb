ca60a560730802e137ae0704814f80063d56c1fa
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-20 14:03:42 -0300

    Avm::Data::Package::Clear: extend Avm::Data::Package::BasePerformer.

--- sub/avm/lib/avm/data/package/clear.rb
+++ sub/avm/lib/avm/data/package/clear.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
+require 'avm/data/package/base_performer'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Data
     class Package
-      class Clear
+      class Clear < ::Avm::Data::Package::BasePerformer
         enable_method_class
-        enable_speaker
-        common_constructor :package
 
         # @return [void]
         def result
-          package.units.each do |identifier, unit|
+          selected_units.each do |identifier, unit|
             infov 'Clearing unit', identifier
             unit.clear
           end
