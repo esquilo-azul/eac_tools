620134450dcb144f78998808b1170b6ca08edd13
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-21 14:19:06 -0300

    Avm::Tools::Runner::Instance::Data::Dump: runner with "instance_data_dump".

--- sub/avm-tools/lib/avm/tools/runner/instance/data/dump.rb
+++ sub/avm-tools/lib/avm/tools/runner/instance/data/dump.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'avm/data/package/dump'
 require 'avm/instances/runner'
 require 'eac_cli/core_ext'
 
@@ -10,53 +9,8 @@ module Avm
       class Instance < ::Avm::Instances::Runner
         class Data
           class Dump
-            DUMP_EXPIRE_TIME = 1.day
-            NO_DUMP_MESSAGE = 'Dump "%s" already exist and rewrite options was no setted nor ' \
-              'dump was expired.'
-
-            runner_with :help do
+            runner_with :help, :instance_data_dump do
               desc 'Dump utility for instance.'
-              bool_opt '-w', '--rewrite', 'Forces dump overwrite.'
-              pos_arg :dump_path, optional: true
-            end
-
-            # @return [String]
-            def help_extra_text
-              "Default dump path: \"#{default_dump_path}\""
-            end
-
-            def run
-              infov 'Instance to dump', "#{instance} (#{instance.class})"
-              if package_dump.runnable?
-                package_dump.run
-              else
-                warn(package_dump.cannot_run_reason)
-              end
-              success("Dump path: \"#{dump_path}\"")
-              dump_path
-            end
-
-            private
-
-            def package_dump_uncached
-              instance
-                .data_package.dump(dump_path, existing: package_dump_existing)
-            end
-
-            def dump_path
-              parsed.dump_path || default_dump_path
-            end
-
-            def default_dump_path
-              instance.data_default_dump_path
-            end
-
-            def package_dump_existing
-              if parsed.rewrite?
-                ::Avm::Data::Package::Dump::EXISTING_ROTATE
-              else
-                ::Avm::Data::Package::Dump::EXISTING_ROTATE_EXPIRED
-              end
             end
           end
         end
