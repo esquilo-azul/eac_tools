6327db0701e14351794f295ac14f774430cad95f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-21 16:04:37 -0300

    Avm::Tools::Runner::Instance::Data::Unit::Dump: runner with "instance_data_dumper".

--- sub/avm-tools/lib/avm/tools/runner/instance/data/unit/dump.rb
+++ sub/avm-tools/lib/avm/tools/runner/instance/data/unit/dump.rb
@@ -10,28 +10,7 @@ module Avm
         class Data
           class Unit
             class Dump
-              runner_with :help do
-                pos_arg :dump_path, optional: true
-              end
-
-              def run
-                data_owner.dump(dump_path)
-              end
-
-              # @return [String]
-              def help_extra_text
-                "Default dump path: \"#{default_dump_path}\""
-              end
-
-              # @return [Pathname]
-              def dump_path
-                (parsed.dump_path || default_dump_path).to_pathname
-              end
-
-              # @return [String]
-              def default_dump_path
-                data_owner.data_default_dump_path
-              end
+              runner_with :help, :instance_data_dump
             end
           end
         end
