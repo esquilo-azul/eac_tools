f0061ff2101cdad16cee439bb5d6eab386af6b60
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-21 22:11:06 -0300

    Avm::Tools::RunnerWith::InstanceDataPerformer.data_performer_set_includes_excludes: extract from .data_performer.

--- sub/avm-tools/lib/avm/tools/runner_with/instance_data_performer.rb
+++ sub/avm-tools/lib/avm/tools/runner_with/instance_data_performer.rb
@@ -25,7 +25,12 @@ module Avm
 
         # @return [Avm::Data::Performer]
         def data_performer_uncached
-          %i[include exclude].inject(data_performer_class.new(data_owner)) do |a1, e1|
+          data_performer_set_includes_excludes(data_performer_class.new(data_owner))
+        end
+
+        # @return [Avm::Data::Performer]
+        def data_performer_set_includes_excludes(data_performer)
+          %i[include exclude].inject(data_performer) do |a1, e1|
             if_respond(e1, []).inject(a1) { |a2, e2| a2.send(e1, e2) }
           end
         end
