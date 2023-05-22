cd33be1256ba5ff8b6227c32fe7ba30b6bece6d4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-21 22:15:22 -0300

    Avm::Tools::Runner::Instance::Data::Load: run with "instance_data_load".

--- sub/avm-tools/lib/avm/tools/runner/instance/data/load.rb
+++ sub/avm-tools/lib/avm/tools/runner/instance/data/load.rb
@@ -9,46 +9,8 @@ module Avm
       class Instance < ::Avm::Instances::Runner
         class Data
           class Load
-            runner_with :help do
+            runner_with :help, :instance_data_load do
               desc 'Load utility for instance.'
-              arg_opt '-S', '--source-instance', 'Informa a instância a ser extraída o dump.'
-              pos_arg :dump_path, optional: true
-            end
-
-            def run
-              return fatal_error("Dump \"#{dump_path}\" does not exist") unless
-              ::File.exist?(dump_path)
-
-              load_dump
-              success("Dump loaded from \"#{dump_path}\"")
-            end
-
-            def dump_path_uncached
-              return parsed.dump_path.to_s if parsed.dump_path.present?
-              return source_instance_dump_path if parsed.source_instance.present?
-
-              raise "Dump path unknown (Options: #{parsed})"
-            end
-
-            def source_instance_dump_path
-              runner_context.call(:instance).class.by_id(parsed.source_instance).run_subcommand(
-                ::Avm::Tools::Runner::Instance::Data::Dump, []
-              )
-            end
-
-            def load_dump
-              info "Loading dump \"#{dump_path}\"..."
-              package_load.run
-            end
-
-            def dump_instance_method
-              :dump_database
-            end
-
-            private
-
-            def package_load_uncached
-              runner_context.call(:instance).data_package.load(dump_path)
             end
           end
         end
