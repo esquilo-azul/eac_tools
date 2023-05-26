5bc369b6fb4d8dc66b3006f34c7a30b5c1683459
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-26 16:02:24 -0300

    Avm::Instances::Base::EntryKeys.entry_keys.

--- sub/avm/lib/avm/instances/base/entry_keys.rb
+++ sub/avm/lib/avm/instances/base/entry_keys.rb
@@ -6,6 +6,11 @@ module Avm
   module Instances
     class Base
       module EntryKeys
+        # @return [Enumerable<String>]
+        def entry_keys
+          ::Avm::Instances::EntryKeys.all
+        end
+
         ::Avm::Instances::EntryKeys.all.each do |key|
           method_name = key.to_s.variableize
           define_method method_name do
