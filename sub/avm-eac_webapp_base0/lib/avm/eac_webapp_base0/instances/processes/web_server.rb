5ec42aaf82a7607389d5806ddd1a995618b94f36
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-31 18:11:50 -0300

    Avm::EacWebappBase0::Instances::Processes::WebServer.

---
+++ sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0/instances/processes/web_server.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'avm/instances/process'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacWebappBase0
+    module Instances
+      module Processes
+        class WebServer < ::Avm::Instances::Process
+          def available?
+            instance.apache_resource.present?
+          end
+
+          def enable
+            apache_resource_change(__method__)
+          end
+
+          def disable
+            apache_resource_change(__method__)
+          end
+
+          private
+
+          def apache_resource_change(action)
+            instance.apache_resource.send(action)
+            instance.platform_instance.apache.service(:reload)
+          end
+        end
+      end
+    end
+  end
+end
