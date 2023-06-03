4ad838d8e2633ce4853766a6c35e248057968c4c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-03 13:53:24 -0300

    Avm::EacRailsBase1::Instances::Processes::Daemon.

---
+++ sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/instances/processes/daemon.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'avm/instances/process'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRailsBase1
+    module Instances
+      module Processes
+        class Daemon < ::Avm::Instances::Process
+          acts_as_abstract :command_name, :gem_name
+
+          # @return [Boolean]
+          def available?
+            instance.the_gem.gemfile_lock_gem_version(gem_name).present?
+          end
+
+          def disable
+            daemon_command(:stop).system!
+          end
+
+          def enable
+            daemon_command(:start).system!
+          end
+
+          private
+
+          def daemon_command(action)
+            instance.bundle('exec', command_name, action)
+          end
+        end
+      end
+    end
+  end
+end
