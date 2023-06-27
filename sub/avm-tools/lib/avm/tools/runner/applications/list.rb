4643440627b680071c98b55d4d4f210cad0b2694
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-27 01:20:01 -0300

    Avm::Tools::Runner::Applications::List.

---
+++ sub/avm-tools/lib/avm/tools/runner/applications/list.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'avm/tools/core_ext'
+
+module Avm
+  module Tools
+    class Runner
+      class Applications
+        class List
+          runner_with :help, :output
+
+          def run
+            infov 'Found', applications.count
+            run_output
+          end
+
+          def output_content
+            applications.map { |s| "#{s}\n" }.join
+          end
+
+          private
+
+          def applications_uncached
+            ::Avm::Registry.applications.available
+              .sort_by { |s| [s.id] }
+          end
+        end
+      end
+    end
+  end
+end
