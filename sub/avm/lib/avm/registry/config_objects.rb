06a7ce0f4e8f784a90e05749fe435e92e915c365
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-27 01:21:09 -0300

    Gem "avm-eac_rails_base0": update to '~> 0.10, >= 0.10.2'.

--- sub/avm/lib/avm/registry/config_objects.rb
+++ sub/avm/lib/avm/registry/config_objects.rb
@@ -1,1 +1,39 @@
 
+# frozen_string_literal: true
+
+require 'avm/applications/base'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Registry
+    class Applications
+      def initialize(*args); end
+
+      # @return [Array<Avm::Applications::Base>]
+      def available
+        load_config
+        detected.values
+      end
+
+      # @return [Avm::Applications::Base]
+      def detect(id)
+        id = id.to_s
+        detected[id] = ::Avm::Applications::Base.new(id) unless detected.key?(id)
+        detected[id]
+      end
+
+      private
+
+      # @return [Hash<String, Avm::Applications::Base>]
+      def detected
+        @detected ||= {}
+      end
+
+      def load_config
+        ::EacConfig::Node.context.current.entries('*.avm_type').node_entries.each do |node_entry|
+          detect(node_entry.path.first)
+        end
+      end
+    end
+  end
+end
