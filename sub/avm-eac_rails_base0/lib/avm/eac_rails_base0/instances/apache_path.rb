12aa4d8b008acca775d82d6d192c8f9a1d82215d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-07-08 12:44:04 -0300

    Avm::EacRailsBase0::Instances::ApachePath: extend Avm::EacRailsBase1::Instances::ApachePath.

--- sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/instances/apache_path.rb
+++ sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/instances/apache_path.rb
@@ -1,20 +1,12 @@
 # frozen_string_literal: true
 
-require 'eac_templates/core_ext'
-require 'avm/eac_webapp_base0/instances/apache_path'
+require 'avm/eac_rails_base1/instances/apache_path'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module EacRailsBase0
     module Instances
-      class ApachePath < ::Avm::EacWebappBase0::Instances::ApachePath
-        def document_root
-          ::File.join(super, 'public')
-        end
-
-        def extra_content
-          template.child('extra_content.conf').apply(instance)
-        end
+      class ApachePath < ::Avm::EacRailsBase1::Instances::ApachePath
       end
     end
   end
