9b04e9f1db8b5b4b3a706232949d36cdd8f156b4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-13 18:48:14 -0300

    Gem "eac_envs-http" (Local): remove.

--- sub/eac_envs-http/lib/eac_envs/http/error.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module EacEnvs
-  module Http
-    class Error < ::RuntimeError
-    end
-  end
-end
--- sub/eac_envs-http/lib/eac_envs/http/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module EacEnvs
-  module Http
-    VERSION = '0.4.1'
-  end
-end
