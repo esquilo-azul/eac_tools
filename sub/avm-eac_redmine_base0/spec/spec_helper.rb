aa9b5feb9ef0a55c86c3340ae10ab40d871f2742
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-13 11:47:37 -0300

    .rubocop.yml#Style/RedundantConstantBase: remove exclusions.

--- sub/avm-eac_redmine_base0/spec/spec_helper.rb
+++ sub/avm-eac_redmine_base0/spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/rspec/default_setup'
-::EacRubyUtils::Rspec.default_setup_create(::File.expand_path('..', __dir__))
-::EacRubyUtils::Rspec.default_setup.stub_eac_config_node
+EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
+EacRubyUtils::Rspec.default_setup.stub_eac_config_node
