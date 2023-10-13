40f6208a35812f4524238c2bc0f690a5968ac1c4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-13 18:48:52 -0300

    Gem "eac_rest" (Local): remove.

--- sub/eac_rest/lib/eac_rest/error.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module EacRest
-  class Error < ::RuntimeError
-  end
-end
--- sub/eac_rest/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/rspec/default_setup'
-::EacRubyUtils::Rspec.default_setup_create(::File.expand_path('..', __dir__))
-
-require 'eac_ruby_utils/require_sub'
-::EacRubyUtils.require_sub __FILE__
