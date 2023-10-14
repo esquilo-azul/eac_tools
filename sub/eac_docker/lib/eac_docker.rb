dedd287dd833aba040777fd6bda49f6756a218c3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-14 20:03:09 -0300

    Gem "eac_docker" (Local): remove.

--- sub/eac_docker/lib/eac_docker.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacDocker
-  require_sub __FILE__
-end
--- sub/eac_docker/lib/eac_docker/rspec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacDocker
-  module Rspec
-    require_sub __FILE__
-  end
-end
