c49f4e6d9f136bc08ff42a8540a7b62885dd7d46
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-24 14:52:48 -0300

    Avm::Applications::Base::Naming.default_name.

--- sub/avm/lib/avm/applications/base/naming.rb
+++ sub/avm/lib/avm/applications/base/naming.rb
@@ -6,14 +6,21 @@ module Avm
   module Applications
     class Base
       module Naming
+        DEFAULT_NAME = '_Undefined_'
+
+        # @return [String]
+        def default_name
+          DEFAULT_NAME
+        end
+
         # @return [String]
         def name
-          name_from_configuration
+          name_from_configuration || default_name
         end
 
         # @return [String]
         def name_from_configuration
-          entry(::Avm::Instances::EntryKeys::NAME).read
+          entry(::Avm::Instances::EntryKeys::NAME).optional_value
         end
       end
     end
