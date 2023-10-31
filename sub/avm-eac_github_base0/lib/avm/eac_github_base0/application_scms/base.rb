39ff74768fa9abf05f2255822a999092262b15a1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-30 21:24:51 -0300

    Avm::EacGithubBase0::ApplicationScms::Base.

---
+++ sub/avm-eac_github_base0/lib/avm/eac_github_base0/application_scms/base.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'avm/application_scms/base'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacGithubBase0
+    module ApplicationScms
+      class Base < ::Avm::ApplicationScms::Base
+      end
+    end
+  end
+end
