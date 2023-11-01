aaed232e6233d1a84afa93834366f457c2cc7e8a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-31 22:02:23 -0300

    Avm::EacRedmineBase0::Instances::Deploy: extend Avm::EacRailsBase1::Instances::Deploy.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/deploy.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/instances/deploy.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'avm/eac_webapp_base0/instances/deploy'
+require 'avm/eac_rails_base1/instances/deploy'
 require 'eac_ruby_utils/ruby'
 
 module Avm
   module EacRedmineBase0
     module Instances
-      class Deploy < ::Avm::EacWebappBase0::Instances::Deploy
+      class Deploy < ::Avm::EacRailsBase1::Instances::Deploy
         set_callback :assert_instance_branch, :after, :run_installer
 
         def run_installer
