46c101e69bad1c82f97084206ca1e01ea56d22ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-31 21:55:30 -0300

    .rubocop.yml#Gemspec/RequiredRubyVersion: remove excludes.

--- sub/avm-eac_rails_base0/template/avm/eac_rails_base0/source_generators/base/config/application.rb
+++ sub/avm-eac_rails_base0/template/avm/eac_rails_base0/source_generators/base/config/application.rb
@@ -2,7 +2,7 @@
 
 APP_PATH = ::File.dirname(__dir__) unless defined?(APP_PATH)
 require File.expand_path('boot', __dir__)
-require 'eac_rails_base0/app_base/application.rb'
+require 'eac_rails_base0/app_base/application'
 
 module EacRailsBase0App
   class Application
