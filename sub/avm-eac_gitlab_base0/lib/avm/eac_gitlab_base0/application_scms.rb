aab2d09a112d2f776940352567bbff7ae2a62dc9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-03 09:27:59 -0300

    Avm::EacGitlabBase0::ApplicationScms.

---
+++ sub/avm-eac_gitlab_base0/lib/avm/eac_gitlab_base0/application_scms.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacGitlabBase0
+    module ApplicationScms
+      require_sub __FILE__
+    end
+  end
+end
