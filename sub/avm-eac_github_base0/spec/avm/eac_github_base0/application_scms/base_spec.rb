56b6fe2a07f1e9731ac530839702b51cd177c57a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-03 15:20:33 -0300

    .rubocop.yml#Style/RedundantConstantBase: remove excludes.

--- sub/avm-eac_github_base0/spec/avm/eac_github_base0/application_scms/base_spec.rb
+++ sub/avm-eac_github_base0/spec/avm/eac_github_base0/application_scms/base_spec.rb
@@ -2,6 +2,6 @@
 
 require 'avm/eac_github_base0/application_scms/base'
 
-::RSpec.describe ::Avm::EacGithubBase0::ApplicationScms::Base do
+RSpec.describe Avm::EacGithubBase0::ApplicationScms::Base do
   include_examples 'in_avm_registry', 'application_scms'
 end
