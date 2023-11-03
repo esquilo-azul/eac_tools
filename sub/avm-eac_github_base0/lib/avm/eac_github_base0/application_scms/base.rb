b60c18b4b0fcd3f00fdb187c0095d14fc4feab91
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-03 15:22:19 -0300

    Avm::EacGithubBase0::ApplicationScms::Base: extend Avm::Git::ApplicationScms::Base.

--- sub/avm-eac_github_base0/lib/avm/eac_github_base0/application_scms/base.rb
+++ sub/avm-eac_github_base0/lib/avm/eac_github_base0/application_scms/base.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'avm/application_scms/base'
+require 'avm/git/application_scms/base'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module EacGithubBase0
     module ApplicationScms
-      class Base < ::Avm::ApplicationScms::Base
+      class Base < ::Avm::Git::ApplicationScms::Base
       end
     end
   end
