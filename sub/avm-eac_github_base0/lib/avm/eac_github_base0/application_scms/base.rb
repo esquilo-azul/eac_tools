2c1737d4d94fa18c1974302b1f7eb96961f5c8e4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-03 20:47:39 -0300

    Avm::EacGithubBase0::ApplicationScms::Base.web_url.

--- sub/avm-eac_github_base0/lib/avm/eac_github_base0/application_scms/base.rb
+++ sub/avm-eac_github_base0/lib/avm/eac_github_base0/application_scms/base.rb
@@ -7,6 +7,10 @@ module Avm
   module EacGithubBase0
     module ApplicationScms
       class Base < ::Avm::Git::ApplicationScms::Base
+        # @return [Addressable::URI]
+        def web_url
+          application.scm_url.to_uri + application.scm_repos_path.to_s
+        end
       end
     end
   end
--- sub/avm-eac_github_base0/spec/lib/avm/eac_github_base0/application_scms/base_spec.rb
+++ sub/avm-eac_github_base0/spec/lib/avm/eac_github_base0/application_scms/base_spec.rb
@@ -3,5 +3,21 @@
 require 'avm/eac_github_base0/application_scms/base'
 
 RSpec.describe Avm::EacGithubBase0::ApplicationScms::Base do
+  let(:stub_application) do
+    {
+      scm_url: 'https://github.com'.to_uri,
+      scm_repos_path: 'esquilo-azul/eac_tools'.to_pathname
+    }.to_struct
+  end
+  let(:instance) { described_class.new(stub_application) }
+
   include_examples 'in_avm_registry', 'application_scms'
+
+  describe '#web_url' do
+    it do
+      expect(instance.web_url).to(
+        eq(Addressable::URI.parse('https://github.com/esquilo-azul/eac_tools'))
+      )
+    end
+  end
 end
