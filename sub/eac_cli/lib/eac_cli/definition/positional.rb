f32b31da35fa18ea39e45cf51dda47bf2d1e15d0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-07 20:51:46 -0300

    EacCli::Definition::Positional: document.

--- sub/eac_cli/lib/eac_cli/definition/positional.rb
+++ sub/eac_cli/lib/eac_cli/definition/positional.rb
@@ -10,10 +10,17 @@ module EacCli
 
       enable_listable
       lists.add_symbol :option, :optional, :repeat, :required, :subcommand, :visible
+
+      # @!method initialize(name, options = {})
+      # @param name [String]
+      # @param options [Hash<Symbol, Object>]
       common_constructor :name, :options, default: [{}] do
         options.assert_valid_keys(self.class.lists.option.values)
       end
 
+      # @param new_value [Array, Object]
+      # @param previous_value [Array, Object]
+      # @return [Array]
       def build_value(new_value, previous_value)
         if previous_value.is_a?(::Array)
           previous_value + [new_value]
@@ -22,22 +29,27 @@ module EacCli
         end
       end
 
+      # @return [Object]
       def default_value
         repeat? ? [] : nil
       end
 
+      # @return [Symbol]
       def identifier
         name.to_s.variableize.to_sym
       end
 
+      # @return [Boolean]
       def optional?
         !required?
       end
 
+      # @return [Boolean]
       def repeat?
         options[OPTION_REPEAT]
       end
 
+      # @return [Boolean]
       def required?
         return true if options.key?(OPTION_REQUIRED) && options.fetch(OPTION_REQUIRED)
         return false if options.key?(OPTION_OPTIONAL) && options.fetch(OPTION_OPTIONAL)
@@ -45,14 +57,17 @@ module EacCli
         DEFAULT_REQUIRED
       end
 
+      # @return [Boolean]
       def subcommand?
         options[OPTION_SUBCOMMAND]
       end
 
+      # @return [String]
       def to_s
         "#{self.class.name.demodulize}[#{identifier}]"
       end
 
+      # @return [Boolean]
       def visible?
         options.key?(OPTION_VISIBLE) ? options.fetch(OPTION_VISIBLE) : DEFAULT_VISIBLE
       end
