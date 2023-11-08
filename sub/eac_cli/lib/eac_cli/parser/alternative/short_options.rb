014dca5a9d554b9e36d7a79a396aa2838140ef07
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-08 00:13:29 -0300

    EacCli::Parser::Alternative::ShortOptions: document.

--- sub/eac_cli/lib/eac_cli/parser/alternative/short_options.rb
+++ sub/eac_cli/lib/eac_cli/parser/alternative/short_options.rb
@@ -8,17 +8,21 @@ module EacCli
 
         private
 
+        # @return [Boolean]
         def argv_current_short_option?
           phase == PHASE_ANY && argv_enum.peek.start_with?(SHORT_OPTION_PREFIX) &&
             !argv_current_long_option?
         end
 
+        # @para char [String]
+        # @return [EacCli::Definition::Option, nil]
         def find_short_option(char)
           alternative.options.find do |option|
             short_without_prefix(option.short).if_present(false) { |v| v == char }
           end
         end
 
+        # @return [void]
         def short_option_collect_argv_value
           last_option = nil
           short_without_prefix(argv_enum.peek).each_char do |char|
@@ -30,12 +34,15 @@ module EacCli
           end
         end
 
+        # @param char [String]
         # @return [EacCli::Definition::Option] The option collected.
         def short_option_collect_char(char)
           option = find_short_option(char)
           option ? option_collect_option(option) : any_option_collect_option
         end
 
+        # @param short [String]
+        # @return [String]
         def short_without_prefix(short)
           short.to_s.gsub(/\A#{::Regexp.quote(SHORT_OPTION_PREFIX)}/, '')
         end
