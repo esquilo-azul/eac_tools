e9a16818402b41cc5016a637fc5d868b8c7ae183
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-18 20:37:16 -0300

    Sub[sub/eac_cli]: remove.

--- sub/eac_cli/lib/eac_cli/core_ext.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-require 'eac_cli/patches'
-
-EacCli::RunnerWithSet.default.add_from_gems_registry
--- sub/eac_cli/lib/eac_cli/rspec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacCli
-  module Rspec
-    require_sub __FILE__
-  end
-end
