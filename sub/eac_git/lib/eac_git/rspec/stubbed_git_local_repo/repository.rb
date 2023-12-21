4bb55e23f52c5016d2c656be7e84a987c7840efc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-12-21 00:50:48 -0300

    EacGit::Rspec::StubbedGitLocalRepo::Repository.directory.

--- sub/eac_git/lib/eac_git/rspec/stubbed_git_local_repo/repository.rb
+++ sub/eac_git/lib/eac_git/rspec/stubbed_git_local_repo/repository.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_git/local'
+require 'eac_git/rspec/stubbed_git_local_repo/directory'
 require 'eac_git/rspec/stubbed_git_local_repo/file'
 require 'eac_ruby_utils/core_ext'
 require 'securerandom'
@@ -9,6 +10,11 @@ module EacGit
   module Rspec
     module StubbedGitLocalRepo
       class Repository < ::EacGit::Local
+        # @return [EacGit::Rspec::StubbedGitLocalRepo::Directory]
+        def directory(*subpath)
+          ::EacGit::Rspec::StubbedGitLocalRepo::Directory.new(self, subpath)
+        end
+
         def file(*subpath)
           ::EacGit::Rspec::StubbedGitLocalRepo::File.new(self, subpath)
         end
