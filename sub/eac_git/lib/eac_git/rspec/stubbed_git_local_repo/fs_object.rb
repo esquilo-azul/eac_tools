567554073abd43bd70a3b6799030ee80286f257d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-12-21 00:46:49 -0300

    EacGit::Rspec::StubbedGitLocalRepo::FsObject.initialize: refact with .common_constructor.

--- sub/eac_git/lib/eac_git/rspec/stubbed_git_local_repo/fs_object.rb
+++ sub/eac_git/lib/eac_git/rspec/stubbed_git_local_repo/fs_object.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/core_ext'
+
 module EacGit
   module Rspec
     module StubbedGitLocalRepo
       class FsObject
-        attr_reader :git, :subpath
+        # @!attribute [r] git
+        #   @param git [EacGit::Rspec::StubbedGitLocalRepo::Repository]
 
-        def initialize(git, subpath)
-          @git = git
-          @subpath = subpath
-        end
+        # @!attribute [r] subpath
+        #   @param git [Array<String>]
+
+        # @!method initialize(git, subpath)
+        #   @param git [EacGit::Rspec::StubbedGitLocalRepo::Repository]
+        #   @param subpath [Array<String>
+        common_constructor :git, :subpath
 
         def path
           git.root_path.join(*subpath)
