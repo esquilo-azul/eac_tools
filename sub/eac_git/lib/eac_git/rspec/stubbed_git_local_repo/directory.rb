f05d200e830c018aff7c01de90dbf7ac1cfd0d90
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-12-21 00:50:47 -0300

    EacGit::Rspec::StubbedGitLocalRepo::Directory.

---
+++ sub/eac_git/lib/eac_git/rspec/stubbed_git_local_repo/directory.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'eac_git/rspec/stubbed_git_local_repo/fs_object'
+require 'fileutils'
+
+module EacGit
+  module Rspec
+    module StubbedGitLocalRepo
+      class Directory < ::EacGit::Rspec::StubbedGitLocalRepo::FsObject
+        # @return [self]
+        def create
+          ::FileUtils.mkdir_p(path)
+          self
+        end
+
+        # @return [self]
+        def delete
+          ::FileUtils.rm_rf(path)
+          self
+        end
+
+        # @param subpath [Array<String>]
+        # @return [EacGit::Rspec::StubbedGitLocalRepo::Directory]
+        def directory(*subpath)
+          git.directory(*self.subpath, *subpath)
+        end
+
+        # @param subpath [Array<String>]
+        # @return [EacGit::Rspec::StubbedGitLocalRepo::File]
+        def file(*subpath)
+          git.file(*self.subpath, *subpath)
+        end
+      end
+    end
+  end
+end
