dccae9dd5db14b6960aae2293577c0f9f3d40bc8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-02-13 17:33:55 -0300

    RSpec.describe(Avm::Launcher::Context): move to "avm":=.

--- sub/avm-tools/spec/spec_helper.rb
+++ sub/avm-tools/spec/spec_helper.rb
@@ -2,6 +2,3 @@
 
 require 'eac_ruby_utils/rspec/default_setup'
 EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
-
-APP_ROOT = File.expand_path('..', __dir__)
-EacRubyUtils.require_sub(__FILE__)
