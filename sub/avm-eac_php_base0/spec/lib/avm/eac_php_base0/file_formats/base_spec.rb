983411e7371e1a41626d3281a853b9b31fffe794
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-02-14 02:15:59 -0300

    .rubocop.yml#Style/RedundantConstantBase: remove.

--- sub/avm-eac_php_base0/spec/lib/avm/eac_php_base0/file_formats/base_spec.rb
+++ sub/avm-eac_php_base0/spec/lib/avm/eac_php_base0/file_formats/base_spec.rb
@@ -2,6 +2,6 @@
 
 require 'avm/eac_php_base0/file_formats/base'
 
-::RSpec.describe ::Avm::EacPhpBase0::FileFormats::Base do
+RSpec.describe Avm::EacPhpBase0::FileFormats::Base do
   include_examples 'avm_file_formats_with_fixtures', __FILE__
 end
--- sub/avm-eac_php_base0/spec/lib/avm/eac_php_base0/sources/base_spec.rb
+++ sub/avm-eac_php_base0/spec/lib/avm/eac_php_base0/sources/base_spec.rb
@@ -2,6 +2,6 @@
 
 require 'avm/eac_php_base0/sources/base'
 
-::RSpec.describe ::Avm::EacPhpBase0::Sources::Base do
+RSpec.describe Avm::EacPhpBase0::Sources::Base do
   include_examples 'in_avm_registry', 'sources'
 end
