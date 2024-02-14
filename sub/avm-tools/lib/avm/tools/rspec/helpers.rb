c9e30ce8754ed8553913a6aba3ecbd7e07dba3b7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-02-13 23:12:24 -0300

    Avm::Tools::Rspec::Helpers.

---
+++ sub/avm-tools/lib/avm/tools/rspec/helpers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Tools
+    module Rspec
+      module Helpers
+        require_sub __FILE__
+      end
+    end
+  end
+end
