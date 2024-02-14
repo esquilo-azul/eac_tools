49ed50cf5a319a9fbd944a913a62c6539975d01c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-02-13 23:12:03 -0300

    Avm::Tools::Rspec::Helpers::Runner.

--- sub/avm-tools/lib/avm/tools/rspec/setup.rb
+++ sub/avm-tools/lib/avm/tools/rspec/setup.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/tools/rspec/helpers/runner'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
@@ -8,10 +9,14 @@ module Avm
       module Setup
         extend ::ActiveSupport::Concern
 
-        SETUPS = %w[].freeze
+        SETUPS = %w[helpers].freeze
 
         def self.extended(setup_obj)
           SETUPS.each { |s| setup_obj.send("setup_#{s}") }
+        end
+
+        def setup_helpers
+          rspec_config.include ::Avm::Tools::Rspec::Helpers::Runner
         end
       end
     end
