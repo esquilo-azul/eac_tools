e539d74f6cd3e512b2c656965d2b9908a3699233
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-05 13:58:01 -0300

    Sub[sub/avm]: remove.

--- sub/avm/lib/avm/sources/update_error.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Sources
-    class UpdateError < ::RuntimeError
-    end
-  end
-end
--- sub/avm/lib/avm/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  VERSION = '0.86.2'
-end
