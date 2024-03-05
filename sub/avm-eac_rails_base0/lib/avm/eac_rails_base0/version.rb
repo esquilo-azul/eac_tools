adc974f826b81ea5cc6e51e3f36382f6f48a8064
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-05 13:58:49 -0300

    Sub[sub/avm-eac_rails_base0]: remove.

--- sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRailsBase0
-    VERSION = '0.11.1'
-  end
-end
--- sub/avm-eac_rails_base0/template/avm/eac_rails_base0/source_generators/base/app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationController < ::EacRailsBase0::ApplicationController
-end
