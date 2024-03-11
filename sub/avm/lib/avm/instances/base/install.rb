112afffdfdc6620387d52bc11ed731b088ff20a0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-11 14:34:41 -0300

    Avm::Instances::Base::Install.install_scheme_default_value.

--- sub/avm/lib/avm/instances/base/install.rb
+++ sub/avm/lib/avm/instances/base/install.rb
@@ -7,6 +7,8 @@ module Avm
   module Instances
     class Base
       module Install
+        INSTALL_SCHEME_DEFAULT_VALUE = 'file'
+
         common_concern do
           uri_components_entries_values 'install', %w[data_path email groupname]
         end
@@ -17,6 +19,11 @@ module Avm
 
         def install_groupname_default_value
           read_entry_optional(::Avm::Instances::EntryKeys::INSTALL_USERNAME)
+        end
+
+        # @return [String]
+        def install_scheme_default_value
+          INSTALL_SCHEME_DEFAULT_VALUE
         end
       end
     end
