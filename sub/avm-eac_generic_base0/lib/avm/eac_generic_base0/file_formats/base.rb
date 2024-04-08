5af5a43abe5f34f5d5664a31cdd339889b218f21
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-08 12:37:13 -0300

    Sub[sub/avm-eac_generic_base0]: add '0.12.2'.

---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/file_formats/base.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'avm/file_formats/base'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacGenericBase0
+    class FileFormats
+      class Base < ::Avm::FileFormats::Base
+        require_sub __FILE__, include_modules: true, require_dependency: true
+
+        VALID_BASENAMES = %w[*.asm *.bat *.coffee *.java *.js *.rb *.s *.sql *.tex *.url *.yml
+                             *.yaml].freeze
+
+        VALID_TYPES = %w[plain x-shellscript].freeze
+
+        def internal_apply(files)
+          files.each { |file| file_apply(file) }
+        end
+
+        # @param file [Pathname]
+        # @return [Boolean]
+        def match?(file)
+          ::Avm::EacGenericBase0::FileFormats::Base::Match.new(self, file).result || !file.exist?
+        end
+
+        def file_apply(file)
+          file.write(string_apply(file.read))
+        end
+
+        def string_apply(string)
+          b = ''
+          string.each_line do |line|
+            b += "#{line.rstrip}\n"
+          end
+          "#{b.strip}\n".gsub("\t", '  ')
+        end
+      end
+    end
+  end
+end
---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Avm
+  module EacGenericBase0
+    VERSION = '0.12.2'
+  end
+end
