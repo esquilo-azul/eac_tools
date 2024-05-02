272a423f66d7ef7601033e977bd7dfc56da1e649
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-02 19:15:36 -0300

    Avm::Applications::Base::LocalSource.user_local_source_path: extract from .local_source_path.

--- sub/avm/lib/avm/applications/base/local_source.rb
+++ sub/avm/lib/avm/applications/base/local_source.rb
@@ -10,12 +10,17 @@ module Avm
       module LocalSource
         # @return [Pathname]
         def local_source_path
-          (local_source_path_entry.value || auto_local_source_path).to_pathname
+          user_local_source_path || auto_local_source_path
         end
 
         # @return [EacConfig::Entry]
         def local_source_path_entry
           ::EacConfig::Node.context.current.entry([local_instance_id, 'install', 'path'])
+        end
+
+        # @return [Path, nil]
+        def user_local_source_path
+          local_source_path_entry.value.if_present(&:to_pathname)
         end
 
         private
