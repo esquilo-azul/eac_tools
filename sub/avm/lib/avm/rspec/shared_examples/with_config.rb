7904df22426bf37291e376e087e56c150469b4cd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-13 23:41:06 -0300

    RSpec.shared_examples('with_config'): use a configuration's temporary copy.

--- sub/avm/lib/avm/rspec/shared_examples/with_config.rb
+++ sub/avm/lib/avm/rspec/shared_examples/with_config.rb
@@ -5,5 +5,7 @@ require 'eac_ruby_utils/core_ext'
 RSpec.shared_examples 'with_config' do |spec_file|
   config_path = spec_file.to_pathname
   config_path = config_path.dirname.join("#{config_path.basename_noext}_files", 'config.yml')
-  EacRubyUtils::Rspec.default_setup.stub_eac_config_node(self, config_path)
+  temp_config_path = EacRubyUtils::Fs::Temp.file
+  FileUtils.cp(config_path, temp_config_path)
+  EacRubyUtils::Rspec.default_setup.stub_eac_config_node(self, temp_config_path)
 end
