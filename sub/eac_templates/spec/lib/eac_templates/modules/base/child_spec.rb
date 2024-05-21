2c7de46c9d88f93d27ad632cb154fe8b254cb8af
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-21 15:08:41 -0300

    RSpec.describe(EacTemplates::Modules::Base.child): new fixtures.

--- sub/eac_templates/spec/lib/eac_templates/modules/base/child_spec.rb
+++ sub/eac_templates/spec/lib/eac_templates/modules/base/child_spec.rb
@@ -54,6 +54,7 @@ RSpec.describe EacTemplates::Modules::Base, '#child' do
   let(:a_c) { a.child('a_c') }
   let(:b) { instance.child('b') }
   let(:c) { instance.child('c') }
+  let(:d) { instance.child('d') }
 
   context 'when module is AModule' do # rubocop:disable RSpec/EmptyExampleGroup
     let(:instance) { described_class.new(a_module, source_set: source_set) }
@@ -64,6 +65,7 @@ RSpec.describe EacTemplates::Modules::Base, '#child' do
     file_specs_error(:a_c)
     file_specs_ok(:b, "A_MODULE.P1.B.%%vy%%\n", "A_MODULE.P1.B._Y_\n", %w[vy])
     file_specs_ok(:c, "A_MODULE.P2.C.%%vx%%\n", "A_MODULE.P2.C._X_\n", %w[vx])
+    file_specs_error(:d)
   end
 
   context 'when module is SuperClass' do # rubocop:disable RSpec/EmptyExampleGroup
@@ -75,29 +77,32 @@ RSpec.describe EacTemplates::Modules::Base, '#child' do
     file_specs_error(:a_c)
     file_specs_ok(:b, "SUPER_CLASS.P1.B\n", "SUPER_CLASS.P1.B\n", [])
     file_specs_ok(:c, "A_MODULE.P2.C.%%vx%%\n", "A_MODULE.P2.C._X_\n", %w[vx])
+    file_specs_error(:d)
   end
 
   context 'when module is SubClass' do # rubocop:disable RSpec/EmptyExampleGroup
     let(:instance) { described_class.new(sub_class, source_set: source_set) }
 
-    dir_specs(:a, %w[a_a a_b a_c])
+    dir_specs(:a, %w[a_a a_b a_c a_d a_d.template])
     file_specs_ok(:a_a, "A_MODULE.P1.A_A\n", "A_MODULE.P1.A_A\n", [])
     file_specs_ok(:a_b, "SUPER_CLASS.P1.A_B\n", "SUPER_CLASS.P1.A_B\n", [])
     file_specs_ok(:a_c, "SUB_CLASS.P1.A_C\n", "SUB_CLASS.P1.A_C\n", [])
     file_specs_ok(:b, "SUB_CLASS.P2.B\n", "SUB_CLASS.P2.B\n", [])
     file_specs_ok(:c, "PREPENDED_MODULE.P2.C.%%vy%%%%vx%%\n", "PREPENDED_MODULE.P2.C._Y__X_\n",
                   %w[vy vx])
+    file_specs_error(:d)
   end
 
   context 'when module is PrependedModule' do # rubocop:disable RSpec/EmptyExampleGroup
     let(:instance) { described_class.new(prepended_module, source_set: source_set) }
 
-    file_specs_error(:a)
+    dir_specs(:a, %w[a_d.template])
     file_specs_error(:a_a)
     file_specs_error(:a_b)
     file_specs_error(:a_c)
     file_specs_error(:b)
     file_specs_ok(:c, "PREPENDED_MODULE.P2.C.%%vy%%%%vx%%\n", "PREPENDED_MODULE.P2.C._Y__X_\n",
                   %w[vy vx])
+    file_specs_error(:d)
   end
 end
