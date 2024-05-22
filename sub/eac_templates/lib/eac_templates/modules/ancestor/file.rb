e471967303cb0ece59d575e12003febf3f2e23ad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-21 22:02:53 -0300

    EacTemplates::Modules::Ancestor::File.parse_basename.

--- sub/eac_templates/lib/eac_templates/modules/ancestor/file.rb
+++ sub/eac_templates/lib/eac_templates/modules/ancestor/file.rb
@@ -10,6 +10,20 @@ module EacTemplates
     class Ancestor
       class File < ::EacTemplates::Abstract::File
         include ::EacTemplates::Modules::Ancestor::FsObject
+
+        TEMPLATE_EXTNAME_PATTERN =
+          /#{::Regexp.quote(::EacTemplates::Variables::FsObject::TEMPLATE_EXTNAME)}\z/.freeze
+
+        class << self
+          def parse_basename(basename)
+            basename.if_present do |v|
+              v.to_pathname
+                .to_path
+                .gsub(TEMPLATE_EXTNAME_PATTERN, '')
+                .to_pathname
+            end
+          end
+        end
       end
     end
   end
