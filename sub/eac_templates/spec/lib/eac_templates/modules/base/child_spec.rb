5141cce55ece0d35c1a55933fcf9a23f4ea83c21
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-21 22:59:14 -0300

    RSpec.describe(EacTemplates::Modules::Base.child): normalize fixtures.

--- sub/eac_templates/spec/lib/eac_templates/modules/base/child_spec.rb
+++ sub/eac_templates/spec/lib/eac_templates/modules/base/child_spec.rb
@@ -59,33 +59,34 @@ RSpec.describe EacTemplates::Modules::Base, '#child' do
     let(:instance) { described_class.new(a_module, source_set: source_set) }
 
     dir_specs(:a, %w[a_a a_b])
-    file_specs_ok(:a_a, "A_MODULE_A_A\n", "A_MODULE_A_A\n", [])
-    file_specs_ok(:a_b, "A_MODULE_A_B\n", "A_MODULE_A_B\n", [])
+    file_specs_ok(:a_a, "A_MODULE.P1.A_A\n", "A_MODULE.P1.A_A\n", [])
+    file_specs_ok(:a_b, "A_MODULE.P2.A_B\n", "A_MODULE.P2.A_B\n", [])
     file_specs_error(:a_c)
-    file_specs_ok(:b, "A_MODULE_B%%vy%%\n", "A_MODULE_B_Y_\n", %w[vy])
-    file_specs_ok(:c, "A_MODULE_C%%vx%%\n", "A_MODULE_C_X_\n", %w[vx])
+    file_specs_ok(:b, "A_MODULE.P1.B.%%vy%%\n", "A_MODULE.P1.B._Y_\n", %w[vy])
+    file_specs_ok(:c, "A_MODULE.P2.C.%%vx%%\n", "A_MODULE.P2.C._X_\n", %w[vx])
   end
 
   context 'when module is SuperClass' do # rubocop:disable RSpec/EmptyExampleGroup
     let(:instance) { described_class.new(super_class, source_set: source_set) }
 
     dir_specs(:a, %w[a_a a_b])
-    file_specs_ok(:a_a, "A_MODULE_A_A\n", "A_MODULE_A_A\n", [])
-    file_specs_ok(:a_b, "SUPER_CLASS_A_B\n", "SUPER_CLASS_A_B\n", [])
+    file_specs_ok(:a_a, "A_MODULE.P1.A_A\n", "A_MODULE.P1.A_A\n", [])
+    file_specs_ok(:a_b, "SUPER_CLASS.P1.A_B\n", "SUPER_CLASS.P1.A_B\n", [])
     file_specs_error(:a_c)
-    file_specs_ok(:b, "SUPER_CLASS_B\n", "SUPER_CLASS_B\n", [])
-    file_specs_ok(:c, "A_MODULE_C%%vx%%\n", "A_MODULE_C_X_\n", %w[vx])
+    file_specs_ok(:b, "SUPER_CLASS.P1.B\n", "SUPER_CLASS.P1.B\n", [])
+    file_specs_ok(:c, "A_MODULE.P2.C.%%vx%%\n", "A_MODULE.P2.C._X_\n", %w[vx])
   end
 
   context 'when module is SubClass' do # rubocop:disable RSpec/EmptyExampleGroup
     let(:instance) { described_class.new(sub_class, source_set: source_set) }
 
     dir_specs(:a, %w[a_a a_b a_c])
-    file_specs_ok(:a_a, "A_MODULE_A_A\n", "A_MODULE_A_A\n", [])
-    file_specs_ok(:a_b, "SUPER_CLASS_A_B\n", "SUPER_CLASS_A_B\n", [])
-    file_specs_ok(:a_c, "SUB_CLASS_A_C\n", "SUB_CLASS_A_C\n", [])
-    file_specs_ok(:b, "SUB_CLASS_B\n", "SUB_CLASS_B\n", [])
-    file_specs_ok(:c, "PREPENDED_MODULE_C%%vy%%%%vx%%\n", "PREPENDED_MODULE_C_Y__X_\n", %w[vy vx])
+    file_specs_ok(:a_a, "A_MODULE.P1.A_A\n", "A_MODULE.P1.A_A\n", [])
+    file_specs_ok(:a_b, "SUPER_CLASS.P1.A_B\n", "SUPER_CLASS.P1.A_B\n", [])
+    file_specs_ok(:a_c, "SUB_CLASS.P1.A_C\n", "SUB_CLASS.P1.A_C\n", [])
+    file_specs_ok(:b, "SUB_CLASS.P2.B\n", "SUB_CLASS.P2.B\n", [])
+    file_specs_ok(:c, "PREPENDED_MODULE.P2.C.%%vy%%%%vx%%\n", "PREPENDED_MODULE.P2.C._Y__X_\n",
+                  %w[vy vx])
   end
 
   context 'when module is PrependedModule' do # rubocop:disable RSpec/EmptyExampleGroup
@@ -96,6 +97,7 @@ RSpec.describe EacTemplates::Modules::Base, '#child' do
     file_specs_error(:a_b)
     file_specs_error(:a_c)
     file_specs_error(:b)
-    file_specs_ok(:c, "PREPENDED_MODULE_C%%vy%%%%vx%%\n", "PREPENDED_MODULE_C_Y__X_\n", %w[vy vx])
+    file_specs_ok(:c, "PREPENDED_MODULE.P2.C.%%vy%%%%vx%%\n", "PREPENDED_MODULE.P2.C._Y__X_\n",
+                  %w[vy vx])
   end
 end
