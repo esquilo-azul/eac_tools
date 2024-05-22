f60f805b88217d71bfdd91ae538bae415980fda0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-21 22:08:21 -0300

    EacTemplates::Modules::Ancestor::Directory.children_basenames: self implementation.

--- sub/eac_templates/lib/eac_templates/modules/ancestor/directory.rb
+++ sub/eac_templates/lib/eac_templates/modules/ancestor/directory.rb
@@ -10,7 +10,26 @@ module EacTemplates
     class Ancestor
       class Directory < ::EacTemplates::Abstract::Directory
         include ::EacTemplates::Modules::Ancestor::FsObject
-        delegate :children_basenames, to: :source_object
+
+        # @return [Hash<Pathname, Symbol>]
+        def children_basenames
+          source_object.children_basenames.to_h { |k, v| parse_child_basename(k, v) }
+        end
+
+        protected
+
+        # @param basename [Pathname]
+        # @param type [Symbol]
+        # @return [Array]
+        def parse_child_basename(basename, type)
+          return [basename, type] unless type == :file
+
+          new_basename = ::EacTemplates::Modules::Ancestor::File.parse_basename(basename)
+
+          return [basename, type] if new_basename == basename
+
+          [new_basename, :file_template]
+        end
       end
     end
   end
