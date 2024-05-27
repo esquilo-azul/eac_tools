796b1a4b90ed557c484f178116cb32096ed330fe
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-27 14:52:39 -0300

    RSpec.shared_examples('avm_source_generated'): option "block_on_each_source".

--- sub/avm/lib/avm/rspec/shared_examples/avm_source_generated.rb
+++ sub/avm/lib/avm/rspec/shared_examples/avm_source_generated.rb
@@ -5,6 +5,7 @@ require 'eac_ruby_utils/core_ext'
 
 RSpec.shared_examples 'avm_source_generated' do |spec_file, stereotype, options = {}|
   include_context 'spec_paths', spec_file
+  block_on_each_source = options.delete(:block_on_each_source)
 
   spec_paths_controller.fixtures_directory.children.select(&:directory?).each do |target_dir|
     context "when target is \"#{target_dir.basename}\"" do
@@ -13,6 +14,8 @@ RSpec.shared_examples 'avm_source_generated' do |spec_file, stereotype, options 
       it do
         expect(fs_comparator.build(source.path)).to eq(fs_comparator.build(target_dir))
       end
+
+      instance_exec(&block_on_each_source) if block_on_each_source
     end
   end
 
