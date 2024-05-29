6a2e31c483958dbd2801ab37b05d2a641ec42b1a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-29 18:57:05 -0300

    Sub[sub/avm-eac_ruby_base0]: remove.

--- sub/avm-eac_ruby_base0/lib/avm/eac_ruby_base0/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRubyBase0
-    VERSION = '0.1.0'
-  end
-end
--- sub/avm-eac_ruby_base0/spec/lib/avm/eac_ruby_base0/source_generators/base_spec_files/mygem_underscore/lib/mygem_underscore.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module MygemUnderscore
-  require_sub __FILE__
-end
