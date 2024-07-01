e25940364e87dcc47066cb8292af51dd5cbd332e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-01 15:31:08 -0300

    Avm::Instances::Base::AutoValues::Source.auto_source_instance_id: refact with Avm::Applications::Base::LocalInstance::LOCAL_INSTANCE_SUFFIX.

--- sub/avm/lib/avm/instances/base/auto_values/source.rb
+++ sub/avm/lib/avm/instances/base/auto_values/source.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require 'avm/applications/base/local_instance'
+
 module Avm
   module Instances
     class Base
       module AutoValues
         module Source
           def auto_source_instance_id
-            "#{application.id}_dev"
+            [application.id, ::Avm::Applications::Base::LocalInstance::LOCAL_INSTANCE_SUFFIX]
+              .join('_')
           end
         end
       end
