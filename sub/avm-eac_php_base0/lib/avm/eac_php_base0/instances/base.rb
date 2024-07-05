2132499b917690df7b2ceda3aa2e736f43720c81
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-05 11:21:40 -0300

    Avm::EacPhpBase0::Instances::Deploy.

--- sub/avm-eac_php_base0/lib/avm/eac_php_base0/instances/base.rb
+++ sub/avm-eac_php_base0/lib/avm/eac_php_base0/instances/base.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/eac_php_base0/instances/deploy'
 require 'avm/eac_webapp_base0/instances/base'
 require 'eac_ruby_utils/core_ext'
 
---
+++ sub/avm-eac_php_base0/lib/avm/eac_php_base0/instances/deploy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'avm/eac_webapp_base0/instances/deploy'
+
+module Avm
+  module EacPhpBase0
+    module Instances
+      class Deploy < ::Avm::EacWebappBase0::Instances::Deploy
+      end
+    end
+  end
+end
