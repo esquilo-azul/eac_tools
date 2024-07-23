51eb5a78883ffabc258b546651853159280d0e83
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-23 18:40:03 -0300

    Avm::Git::Scms::GitSubtree.no_other_git_scm?: extract from .valid?.

--- sub/avm-git/lib/avm/git/scms/git_subtree.rb
+++ sub/avm-git/lib/avm/git/scms/git_subtree.rb
@@ -9,16 +9,22 @@ module Avm
   module Git
     module Scms
       class GitSubtree < ::Avm::Git::Scms::GitSubBase
+        # @return [Boolean]
+        def no_other_git_scm?
+          (::Avm::Git::Scms::Provider.new.all - [self.class])
+            .lazy.map { |scm_class| scm_class.new(path) }.none?(&:valid?)
+        end
+
         def update
           # Do nothing
         end
 
+        # @return [Boolean]
         def valid?
           return false unless ::Avm::Git::Scms::Provider
                                 .new.all.any? { |scm_class| parent_scm.is_a?(scm_class) }
 
-          (::Avm::Git::Scms::Provider.new.all - [self.class])
-            .lazy.map { |scm_class| scm_class.new(path) }.none?(&:valid?)
+          no_other_git_scm?
         end
       end
     end
