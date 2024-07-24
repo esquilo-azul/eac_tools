41f7ad9929aa08e11d1556123f62eb6d3215d3f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-23 21:44:44 -0300

    Sub[sub/eac_templates]: remove.

--- sub/eac_templates/lib/eac_templates/patches/module/erb_template.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'erb'
-
-class Module
-  def erb_template(subpath, binding_obj)
-    ::ERB.new(template.child(subpath).path.read).result(binding_obj)
-  end
-end
--- sub/eac_templates/lib/eac_templates/sources/from_all_gems.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_templates/sources/from_gem'
-EacTemplates::Sources::FromGem.include_all
