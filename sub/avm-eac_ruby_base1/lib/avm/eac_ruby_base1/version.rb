42f053201f1fbe04386d50428b1b8b89f03e34cd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-08-25 12:14:57 -0300

    Sub[sub/avm-eac_ruby_base1]: remove.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRubyBase1
-    VERSION = '0.35.0'
-  end
-end
--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/source_generators/base_spec_files/dashed-mygem/lib/dashed/mygem.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module Dashed
-  module Mygem
-    require_sub __FILE__
-  end
-end
