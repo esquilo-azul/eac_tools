f0e05ca4f0c8f665fc3f5237faf8704be676ece3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-09-15 21:48:24 -0300

    RSpec.describe(Avm::EacRubyBase1::Rubygems::VersionFile): refact.

--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/rubygems/version_file_spec.rb
+++ sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/rubygems/version_file_spec.rb
@@ -3,8 +3,9 @@
 require 'avm/eac_ruby_base1/rubygems/version_file'
 
 RSpec.describe Avm::EacRubyBase1::Rubygems::VersionFile do
-  let(:stubs_dir) { Pathname.new(__dir__).join('version_file_spec_files') }
-  let(:stub_file) { stubs_dir.join('a_version_file.rb') }
+  include_context 'spec_paths', __FILE__
+
+  let(:stub_file) { fixtures_directory.join('a_version_file.rb') }
   let(:instance) { described_class.new(stub_file) }
   let(:target_version) { Gem::Version.new('0.69.1') }
 
