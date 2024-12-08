d94d644a9757bf9bb01b4d078326c24f5b0cfb16
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-12-07 21:32:03 -0300

    Sub[sub/avm-eac_generic_base0]: add '0.13.0'.

---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/sources/docker.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacGenericBase0
+    module Sources
+      module Docker
+        require_sub __FILE__
+      end
+    end
+  end
+end
