ad87b3a05e5d268e118d4200fb9133ea65b4f260
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-12-08 13:57:34 -0300

    Sub[sub/eac_ruby_utils]: remove.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/patches/module/patch.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/patch'
-
-class Module
-  def patch(patch_module)
-    ::EacRubyUtils.patch(self, patch_module)
-  end
-end
--- sub/eac_ruby_utils/lib/eac_ruby_utils/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module EacRubyUtils
-  VERSION = '0.124.0'
-end
