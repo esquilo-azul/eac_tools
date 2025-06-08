a97c32ed30c09944ac76bf22a6fbf448a3d2bab1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-07 22:48:14 -0300

    Avm::EacRubyBase1: setup with EacRubyUtils::RootModuleSetup.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils'
+EacRubyUtils::RootModuleSetup.perform __FILE__
+
+require 'avm'
+require 'avm/eac_generic_base0'
+require 'eac_templates'
+require 'eac_envs/http'
 
 module Avm
   module EacRubyBase1
-    require_sub __FILE__
   end
 end
