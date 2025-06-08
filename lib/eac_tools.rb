b25defabaa198510aa6338acbd8f8c5cfda9b775
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-08 10:23:40 -0300

    EacTools: require all avm's extensions.

--- lib/eac_tools.rb
+++ lib/eac_tools.rb
@@ -3,6 +3,23 @@
 require 'eac_ruby_utils'
 EacRubyUtils::RootModuleSetup.perform __FILE__
 
+require 'avm/eac_asciidoctor_base0'
+require 'avm/eac_generic_base0'
+require 'avm/eac_github_base0'
+require 'avm/eac_gitlab_base0'
+require 'avm/eac_latex_base0'
+require 'avm/eac_php_base0'
+require 'avm/eac_postgresql_base0'
+require 'avm/eac_python_base0'
+require 'avm/eac_rails_base0'
+require 'avm/eac_rails_base1'
+require 'avm/eac_redmine_base0'
+require 'avm/eac_redmine_plugin_base0'
+require 'avm/eac_ruby_base0'
+require 'avm/eac_ruby_base1'
+require 'avm/eac_webapp_base0'
+require 'avm/eac_wordpress_base0'
+require 'avm/git'
 require 'avm/tools'
 
 module EacTools
