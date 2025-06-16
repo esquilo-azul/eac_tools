34736c578bad9c5de16f9a2be2c273c14fe247f3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-15 22:49:49 -0300

    Avm::Git::Scms::Git::ChangedFile.action.

--- sub/avm-git/lib/avm/git/scms/git/changed_file.rb
+++ sub/avm-git/lib/avm/git/scms/git/changed_file.rb
@@ -9,6 +9,15 @@ module Avm
 
           # @return [Pathname]
           delegate :path, to: :sub_changed_file
+
+          # @return [Symbol]
+          def action
+            return ACTION_ADD if sub_changed_file.add?
+            return ACTION_DELETE if sub_changed_file.delete?
+            return ACTION_MODIFY if sub_changed_file.modify?
+
+            nyi sub_changed_file
+          end
         end
       end
     end
