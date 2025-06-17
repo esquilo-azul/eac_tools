a3804f3441392408907fd1c13f22761fa21be638
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-17 20:55:52 -0300

    Sub[sub/eac_ruby_base0]: remove.

--- sub/eac_ruby_base0/lib/eac_ruby_base0/patches/class/jobs_runner.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_base0/jobs_runner'
-
-class Class
-  def enable_jobs_runner
-    ::EacRubyUtils.patch_module(self, ::EacRubyBase0::JobsRunner)
-  end
-end
