b7b0c0357563d3f60029f10b1a886bc245a33adf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-19 09:50:26 -0300

    spec/spec_helper.rb: move "eac_ruby_gem_support"'s require from "lib/avm/eac_webapp_base0.rb".

--- sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0.rb
+++ sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0.rb
@@ -3,8 +3,6 @@
 require 'eac_ruby_utils'
 EacRubyUtils::RootModuleSetup.perform __FILE__
 
-require 'eac_ruby_gem_support'
-
 module Avm
   module EacWebappBase0
     require_sub __FILE__
--- sub/avm-eac_webapp_base0/spec/spec_helper.rb
+++ sub/avm-eac_webapp_base0/spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 require 'avm/eac_webapp_base0'
+require 'eac_ruby_gem_support'
 EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__)).stub_avm_contexts
