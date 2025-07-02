cc9a22c15668906ca46b060b8c4ff7c5977b1bc5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-02 19:21:39 -0300

    Sub[sub/avm-git]: remove.

--- sub/avm-git/lib/avm/git/organize.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Git
-    module Organize
-      require_sub __FILE__
-    end
-  end
-end
--- sub/avm-git/lib/avm/git/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Git
-    VERSION = '0.20.1'
-  end
-end
