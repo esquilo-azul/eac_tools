a4c0b4aee7376d45b83191393f9ef4db71aadaf8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-12 23:46:41 -0300

    Avm::EacRubyBase1::Rubygems::Providers::Base: extract from Avm::EacRubyBase1::Rubygems::Providers::RubygemsOrg.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/rubygems/providers/rubygems_org.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/rubygems/providers/rubygems_org.rb
@@ -7,14 +7,15 @@ module Avm
   module EacRubyBase1
     module Rubygems
       module Providers
-        class RubygemsOrg
+        class RubygemsOrg < ::Avm::EacRubyBase1::Rubygems::Providers::Base
           DEFAULT_ROOT_HTTP_URL = 'https://rubygems.org'.to_uri
 
-          common_constructor :root_http_url, default: [DEFAULT_ROOT_HTTP_URL] do
-            self.root_http_url = root_http_url.to_uri
+          # @param root_http_url [Pathname]
+          def initialize(root_http_url = DEFAULT_ROOT_HTTP_URL)
+            super
           end
 
-          # @return [Enumerable<String>]
+          # @return [Enumerable<Hash>]
           def gem_versions(gem_name)
             ::EacEnvs::Http::Request.new
               .url(root_http_url + "/api/v1/versions/#{gem_name}.json")
@@ -24,14 +25,13 @@ module Avm
           end
 
           # @param gem_package_path [Pathname]
-          def push_gem(gem_package_path)
-            info("Pushing gem #{gem_package_path}...")
+          # @return [Enumerable<String>]
+          def push_gem_command_args(gem_package_path)
             command = ['gem', 'push', gem_package_path]
             unless ::Avm::Launcher::Context.current.publish_options[:confirm]
               command = %w[echo] + command + %w[(Dry-run)]
             end
-            EacRubyUtils::Envs.local.command(command).system
-            info('Pushed!')
+            command
           end
         end
       end
