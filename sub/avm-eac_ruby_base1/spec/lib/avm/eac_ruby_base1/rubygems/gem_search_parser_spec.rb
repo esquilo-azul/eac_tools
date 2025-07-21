5c2d02c8e22df0b6268eb9c078a6bdcc09089eed
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-21 11:56:06 -0300

    Sub[sub/avm-eac_ruby_base1]: remove.

--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/rubygems/gem_search_parser_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Avm::EacRubyBase1::Rubygems::GemSearchParser do
-  include_examples 'source_target_fixtures', __FILE__
-end
--- sub/avm-eac_ruby_base1/spec/lib/avm/eac_ruby_base1/runners/base/rubocop_spec_files/dir4/false_command.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-puts '33.33.33'
