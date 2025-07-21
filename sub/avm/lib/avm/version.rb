32d8e17e854f605d2078d2e60b2135ab229469b7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-21 11:56:44 -0300

    Sub[sub/avm]: remove.

--- sub/avm/lib/avm/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  VERSION = '0.97.0'
-end
--- sub/avm/spec/lib/avm/sources/base_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Avm::Sources::Base do
-  include_examples 'not_in_avm_registry'
-end
