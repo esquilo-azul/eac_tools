1b29c3f10fc5d6c355d0d0335ead21bf4f9bcac4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-08-08 14:03:07 -0300

    Sub[sub/avm-git]: remove.

--- sub/avm-git/lib/avm/git/runners.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Git
-    module Runners
-      require_sub __FILE__
-    end
-  end
-end
--- sub/avm-git/lib/avm/git/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Git
-    VERSION = '0.20.2'
-  end
-end
