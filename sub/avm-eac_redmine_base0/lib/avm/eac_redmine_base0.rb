1d63313b9cd3c1ba6a3ca4474d3f8098a44aa0f0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-08-08 14:11:35 -0300

    Avm::EacRedmineBase0: setup with EacRubyUtils::RootModuleSetup.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils'
+EacRubyUtils::RootModuleSetup.perform __FILE__
 
 module Avm
   module EacRedmineBase0
-    require_sub __FILE__
   end
 end
+
+require 'avm'
+require 'avm/eac_generic_base0'
+require 'avm/eac_rails_base1'
+require 'avm/eac_ubuntu_base0'
+require 'eac_fs'
+require 'eac_rest'
--- sub/avm-eac_redmine_base0/spec/spec_helper.rb
+++ sub/avm-eac_redmine_base0/spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/rspec/default_setup'
+require 'avm/eac_redmine_base0'
+require 'eac_ruby_gem_support'
 EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
 EacRubyUtils::Rspec.default_setup.stub_eac_config_node
