30fcb96a28b5d39c337ec9062e4b499ce7b2bce5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 10:55:15 -0300

    lib/**/*.rb: remove empty modules.

--- sub/eac_cli/lib/eac_cli/rspec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module EacCli
-  module Rspec
-    require_sub __FILE__
-  end
-end
--- sub/eac_cli/lib/eac_cli/runner/context_responders.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module EacCli
-  module Runner
-    module ContextResponders
-      require_sub __FILE__
-    end
-  end
-end
