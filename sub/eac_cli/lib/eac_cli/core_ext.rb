dfcf6c88108f8a027e9db466fb200f0114ce8d6a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 10:27:36 -0300

    EacCli::RunnerWithSet::FromGemsRegistry.namespaces_set.

--- sub/eac_cli/lib/eac_cli/core_ext.rb
+++ sub/eac_cli/lib/eac_cli/core_ext.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
 require 'eac_cli/patches'
-EacCli::RunnerWithSet.default.add_from_gems_registry
--- sub/eac_cli/lib/eac_cli/runner_with_set/from_gems_registry.rb
+++ sub/eac_cli/lib/eac_cli/runner_with_set/from_gems_registry.rb
@@ -3,10 +3,11 @@
 module EacCli
   class RunnerWithSet
     class FromGemsRegistry < ::EacCli::RunnerWithSet
-      def add_from_gems_registry
-        ::EacRubyUtils::GemsRegistry.new('RunnerWith').registered.each do |registered_gem|
-          add_namespace(registered_gem.registered_module)
-        end
+      protected
+
+      # @return [Enumerable<Object>]
+      def namespace_set
+        super + ::EacRubyUtils::GemsRegistry.new('RunnerWith').registered.map(&:registered_module)
       end
     end
   end
