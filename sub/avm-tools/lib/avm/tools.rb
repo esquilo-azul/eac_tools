ffaae0af44687840f214da7a6d51fc24df5bd73f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 09:33:50 -0300

    Avm::Tools: require "avm/tools/core_ext".

--- sub/avm-tools/lib/avm/tools.rb
+++ sub/avm-tools/lib/avm/tools.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
+EacRubyUtils::RootModuleSetup.perform __FILE__ do
+  ignore 'core_ext'
+end
 
 module Avm
   module Tools
@@ -10,3 +12,5 @@ end
 
 require 'avm'
 require 'eac_ruby_base0'
+
+require 'avm/tools/core_ext'
