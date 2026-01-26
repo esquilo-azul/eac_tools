df4d7e539b36dd5f50570a887f1bd2e7334a149f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-01-26 12:17:06 -0300

    Sub[sub/eac_ruby_utils]: remove.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/unimplemented_method_error.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module EacRubyUtils
-  class UnimplementedMethodError < ::StandardError
-  end
-end
--- sub/eac_ruby_utils/lib/eac_ruby_utils/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module EacRubyUtils
-  VERSION = '0.128.6'
-end
