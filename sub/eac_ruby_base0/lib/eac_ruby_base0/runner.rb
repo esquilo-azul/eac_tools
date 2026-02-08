cc06faa84f97895ecbf05bae32239a3a50983107
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-08 15:10:28 -0300

    **/*.rb: remove unnecessary requires.

--- sub/eac_ruby_base0/lib/eac_ruby_base0/runner.rb
+++ sub/eac_ruby_base0/lib/eac_ruby_base0/runner.rb
@@ -2,13 +2,13 @@
 
 module EacRubyBase0
   module Runner
-    require_sub __FILE__
     enable_speaker
     common_concern do
       include ::EacCli::RunnerWith::Help
       include ::EacCli::RunnerWith::Subcommands
       include ::EacRubyBase0::Runner::Contexts
       prepend ::EacRubyBase0::Runner::Prepend
+
       runner_definition do
         bool_opt '-q', '--quiet', 'Quiet mode.'
         bool_opt '-I', '--no-input', 'Fail if a input is requested.'
