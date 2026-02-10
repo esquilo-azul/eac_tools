67bd5c5ca92854eb53f1219fa417f752ca2ef516
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-09 22:17:12 -0300

    EacRubyGemSupport: fix "super_diff".

--- sub/eac_ruby_gem_support/lib/eac_ruby_gem_support.rb
+++ sub/eac_ruby_gem_support/lib/eac_ruby_gem_support.rb
@@ -3,6 +3,7 @@
 require 'eac_ruby_utils'
 EacRubyUtils::RootModuleSetup.perform __FILE__
 
+require 'active_support/ordered_options' # Fix "super_diff" '0.18.0'.
 require 'super_diff'
 require 'super_diff/active_support'
 require 'super_diff/rspec'
