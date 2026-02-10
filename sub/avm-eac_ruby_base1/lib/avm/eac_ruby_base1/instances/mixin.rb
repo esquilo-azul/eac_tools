d25964f89f183a48dcfc037035e50d96baf9ac50
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-09 23:26:41 -0300

    Avm::EacRubyBase1::Instances::Mixin.default_ruby_version.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/instances/mixin.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/instances/mixin.rb
@@ -7,6 +7,13 @@ module Avm
         DEFAULT_RUBY_VERSION = '2.7.7'
         RUBY_VERSION_KEY = 'ruby.version'
 
+        class << self
+          # @return [Avm::VersionNumber]
+          def default_ruby_version
+            ::Avm::VersionNumber.new(DEFAULT_RUBY_VERSION)
+          end
+        end
+
         def auto_install_ruby_version
           inherited_entry_value(::Avm::Instances::EntryKeys::INSTALL_ID, RUBY_VERSION_KEY) ||
             DEFAULT_RUBY_VERSION
