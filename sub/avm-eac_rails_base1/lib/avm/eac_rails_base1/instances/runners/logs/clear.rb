7c53cb5b72a5edd991ba3345b852e7519aef48fb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-13 22:08:55 -0300

    Sub[sub/avm-eac_rails_base1]: add '0.11.1'.

---
+++ sub/avm-eac_rails_base1/lib/avm/eac_rails_base1/instances/runners/logs/clear.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Avm
+  module EacRailsBase1
+    module Instances
+      module Runners
+        class Logs
+          class Clear
+            runner_with :help, ::Avm::EacRailsBase1::RunnerWith::Bundle do
+              desc 'Clear logs.'
+            end
+
+            def run
+              bundle_run
+            end
+
+            def bundle_args
+              %w[exec rake log:clear]
+            end
+          end
+        end
+      end
+    end
+  end
+end
