d74b97a0e6ad1a8b3e955e1c917c330a219479dc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-14 22:14:20 -0300

    Sub[sub/avm-eac_webapp_base0]: add '0.21.3'.

---
+++ sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils'
+EacRubyUtils::RootModuleSetup.perform __FILE__
+
+module Avm
+  module EacWebappBase0
+  end
+end
+
+require 'avm/eac_generic_base0'
+require 'avm/eac_postgresql_base0'
