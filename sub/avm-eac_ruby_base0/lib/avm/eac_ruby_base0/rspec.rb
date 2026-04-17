6cfb7c133eed830eff363aca34645e8c34ee7100
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-04-17 00:54:35 -0300

    **/*.rb: remove empty modules.

--- sub/avm-eac_ruby_base0/lib/avm/eac_ruby_base0/rspec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRubyBase0
-    module Rspec
-      require_sub __FILE__
-    end
-  end
-end
--- sub/avm-eac_ruby_base0/lib/avm/eac_ruby_base0/source_generators.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module EacRubyBase0
-    module SourceGenerators
-      require_sub __FILE__
-    end
-  end
-end
