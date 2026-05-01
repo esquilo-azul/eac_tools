27900a9b528e346ecfbddea748df1d38ce3f5a21
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-05-01 11:23:44 -0300

    Avm::EacRubyBase1::Instances::Mixin::DEFAULT_RUBY_VERSION: '3.2.9'.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/instances/mixin.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/instances/mixin.rb
@@ -4,7 +4,7 @@ module Avm
   module EacRubyBase1
     module Instances
       module Mixin
-        DEFAULT_RUBY_VERSION = '2.7.7'
+        DEFAULT_RUBY_VERSION = '3.2.9'
         RUBY_VERSION_KEY = 'ruby.version'
 
         class << self
