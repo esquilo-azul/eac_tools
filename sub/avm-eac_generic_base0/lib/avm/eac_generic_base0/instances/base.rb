91efb3b83bd393fbae7885ed325558cfcefd9065
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-27 13:40:04 -0300

    Sub[sub/avm-eac_generic_base0]: add '0.15.2'.

---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/instances/base.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Avm
+  module EacGenericBase0
+    module Instances
+      class Base < ::Avm::Instances::Base
+      end
+    end
+  end
+end
---
+++ sub/avm-eac_generic_base0/lib/avm/eac_generic_base0/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Avm
+  module EacGenericBase0
+    VERSION = '0.15.2'
+  end
+end
