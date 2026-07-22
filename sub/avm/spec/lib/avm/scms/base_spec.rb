77fe188ae4de22ebad6586bc7d8704f16cab6812
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-22 15:04:07 -0300

    Sub[sub/avm]: remove.

--- sub/avm/spec/lib/avm/scms/base_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Avm::Scms::Base do
-  include_examples 'not_in_avm_registry'
-end
--- sub/avm/template/avm/rspec/launcher_controller/dummy/avm-tools_stub/lib/avm/tools.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Tools
-  end
-end
