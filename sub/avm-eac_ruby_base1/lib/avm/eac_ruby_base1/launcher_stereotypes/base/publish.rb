b444911098c266867ac6ef28380cd16330aad473
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-24 10:16:51 -0300

    Avm::EacRubyBase1::LauncherStereotypes::Base::Publish.push_gem: check for non-success.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher_stereotypes/base/publish.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/launcher_stereotypes/base/publish.rb
@@ -18,10 +18,11 @@ module Avm
             gem_build.close
           end
 
+          # @return [void]
           def push_gem
             info("Pushing gem #{gem_spec}...")
-            gem_provider.push_gem(gem_build.output_file)
-            info('Pushed!')
+            r = gem_provider.push_gem(gem_build.output_file)
+            send(r.type == :success ? :success : :error, r.message)
           end
 
           require_sub __FILE__, include_modules: true, require_mode: :kernel
