e4260e49bc655d70c362d0c27aae62f1042fd03e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 15:20:31 -0300

    Sub[sub/eac_docker]: remove.

--- sub/eac_docker/spec/lib/eac_docker/executables_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe(EacDocker::Executables, :docker) do
-  it 'output version' do
-    expect(described_class.docker.command('--version').execute!).to be_present
-  end
-end
