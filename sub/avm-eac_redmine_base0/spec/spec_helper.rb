e08bfba6aff6ab2dc97f98185c5a64643bd6d9a5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 15:13:28 -0300

    Sub[sub/avm-eac_redmine_base0]: remove.

--- sub/avm-eac_redmine_base0/spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/eac_redmine_base0'
-require 'eac_ruby_gem_support'
-EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
-EacRubyUtils::Rspec.default_setup.stub_eac_config_node
