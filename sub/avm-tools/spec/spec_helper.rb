deec3708afbedec2cb08503e3b4534d0b2325e22
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 15:19:56 -0300

    Sub[sub/avm-tools]: remove.

--- sub/avm-tools/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/tools'
-require 'avm/git'
-require 'avm/git/scms/provider'
-require 'eac_ruby_gem_support'
-EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
