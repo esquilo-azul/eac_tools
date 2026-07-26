012ec4b7f1360ebab33adffa7935eaca4454a5c6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-25 23:11:20 -0300

    EacTemplates: refact with EacRubyBase1::RootModuleSetup.

--- sub/eac_templates/lib/eac_templates.rb
+++ sub/eac_templates/lib/eac_templates.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__ do
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
   ignore 'core_ext'
-  ignore 'patches'
   ignore 'sources/from_all_gems'
+  require 'eac_config'
+  require 'eac_templates/core_ext'
 end
-
-require 'eac_config'
-
-module EacTemplates
-end
-
-require 'eac_templates/core_ext'
--- sub/eac_templates/lib/eac_templates/core_ext.rb
+++ sub/eac_templates/lib/eac_templates/core_ext.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-require 'eac_templates/patches'
 require 'eac_templates/sources/from_all_gems'
