410ebdee6fb00298e8c2c74b5d4017dc3d589e64
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 11:33:45 -0300

    Avm::EacAsciidoctorBase0: refact with EacRubyBase1::RootModuleSetup.

--- sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0.rb
+++ sub/avm-eac_asciidoctor_base0/lib/avm/eac_asciidoctor_base0.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
-module Avm
-  module EacAsciidoctorBase0
-  end
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'avm/eac_webapp_base0'
 end
-
-require 'avm/eac_webapp_base0'
