8922498e55a9215543f7795baa5371798d269ea9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 11:55:35 -0300

    Avm::EacRailsBase1: refact with EacRubyBase1::RootModuleSetup.

--- sub/avm-eac_rails_base1/lib/avm/eac_rails_base1.rb
+++ sub/avm-eac_rails_base1/lib/avm/eac_rails_base1.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
-module Avm
-  module EacRailsBase1
-  end
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'avm/eac_ruby_base1'
+  require 'avm/eac_webapp_base0'
 end
-
-require 'avm/eac_ruby_base1'
-require 'avm/eac_webapp_base0'
