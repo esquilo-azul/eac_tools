bfaf703dff58b788e5b4ca0b4f3ae8b0be8a5f0d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 11:43:04 -0300

    Avm::EacLatexBase0: refact with EacRubyBase1::RootModuleSetup.

--- sub/avm-eac_latex_base0/lib/avm/eac_latex_base0.rb
+++ sub/avm-eac_latex_base0/lib/avm/eac_latex_base0.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
-
-module Avm
-  module EacLatexBase0
-  end
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'avm/eac_webapp_base0'
 end
