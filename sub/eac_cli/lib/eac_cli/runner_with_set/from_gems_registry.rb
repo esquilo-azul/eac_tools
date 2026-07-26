27ad5e6500a3d4769bcdf100f656dd43c3a40ea7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 16:55:29 -0300

    EacCli::RunnerWithSet::FromGemsRegistry.gems_registry: extract from .namespace_set.

--- sub/eac_cli/lib/eac_cli/runner_with_set/from_gems_registry.rb
+++ sub/eac_cli/lib/eac_cli/runner_with_set/from_gems_registry.rb
@@ -3,13 +3,20 @@
 module EacCli
   class RunnerWithSet
     class FromGemsRegistry < ::EacCli::RunnerWithSet
+      enable_memoized
+
       MODULE_SUFFIX = 'RunnerWith'
 
       protected
 
+      # @return [EacRubyUtils::GemsRegistry]
+      memoize def gems_registry
+        ::EacRubyUtils::GemsRegistry.new(MODULE_SUFFIX)
+      end
+
       # @return [Enumerable<Object>]
       def namespace_set
-        super + ::EacRubyUtils::GemsRegistry.new(MODULE_SUFFIX).registered.map(&:registered_module)
+        super + gems_registry.registered.map(&:registered_module)
       end
     end
   end
