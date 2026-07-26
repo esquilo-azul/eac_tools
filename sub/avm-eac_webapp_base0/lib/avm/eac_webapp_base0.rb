ba0abda59f125b6e59eb69721c8b5a0b02ff8f09
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-26 15:19:16 -0300

    Sub[sub/avm-eac_webapp_base0]: remove.

--- sub/avm-eac_webapp_base0/lib/avm/eac_webapp_base0.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_base1'
-EacRubyBase1::RootModuleSetup.perform __FILE__ do
-  require 'avm'
-  require 'avm/eac_generic_base0'
-  require 'avm/eac_postgresql_base0'
-  require 'avm/eac_ubuntu_base0'
-end
