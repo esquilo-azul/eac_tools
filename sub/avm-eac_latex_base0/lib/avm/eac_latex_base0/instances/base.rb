e4533f6368c626cce71313ed871c2b064b54bf65
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-25 23:44:37 -0300

    Sub[sub/avm-eac_latex_base0]: add '0.3.2'.

---
+++ sub/avm-eac_latex_base0/lib/avm/eac_latex_base0/instances/base.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'avm/eac_webapp_base0/instances/base'
+
+module Avm
+  module EacLatexBase0
+    module Instances
+      class Base < ::Avm::EacWebappBase0::Instances::Base
+      end
+    end
+  end
+end
---
+++ sub/avm-eac_latex_base0/lib/avm/eac_latex_base0/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Avm
+  module EacLatexBase0
+    VERSION = '0.3.2'
+  end
+end
