5b08baf943f077c9392df4ba8ff113e7364a8bf4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-31 23:58:53 -0300

    EacRubyBase0: refact with EacRubyBase1::RootModuleSetup.

--- sub/eac_ruby_base0/lib/eac_ruby_base0.rb
+++ sub/eac_ruby_base0/lib/eac_ruby_base0.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__ do
-  ignore 'patches'
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'avm/eac_ruby_base1'
+  require 'eac_cli'
+  require 'eac_config'
+  require 'eac_fs'
 end
-
-module EacRubyBase0
-end
-
-require 'avm/eac_ruby_base1'
-require 'eac_cli'
-require 'eac_config'
-require 'eac_fs'
-require 'eac_ruby_base0/patches'
