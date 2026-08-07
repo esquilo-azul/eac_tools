8fc6a36762fd21ef9f5d29cad75847e873282f0d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-07 13:15:30 -0300

    Avm::EacRedmineBase0::Sources::CoreUpdate::GITIGNORE_ADD: adapt to "redmine" '7.0.0'.

--- sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/sources/core_update.rb
+++ sub/avm-eac_redmine_base0/lib/avm/eac_redmine_base0/sources/core_update.rb
@@ -8,8 +8,8 @@ module Avm
         enable_simple_cache
         common_constructor :source, :version, :url
 
-        GITIGNORE_ADD = %w[/public/assets/**/* /config/install.sh /config/secrets_key.txt
-                           /log/**/*].freeze
+        GITIGNORE_ADD = %w[/public/assets/**/* /config/credentials.* /config/credentials/*
+                           /config/install.sh /config/master.key /log/**/*].freeze
         GITIGNORE_DEL = %w[/Gemfile.lock /plugins/* /public/themes/*].freeze
         TARGET_KEEP = ::Avm::Sources::Base::Configuration::CONFIGURATION_FILENAMES
                         .map { |b| "/#{b}" } + %w[/Gemfile.lock /plugins/*/**
