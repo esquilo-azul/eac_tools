32b822380ed96ca88ea494d4b7384cbe675391c8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-08 23:29:23 -0300

    Sub[sub/eac_ruby_gem_support]: remove.

--- sub/eac_ruby_gem_support/lib/eac_ruby_gem_support.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_base1'
-EacRubyBase1::RootModuleSetup.perform __FILE__ do
-  ignore 'rspec/shared_examples/**/*'
-  require 'super_diff'
-  require 'super_diff/active_support'
-  require 'super_diff/rspec'
-end
