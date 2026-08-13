f8c98748fbd49e88f8195a55ccd5981ff5e6ed8a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-13 13:26:41 -0300

    Sub[sub/avm]: remove.

--- sub/avm/lib/avm/files/appender.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Files
-    class Appender
-      include ::Avm::Files::Appendable
-    end
-  end
-end
--- sub/avm/template/avm/rspec/launcher_controller/dummy/avm-tools_stub/lib/avm/tools/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Avm
-  module Tools
-    VERSION = '1.0.0.pre.stub'
-  end
-end
