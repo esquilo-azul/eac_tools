de6e31cd96cf4cb0125917d1a5f45642d7d85019
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-13 10:01:32 -0300

    EacDocker::Container::Identifiers.identifier.

--- sub/eac_docker/lib/eac_docker/container/identifiers.rb
+++ sub/eac_docker/lib/eac_docker/container/identifiers.rb
@@ -9,6 +9,14 @@ module EacDocker
 
       attr_reader :id
 
+      # Value that identifies the container to Docker commands: {#id}, if present, or {#name}.
+      #
+      # @return [String]
+      def identifier
+        id.presence || name.presence || raise('Neither `id` or `name` is present ' \
+                                              '- need at least one of them')
+      end
+
       protected
 
       attr_writer :id
