d37bc1d6b4e86e0539ff25f1b262ef8bffdbfd2b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-13 11:02:52 -0300

    EacRubyUtils::PatchModule.perform: rename from .patch_module.

--- sub/eac_ruby_utils/lib/eac_ruby_utils/patch_module.rb
+++ sub/eac_ruby_utils/lib/eac_ruby_utils/patch_module.rb
@@ -5,13 +5,13 @@ require 'eac_ruby_utils/patches/module/common_concern'
 module EacRubyUtils
   class << self
     def patch_module(*)
-      ::EacRubyUtils::PatchModule.patch_module(*)
+      ::EacRubyUtils::PatchModule.perform(*)
     end
   end
 
   module PatchModule
     class << self
-      def patch_module(target, patch)
+      def perform(target, patch)
         return if target.include?(patch)
 
         target.send(:include, patch)
