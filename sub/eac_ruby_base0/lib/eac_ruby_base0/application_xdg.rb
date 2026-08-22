37bad83404ac8bf4bdb8f28650c2044870f6359b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-22 16:11:17 -0300

    Sub[sub/eac_ruby_base0]: add '0.21.0'.

---
+++ sub/eac_ruby_base0/lib/eac_ruby_base0/application_xdg.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module EacRubyBase0
+  # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+  class ApplicationXdg
+    class << self
+      # @return [Pathname]
+      def user_home_dir_from_env
+        Dir.home.to_pathname
+      end
+    end
+
+    DIRECTORIES = { cache: '.cache', config: '.config', data: '.local/share',
+                    state: '.local/state' }.freeze
+
+    common_constructor :app_name, :user_home_dir, default: [nil] do
+      self.user_home_dir ||= self.class.user_home_dir_from_env
+    end
+
+    DIRECTORIES.each do |item, subpath|
+      xdg_env_method_name = "#{item}_xdg_env"
+
+      define_method xdg_env_method_name do
+        ENV["XDG_#{item.upcase}_HOME"].if_present(&:to_pathname)
+      end
+
+      define_method "#{item}_dir" do
+        (send(xdg_env_method_name) || user_home_dir.join(subpath)).join(app_name)
+      end
+    end
+  end
+end
