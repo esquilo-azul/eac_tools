1ad9c05107e7326694a66ae5566556ea644d1025
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-12 16:41:01 -0300

    Sub[sub/eac_ruby_base0]: remove.

--- sub/eac_ruby_base0/lib/eac_ruby_base0.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_base1'
-EacRubyBase1::RootModuleSetup.perform __FILE__ do
-  require 'avm/eac_ruby_base1'
-  require 'eac_cli'
-  require 'eac_config'
-  require 'eac_fs'
-end
