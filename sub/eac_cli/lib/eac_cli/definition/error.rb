df70a692910b2a06f5174cb5cbea25c9a9b952dd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-12 16:40:45 -0300

    Sub[sub/eac_cli]: remove.

--- sub/eac_cli/lib/eac_cli/definition/error.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module EacCli
-  class Definition
-    class Error < ::StandardError
-    end
-  end
-end
--- sub/eac_cli/lib/eac_cli/speaker/input_requested.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module EacCli
-  class Speaker
-    class InputRequested < ::RuntimeError
-    end
-  end
-end
