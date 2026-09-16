698f2fc85fe280127b9692b5d361723cb18d2ee0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-16 11:19:46 -0300

    EacGit::Executables::GitCommandExtensions: move to own file.

--- sub/eac_git/lib/eac_git/executables.rb
+++ sub/eac_git/lib/eac_git/executables.rb
@@ -11,32 +11,12 @@ module EacGit
 
       memoize def git
         r = env.executable('git', '--version')
-        r.extend(GitCommandExtensions)
+        r.extend(::EacGit::Executables::GitCommandExtensions)
         r
       end
 
       memoize def tar
         env.executable('tar', '--version')
-      end
-
-      module GitCommandExtensions
-        def command(*args)
-          super.envvar('PATH', path_with_git_subrepo)
-        end
-
-        def gem_root
-          '../..'.to_pathname.expand_path(__dir__)
-        end
-
-        def git_subrepo_root
-          gem_root.join('vendor', 'git-subrepo')
-        end
-
-        def path_with_git_subrepo
-          ([git_subrepo_root.join('lib').to_path] +
-              ENV['PATH'].if_present('').split(::File::PATH_SEPARATOR))
-            .join(::File::PATH_SEPARATOR)
-        end
       end
     end
   end
